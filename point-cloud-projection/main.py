f90d4498dd73a042cfaacdab612a55e6aeb88307
Łukasz Piłatowski <lukpila29@gmail.com>
2020-09-25 13:40:41 +0200

    migrate PCL example

---
+++ point-cloud-projection/main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+import cv2
+import depthai
+from projector_3d import PointCloudVisualizer
+
+device = depthai.Device("", False)
+pipeline = device.create_pipeline(config={
+    'streams': ['right', 'depth'],
+    'ai': {
+        "blob_file": str(Path('./mobilenet-ssd/mobilenet-ssd.blob').resolve().absolute()),
+        "blob_file_config": str(Path('./mobilenet-ssd/mobilenet-ssd.json').resolve().absolute()),
+    },
+    'camera': {'mono': {'resolution_h': 720, 'fps': 30}},
+})
+
+if pipeline is None:
+    raise RuntimeError("Error creating a pipeline!")
+
+right = None
+pcl_converter = None
+
+while True:
+    nnet_packets, data_packets = pipeline.get_available_nnet_and_data_packets()
+
+    for packet in data_packets:
+        if packet.stream_name == "right":
+            right = packet.getData()
+            cv2.imshow(packet.stream_name, right)
+        elif packet.stream_name == "depth":
+            frame = packet.getData()
+            if right is not None:
+                if pcl_converter is None:
+                    pcl_converter = PointCloudVisualizer('intrinisc_right.json')
+                pcd = pcl_converter.rgbd_to_projection(frame, right)
+                pcl_converter.visualize_pcd()
+            cv2.imshow(packet.stream_name, frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+if pcl_converter is not None:
+    pcl_converter.close_window()
+
+
--- point-cloud-projection/projector_3d.py
+++ point-cloud-projection/projector_3d.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-
-import numpy as np
 import open3d as o3d
 import os
 
