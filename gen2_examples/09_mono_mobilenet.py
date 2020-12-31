04c41a64e4278f3c2833a1e5969e35c26577a035
alex-luxonis <alex@luxonis.com>
2020-12-31 03:57:42 +0200

    Fix Mono mobilenet example: Mono -> ImageManip -> NN

--- gen2_examples/09_mono_mobilenet.py
+++ gen2_examples/09_mono_mobilenet.py
@@ -13,9 +13,20 @@ detection_nn = pipeline.createNeuralNetwork()
 detection_nn.setBlobPath(str((Path(__file__).parent / Path('models/mobilenet-ssd.blob')).resolve().absolute()))
 cam_left.out.link(detection_nn.input)
 
+manip = pipeline.createImageManip()
+manip.setResize(300, 300)
+# The NN model expects BGR input. By default ImageManip output type would be same as input (gray in this case)
+manip.setFrameType(depthai.RawImgFrame.Type.BGR888p)
+cam_left.out.link(manip.inputImage)
+manip.out.link(detection_nn.input)
+
 xout_left = pipeline.createXLinkOut()
 xout_left.setStreamName("left")
 cam_left.out.link(xout_left.input)
+
+xout_manip = pipeline.createXLinkOut()
+xout_manip.setStreamName("manip")
+manip.out.link(xout_manip.input)
 
 xout_nn = pipeline.createXLinkOut()
 xout_nn.setStreamName("nn")
@@ -28,9 +39,11 @@ device = depthai.Device(pipeline, device_info)
 device.startPipeline()
 
 q_left = device.getOutputQueue("left")
+q_manip = device.getOutputQueue("manip")
 q_nn = device.getOutputQueue("nn")
 
 frame = None
+frame_manip = None
 bboxes = []
 
 
@@ -40,12 +53,18 @@ def frame_norm(frame, bbox):
 
 while True:
     in_left = q_left.tryGet()
+    in_manip = q_manip.tryGet()
     in_nn = q_nn.tryGet()
 
     if in_left is not None:
         shape = (in_left.getHeight(), in_left.getWidth())
         frame = in_left.getData().reshape(shape).astype(np.uint8)
         frame = np.ascontiguousarray(frame)
+
+    if in_manip is not None:
+        shape = (3, in_manip.getHeight(), in_manip.getWidth())
+        frame_manip = in_manip.getData().reshape(shape).transpose(1, 2, 0).astype(np.uint8)
+        frame_manip = np.ascontiguousarray(frame_manip)
 
     if in_nn is not None:
         bboxes = np.array(in_nn.getFirstLayerFp16())
@@ -54,10 +73,13 @@ while True:
         bboxes = bboxes[bboxes[:, 2] > 0.5][:, 3:7]
 
     if frame is not None:
+        cv2.imshow("left", frame)
+
+    if frame_manip is not None:
         for raw_bbox in bboxes:
-            bbox = frame_norm(frame, raw_bbox)
-            cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
-        cv2.imshow("rgb", frame)
+            bbox = frame_norm(frame_manip, raw_bbox)
+            cv2.rectangle(frame_manip, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
+        cv2.imshow("manip", frame_manip)
 
     if cv2.waitKey(1) == ord('q'):
         break
