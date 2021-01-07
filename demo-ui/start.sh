fa4c0be1619148fd246d801a68390590450ecca0
Łukasz Piłatowski <lukpila29@gmail.com>
2021-01-07 14:22:38 +0100

    remove demo-ui as it's not relevant anymore

--- demo-ui/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-(cd api && nohup python3 main.py > app.log 2>&1 &)
-sleep 1
-sudo systemctl restart nginx
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost
--- demo-ui/web/src/redux/sagas/index.js
+++
@@ -1,9 +0,0 @@
-import {all} from 'redux-saga/effects';
-import app from './appSaga';
-
-
-export default function* index() {
-    yield all([
-      app(),
-    ]);
-}
