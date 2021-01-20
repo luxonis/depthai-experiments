87bcc63b84928d57212b97d7f6398c52f5674634
alex-luxonis <alex@luxonis.com>
2021-01-20 14:20:57 +0200

    Move rotated crop / warp on device (ImageManip), link recognition NN to ImageManip

--- gen2-ocr/main.py
+++ gen2-ocr/main.py
@@ -26,12 +26,28 @@ nn_xout = pipeline.createXLinkOut()
 nn_xout.setStreamName("detections")
 nn.out.link(nn_xout.input)
 
+manip = pipeline.createImageManip()
+manip.setWaitForConfigInput(True)
+
+manip_img = pipeline.createXLinkIn()
+manip_img.setStreamName('manip_img')
+manip_img.out.link(manip.inputImage)
+
+manip_cfg = pipeline.createXLinkIn()
+manip_cfg.setStreamName('manip_cfg')
+manip_cfg.out.link(manip.inputConfig)
+
+manip_xout = pipeline.createXLinkOut()
+manip_xout.setStreamName('manip_out')
+
 nn2 = pipeline.createNeuralNetwork()
 nn2.setBlobPath(str((Path(__file__).parent / Path('text-recognition-0012.blob')).resolve().absolute()))
+manip.out.link(nn2.input)
+manip.out.link(manip_xout.input)
 
-nn2_in = pipeline.createXLinkIn()
-nn2_in.setStreamName("in_recognition")
-nn2_in.out.link(nn2.input)
+#nn2_in = pipeline.createXLinkIn()
+#nn2_in.setStreamName("in_recognition")
+#nn2_in.out.link(nn2.input)
 
 nn2_xout = pipeline.createXLinkOut()
 nn2_xout.setStreamName("recognitions")
@@ -52,8 +68,12 @@ q_det = device.getOutputQueue("detections", 1, overwrite=True)
 q_rec_in = device.getInputQueue("in_recognition")
 q_rec = device.getOutputQueue("recognitions")
 
+q_manip_img = device.getInputQueue("manip_img")
+q_manip_cfg = device.getInputQueue("manip_cfg")
+q_manip_out = device.getOutputQueue("manip_out")
+
 frame = None
-points = []
+rotated_rectangles = []
 rec_pushed = 0
 rec_received = 0
 
@@ -112,6 +132,7 @@ while True:
 
     if in_prev is not None:
         shape = (3, in_prev.getHeight(), in_prev.getWidth())
+        frame_orig = in_prev
         frame = in_prev.getData().reshape(shape).transpose(1, 2, 0).astype(np.uint8)
         frame = np.ascontiguousarray(frame)
 
@@ -131,28 +152,42 @@ while True:
     
             bboxes, confs, angles = east.decode_predictions(scores, geom1, geom2)
             boxes, angles = east.non_max_suppression(np.array(bboxes), probs=confs, angles=np.array(angles))
-            points = [
-                east.rotated_Rectangle(bbox, angle * -1)
+            rotated_rectangles = [
+                east.get_cv_rotated_rect(bbox, angle * -1)
                 for (bbox, angle) in zip(boxes, angles)
             ]
 
     if frame is not None:
         if in_det is not None:
             rec_received = 0
-            rec_pushed = len(points)
+            rec_pushed = len(rotated_rectangles)
             if rec_pushed:
                 print("====== Pushing for recognition, count:", rec_pushed)
             cropped_stacked = None
-            for point_arr in points:
-                cv2.polylines(frame, [point_arr], isClosed=True, color=(255, 0, 0), thickness=1, lineType=cv2.LINE_8)
+            for rotated_rect in rotated_rectangles:
+                # Draw detection crop area on input frame
+                points = np.int0(cv2.boxPoints(rotated_rect))
+                cv2.polylines(frame, [points], isClosed=True, color=(255, 0, 0), thickness=1, lineType=cv2.LINE_8)
 
-                transformed = east.four_point_transform(frame, point_arr)
-                transformed = cv2.cvtColor(transformed, cv2.COLOR_BGR2GRAY)
-                transformed = cv2.resize(transformed, (120, 32), interpolation=cv2.INTER_AREA)
-                transformed = np.ascontiguousarray(transformed)
-                nn_data = depthai.NNData()
-                nn_data.setLayer("Placeholder", transformed)
-                q_rec_in.send(nn_data)
+                # TODO make it work taking args like in OpenCV:
+                # rr = ((256, 256), (128, 64), 30)
+                rr = depthai.RawImageManipConfig.RotatedRect()
+                rr.center.x    = rotated_rect[0][0]
+                rr.center.y    = rotated_rect[0][1]
+                rr.size.width  = rotated_rect[1][0]
+                rr.size.height = rotated_rect[1][1]
+                rr.angle       = rotated_rect[2]
+                cfg = depthai.ImageManipConfig()
+                cfg.setCropRotatedRect(rr, False)
+                cfg.setResize(120, 32)
+                # Send frame and config to device
+                q_manip_img.send(frame_orig)
+                q_manip_cfg.send(cfg)
+                # Get processed output from device
+                cropped = q_manip_out.get()
+                shape = (3, cropped.getHeight(), cropped.getWidth())
+                transformed = cropped.getData().reshape(shape).transpose(1, 2, 0)
+
                 if cropped_stacked is None:
                     cropped_stacked = transformed
                 else:
