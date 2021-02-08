196c88abf65e686f0017a86908bdaac902c868d4
Łukasz Piłatowski <lukpila29@gmail.com>
2021-02-08 14:28:41 +0100

    Add pose estimation example

---
+++ gen2-human-pose/README.md
@@ -0,0 +1,41 @@
+# Gen2 Pose Estimation Example
+
+This example demonstrates how to run [Human Pose Estimation Network](https://docs.openvinotoolkit.org/latest/omz_models_intel_human_pose_estimation_0001_description_human_pose_estimation_0001.html) using Gen2 Pipeline Builder.
+
+
+## Demo
+
+WIP
+
+## Pre-requisites
+
+1. Purchase a DepthAI model (see [shop.luxonis.com](https://shop.luxonis.com/))
+2. Install requirements
+   ```
+   python3 -m pip install -r requirements.txt
+   ```
+
+## Usage
+
+```
+usage: main.py [-h] [-nd] [-cam] [-vid VIDEO]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -nd, --no-debug       Prevent debug output
+  -cam, --camera        Use DepthAI 4K RGB camera for inference (conflicts with -vid)
+  -vid VIDEO, --video VIDEO
+                        Path to video file to be used for inference (conflicts with -cam)
+```
+
+To use with a video file, run the script with the following arguments
+
+```
+python3 main.py -vid ./input.mp4
+```
+
+To use with DepthAI 4K RGB camera, use instead
+
+```
+python3 main.py -cam
+``` 
---
+++ gen2-human-pose/main.py
@@ -0,0 +1,178 @@
+import argparse
+from pathlib import Path
+from pose import getKeypoints, getValidPairs, getPersonwiseKeypoints
+import cv2
+import depthai
+import numpy as np
+from imutils.video import FPS
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-nd', '--no-debug', action="store_true", help="Prevent debug output")
+parser.add_argument('-cam', '--camera', action="store_true", help="Use DepthAI 4K RGB camera for inference (conflicts with -vid)")
+parser.add_argument('-vid', '--video', type=str, help="Path to video file to be used for inference (conflicts with -cam)")
+args = parser.parse_args()
+
+if not args.camera and not args.video:
+    raise RuntimeError("No source selected. Please use either \"-cam\" to use RGB camera as a source or \"-vid <path>\" to run on video")
+
+debug = not args.no_debug
+
+
+def cos_dist(a, b):
+    return np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
+
+
+def to_tensor_result(packet):
+    return {
+        tensor.name: np.array(packet.getLayerFp16(tensor.name)).reshape(tensor.dims)
+        for tensor in packet.getRaw().tensors
+    }
+
+def frame_norm(frame, bbox):
+    return (np.clip(np.array(bbox), 0, 1) * np.array([*frame.shape[:2], *frame.shape[:2]])[::-1]).astype(int)
+
+
+def to_planar(arr: np.ndarray, shape: tuple) -> list:
+    return [val for channel in cv2.resize(arr, shape).transpose(2, 0, 1) for y_col in channel for val in y_col]
+
+
+def create_pipeline():
+    print("Creating pipeline...")
+    pipeline = depthai.Pipeline()
+
+    if args.camera:
+        # ColorCamera
+        print("Creating Color Camera...")
+        cam = pipeline.createColorCamera()
+        cam.setPreviewSize(456, 256)
+        cam.setResolution(depthai.ColorCameraProperties.SensorResolution.THE_1080_P)
+        cam.setInterleaved(False)
+        cam.setBoardSocket(depthai.CameraBoardSocket.RGB)
+        cam_xout = pipeline.createXLinkOut()
+        cam_xout.setStreamName("cam_out")
+        cam.preview.link(cam_xout.input)
+
+    # NeuralNetwork
+    print("Creating Human Pose Estimation Neural Network...")
+    pose_nn = pipeline.createNeuralNetwork()
+    pose_nn.setBlobPath(str(Path("models/human-pose-estimation-0001.blob").resolve().absolute()))
+    pose_nn_xout = pipeline.createXLinkOut()
+    pose_nn_xout.setStreamName("pose_nn")
+    pose_nn.out.link(pose_nn_xout.input)
+
+    if args.camera:
+        cam.preview.link(pose_nn.input)
+    else:
+        pose_in = pipeline.createXLinkIn()
+        pose_in.setStreamName("pose_in")
+        pose_in.out.link(pose_nn.input)
+
+    print("Pipeline created.")
+    return pipeline
+
+
+colors = [[0, 100, 255], [0, 100, 255], [0, 255, 255], [0, 100, 255], [0, 255, 255], [0, 100, 255], [0, 255, 0],
+          [255, 200, 100], [255, 0, 255], [0, 255, 0], [255, 200, 100], [255, 0, 255], [0, 0, 255], [255, 0, 0],
+          [200, 200, 0], [255, 0, 0], [200, 200, 0], [0, 0, 0]]
+POSE_PAIRS = [[1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9], [9, 10], [1, 11], [11, 12], [12, 13],
+              [1, 0], [0, 14], [14, 16], [0, 15], [15, 17], [2, 17], [5, 16]]
+
+
+with depthai.Device(create_pipeline()) as device:
+    print("Starting pipeline...")
+    device.startPipeline()
+    if args.camera:
+        cam_out = device.getOutputQueue("cam_out", 1, True)
+    else:
+        pose_in = device.getInputQueue("pose_in")
+    pose_nn = device.getOutputQueue("pose_nn")
+
+    if args.video:
+        cap = cv2.VideoCapture(str(Path(args.video).resolve().absolute()))
+
+    fps = FPS()
+    fps.start()
+
+
+    def should_run():
+        return cap.isOpened() if args.video else True
+
+
+    def get_frame():
+        if args.video:
+            read_correctly, frame = cap.read()
+            return read_correctly, cv2.resize(frame, (456, 256))
+        else:
+            return True, np.array(cam_out.get().getData()).reshape((3, 256, 456)).transpose(1, 2, 0).astype(np.uint8)
+
+
+    try:
+        while should_run():
+            read_correctly, frame = get_frame()
+
+            if not read_correctly:
+                break
+
+            if frame is not None:
+                fps.update()
+                debug_frame = frame.copy()
+
+                if not args.camera:
+                    nn_data = depthai.NNData()
+                    nn_data.setLayer("input", to_planar(frame, (456, 256)))
+                    pose_in.send(nn_data)
+
+            while pose_nn.has():
+                raw_in = pose_nn.get()
+                heatmaps = np.array(raw_in.getLayerFp16('Mconv7_stage2_L2')).reshape((1, 19, 32, 57))
+                pafs = np.array(raw_in.getLayerFp16('Mconv7_stage2_L1')).reshape((1, 38, 32, 57))
+                heatmaps = heatmaps.astype('float32')
+                pafs = pafs.astype('float32')
+                outputs = np.concatenate((heatmaps, pafs), axis=1)
+
+                detected_keypoints = []
+                keypoints_list = np.zeros((0, 3))
+                keypoint_id = 0
+
+                for row in range(18):
+                    probMap = outputs[0, row, :, :]
+                    probMap = cv2.resize(probMap, (456, 256))  # (456, 256)
+                    keypoints = getKeypoints(probMap, 0.3)
+                    keypoints_with_id = []
+
+                    for i in range(len(keypoints)):
+                        keypoints_with_id.append(keypoints[i] + (keypoint_id,))
+                        keypoints_list = np.vstack([keypoints_list, keypoints[i]])
+                        keypoint_id += 1
+
+                    detected_keypoints.append(keypoints_with_id)
+
+                valid_pairs, invalid_pairs = getValidPairs(outputs, 456, 256, detected_keypoints)
+                personwiseKeypoints = getPersonwiseKeypoints(valid_pairs, invalid_pairs, keypoints_list)
+
+                for i in range(18):
+                    for j in range(len(detected_keypoints[i])):
+                        cv2.circle(debug_frame, detected_keypoints[i][j][0:2], 5, colors[i], -1, cv2.LINE_AA)
+                for i in range(17):
+                    for n in range(len(personwiseKeypoints)):
+                        index = personwiseKeypoints[n][np.array(POSE_PAIRS[i])]
+                        if -1 in index:
+                            continue
+                        B = np.int32(keypoints_list[index.astype(int), 0])
+                        A = np.int32(keypoints_list[index.astype(int), 1])
+                        cv2.line(debug_frame, (B[0], A[0]), (B[1], A[1]), colors[i], 3, cv2.LINE_AA)
+
+
+            if debug and frame is not None:
+                cv2.imshow("rgb", debug_frame)
+
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+    except KeyboardInterrupt:
+        pass
+
+fps.stop()
+print("FPS: {:.2f}".format(fps.fps()))
+if args.video:
+    cap.release()
