08b52c3ff8f9ca18eb6c5265b5ec5d72f43729be
Łukasz Piłatowski <lukpila29@gmail.com>
2021-03-16 14:47:59 +0100

    Fix and improve human pose estimation example (#90)
    * Fix and improve human pose estimation example
    
    * update README.md
    
    * refactor model names
    
    * update models to use OpenVINO 2021.2

--- gen2-human-pose/README.md
+++ gen2-human-pose/README.md
@@ -5,8 +5,12 @@ This example demonstrates how to run [Human Pose Estimation Network](https://doc
 
 ## Demo
 
+### Camera
 [![Gen2 Age & Gender recognition](https://user-images.githubusercontent.com/5244214/107493701-35f97100-6b8e-11eb-8b13-02a7a8dbec21.gif)](https://www.youtube.com/watch?v=Py3-dHQymko "Human pose estimation on DepthAI")
 
+### Video file
+
+[![Gen2 Age & Gender recognition](https://user-images.githubusercontent.com/5244214/110801736-d3bf8900-827d-11eb-934b-9755978f80d9.gif)](https://www.youtube.com/watch?v=1dp2wJ_OqxI "Human pose estimation on DepthAI")
 
 
 ## Pre-requisites
--- gen2-human-pose/main.py
+++ gen2-human-pose/main.py
@@ -1,4 +1,6 @@
 import argparse
+import threading
+import time
 from pathlib import Path
 from pose import getKeypoints, getValidPairs, getPersonwiseKeypoints
 import cv2
@@ -33,14 +35,12 @@ def frame_norm(frame, bbox):
 
 
 def to_planar(arr: np.ndarray, shape: tuple) -> list:
-    return [val for channel in cv2.resize(arr, shape).transpose(2, 0, 1) for y_col in channel for val in y_col]
+    return cv2.resize(arr, shape).transpose(2,0,1).flatten()
 
 
 def create_pipeline():
     print("Creating pipeline...")
     pipeline = dai.Pipeline()
-    pipeline.setOpenVINOVersion(version = dai.OpenVINO.Version.VERSION_2020_1)
-    
 
     if args.camera:
         # ColorCamera
@@ -53,11 +53,22 @@ def create_pipeline():
         cam_xout = pipeline.createXLinkOut()
         cam_xout.setStreamName("cam_out")
         cam.preview.link(cam_xout.input)
+        controlIn = pipeline.createXLinkIn()
+        controlIn.setStreamName('control')
+        controlIn.out.link(cam.inputControl)
 
     # NeuralNetwork
     print("Creating Human Pose Estimation Neural Network...")
     pose_nn = pipeline.createNeuralNetwork()
-    pose_nn.setBlobPath(str(Path("models/human-pose-estimation-0001.blob").resolve().absolute()))
+    if args.camera:
+        pose_nn.setBlobPath(str(Path("models/human-pose-estimation-0001_openvino_2021.2_6shave.blob").resolve().absolute()))
+    else:
+        pose_nn.setBlobPath(str(Path("models/human-pose-estimation-0001_openvino_2021.2_8shave.blob").resolve().absolute()))
+    # Increase threads for detection
+    pose_nn.setNumInferenceThreads(2)
+    # Specify that network takes latest arriving frame in non-blocking manner
+    pose_nn.input.setQueueSize(1)
+    pose_nn.input.setBlocking(False)
     pose_nn_xout = pipeline.createXLinkOut()
     pose_nn_xout.setStreamName("pose_nn")
     pose_nn.out.link(pose_nn_xout.input)
@@ -79,22 +90,105 @@ colors = [[0, 100, 255], [0, 100, 255], [0, 255, 255], [0, 100, 255], [0, 255, 2
 POSE_PAIRS = [[1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9], [9, 10], [1, 11], [11, 12], [12, 13],
               [1, 0], [0, 14], [14, 16], [0, 15], [15, 17], [2, 17], [5, 16]]
 
+running = True
+pose = None
+keypoints_list = None
+detected_keypoints = None
+personwiseKeypoints = None
+
+
+class FPSHandler:
+    def __init__(self, cap=None):
+        self.timestamp = time.time()
+        self.start = time.time()
+        self.framerate = cap.get(cv2.CAP_PROP_FPS) if cap is not None else None
+
+        self.frame_cnt = 0
+        self.ticks = {}
+        self.ticks_cnt = {}
+
+    def next_iter(self):
+        if not args.camera:
+            frame_delay = 1.0 / self.framerate
+            delay = (self.timestamp + frame_delay) - time.time()
+            if delay > 0:
+                time.sleep(delay)
+        self.timestamp = time.time()
+        self.frame_cnt += 1
+
+    def tick(self, name):
+        if name in self.ticks:
+            self.ticks_cnt[name] += 1
+        else:
+            self.ticks[name] = time.time()
+            self.ticks_cnt[name] = 0
+
+    def tick_fps(self, name):
+        if name in self.ticks:
+            return self.ticks_cnt[name] / (time.time() - self.ticks[name])
+        else:
+            return 0
+
+    def fps(self):
+        return self.frame_cnt / (self.timestamp - self.start)
+
+
+if args.camera:
+    fps = FPSHandler()
+else:
+    cap = cv2.VideoCapture(str(Path(args.video).resolve().absolute()))
+    fps = FPSHandler(cap)
+
+
+def pose_thread(in_queue):
+    global keypoints_list, detected_keypoints, personwiseKeypoints
+
+    while running:
+        try:
+            raw_in = in_queue.get()
+        except RuntimeError:
+            return
+        fps.tick('nn')
+        heatmaps = np.array(raw_in.getLayerFp16('Mconv7_stage2_L2')).reshape((1, 19, 32, 57))
+        pafs = np.array(raw_in.getLayerFp16('Mconv7_stage2_L1')).reshape((1, 38, 32, 57))
+        heatmaps = heatmaps.astype('float32')
+        pafs = pafs.astype('float32')
+        outputs = np.concatenate((heatmaps, pafs), axis=1)
+
+        new_keypoints = []
+        new_keypoints_list = np.zeros((0, 3))
+        keypoint_id = 0
+
+        for row in range(18):
+            probMap = outputs[0, row, :, :]
+            probMap = cv2.resize(probMap, (w, h))  # (456, 256)
+            keypoints = getKeypoints(probMap, 0.3)
+            new_keypoints_list = np.vstack([new_keypoints_list, *keypoints])
+            keypoints_with_id = []
+
+            for i in range(len(keypoints)):
+                keypoints_with_id.append(keypoints[i] + (keypoint_id,))
+                keypoint_id += 1
+
+            new_keypoints.append(keypoints_with_id)
+
+        valid_pairs, invalid_pairs = getValidPairs(outputs, w, h, new_keypoints)
+        newPersonwiseKeypoints = getPersonwiseKeypoints(valid_pairs, invalid_pairs, new_keypoints_list)
+
+        detected_keypoints, keypoints_list, personwiseKeypoints = (new_keypoints, new_keypoints_list, newPersonwiseKeypoints)
+
 
 with dai.Device(create_pipeline()) as device:
     print("Starting pipeline...")
     device.startPipeline()
     if args.camera:
         cam_out = device.getOutputQueue("cam_out", 1, True)
+        controlQueue = device.getInputQueue('control')
     else:
         pose_in = device.getInputQueue("pose_in")
-    pose_nn = device.getOutputQueue("pose_nn")
-
-    if args.video:
-        cap = cv2.VideoCapture(str(Path(args.video).resolve().absolute()))
-
-    fps = FPS()
-    fps.start()
-
+    pose_nn = device.getOutputQueue("pose_nn", 1, False)
+    t = threading.Thread(target=pose_thread, args=(pose_nn, ))
+    t.start()
 
     def should_run():
         return cap.isOpened() if args.video else True
@@ -110,61 +204,34 @@ with dai.Device(create_pipeline()) as device:
     try:
         while should_run():
             read_correctly, frame = get_frame()
-            h, w = frame.shape[:2]  # 256, 456
 
             if not read_correctly:
                 break
 
-            fps.update()
+            fps.next_iter()
+            h, w = frame.shape[:2]  # 256, 456
             debug_frame = frame.copy()
 
             if not args.camera:
-                nn_data = depthai.NNData()
+                nn_data = dai.NNData()
                 nn_data.setLayer("input", to_planar(frame, (456, 256)))
                 pose_in.send(nn_data)
 
-            while pose_nn.has():
-                raw_in = pose_nn.get()
-                heatmaps = np.array(raw_in.getLayerFp16('Mconv7_stage2_L2')).reshape((1, 19, 32, 57))
-                pafs = np.array(raw_in.getLayerFp16('Mconv7_stage2_L1')).reshape((1, 38, 32, 57))
-                heatmaps = heatmaps.astype('float32')
-                pafs = pafs.astype('float32')
-                outputs = np.concatenate((heatmaps, pafs), axis=1)
-
-                detected_keypoints = []
-                keypoints_list = np.zeros((0, 3))
-                keypoint_id = 0
-
-                for row in range(18):
-                    probMap = outputs[0, row, :, :]
-                    probMap = cv2.resize(probMap, (w, h))  # (456, 256)
-                    keypoints = getKeypoints(probMap, 0.3)
-                    keypoints_list = np.vstack([keypoints_list, *keypoints])
-                    keypoints_with_id = []
-
-                    for i in range(len(keypoints)):
-                        keypoints_with_id.append(keypoints[i] + (keypoint_id,))
-                        keypoint_id += 1
-
-                    detected_keypoints.append(keypoints_with_id)
-
-                valid_pairs, invalid_pairs = getValidPairs(outputs, w, h, detected_keypoints)
-                personwiseKeypoints = getPersonwiseKeypoints(valid_pairs, invalid_pairs, keypoints_list)
-
-                for i in range(18):
-                    for j in range(len(detected_keypoints[i])):
-                        cv2.circle(debug_frame, detected_keypoints[i][j][0:2], 5, colors[i], -1, cv2.LINE_AA)
-                for i in range(17):
-                    for n in range(len(personwiseKeypoints)):
-                        index = personwiseKeypoints[n][np.array(POSE_PAIRS[i])]
-                        if -1 in index:
-                            continue
-                        B = np.int32(keypoints_list[index.astype(int), 0])
-                        A = np.int32(keypoints_list[index.astype(int), 1])
-                        cv2.line(debug_frame, (B[0], A[0]), (B[1], A[1]), colors[i], 3, cv2.LINE_AA)
-
-
             if debug:
+                if keypoints_list is not None and detected_keypoints is not None and personwiseKeypoints is not None:
+                    for i in range(18):
+                        for j in range(len(detected_keypoints[i])):
+                            cv2.circle(debug_frame, detected_keypoints[i][j][0:2], 5, colors[i], -1, cv2.LINE_AA)
+                    for i in range(17):
+                        for n in range(len(personwiseKeypoints)):
+                            index = personwiseKeypoints[n][np.array(POSE_PAIRS[i])]
+                            if -1 in index:
+                                continue
+                            B = np.int32(keypoints_list[index.astype(int), 0])
+                            A = np.int32(keypoints_list[index.astype(int), 1])
+                            cv2.line(debug_frame, (B[0], A[0]), (B[1], A[1]), colors[i], 3, cv2.LINE_AA)
+                cv2.putText(debug_frame, f"RGB FPS: {round(fps.fps(), 1)}", (5, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+                cv2.putText(debug_frame, f"NN FPS:  {round(fps.tick_fps('nn'), 1)}", (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
                 cv2.imshow("rgb", debug_frame)
 
             key = cv2.waitKey(1)
@@ -181,7 +248,9 @@ with dai.Device(create_pipeline()) as device:
     except KeyboardInterrupt:
         pass
 
-fps.stop()
+    running = False
+
+t.join()
 print("FPS: {:.2f}".format(fps.fps()))
-if args.video:
+if not args.camera:
     cap.release()
