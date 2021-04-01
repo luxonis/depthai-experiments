95c835b2154af32a5e89e20f904cbe192f587f6d
Erol444 <erol123444@gmail.com>
2021-04-01 16:59:11 +0200

    added gif/video, used context manager

--- gen2-multiple-devices/README.md
+++ gen2-multiple-devices/README.md
@@ -2,8 +2,9 @@
 
 This example shows how you can use multiple DepthAI's on a single host. The demo will find all devices connected to the host and display an RGB preview from each of them. Note that at the end of the code we have to close the connection to all devices since we don't use context manager.
 
-**Demo**
-![image](https://artifacts.luxonis.com/artifactory/luxonis-depthai-data-local/images/multiple-devices.png)
+## Demo
+
+[![Multiple devices per host](https://user-images.githubusercontent.com/18037362/113307040-01d83c00-9305-11eb-9a42-c69c72a5dba5.gif)](https://www.youtube.com/watch?v=N1IY2CfhmEc "Multiple devices per host")
 
 Just two DepthAI's looking at each other.
 
--- gen2-multiple-devices/main.py
+++ gen2-multiple-devices/main.py
@@ -2,13 +2,14 @@
 
 import cv2
 import depthai as dai
+import contextlib
 
 # Start defining a pipeline
 pipeline = dai.Pipeline()
 
 # Define a source - color camera
 cam_rgb = pipeline.createColorCamera()
-cam_rgb.setPreviewSize(300, 300)
+cam_rgb.setPreviewSize(600, 600)
 cam_rgb.setBoardSocket(dai.CameraBoardSocket.RGB)
 cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
 cam_rgb.setInterleaved(False)
@@ -18,27 +19,22 @@ xout_rgb = pipeline.createXLinkOut()
 xout_rgb.setStreamName("rgb")
 cam_rgb.preview.link(xout_rgb.input)
 
-device_list = []
 q_rgb_list = []
 
-for device_info in dai.Device.getAllAvailableDevices():
-    device = dai.Device(pipeline, device_info)
-    device.startPipeline()
-    device_list.append(device)
+# https://docs.python.org/3/library/contextlib.html#contextlib.ExitStack
+with contextlib.ExitStack() as stack:
+    for device_info in dai.Device.getAllAvailableDevices():
+        device = stack.enter_context(dai.Device(pipeline, device_info))
+        device.startPipeline()
+        # Output queue will be used to get the rgb frames from the output defined above
+        q_rgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+        q_rgb_list.append(q_rgb)
 
-    # Output queue will be used to get the rgb frames from the output defined above
-    q_rgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
-    q_rgb_list.append(q_rgb)
+    while True:
+        for i, q_rgb in enumerate(q_rgb_list):
+            in_rgb = q_rgb.tryGet()
+            if in_rgb is not None:
+                cv2.imshow("rgb-" + str(i + 1), in_rgb.getCvFrame())
 
-while True:
-    for i, q_rgb in enumerate(q_rgb_list):
-        in_rgb = q_rgb.tryGet()  # TODO wait for queue events instead
-        if in_rgb is not None:
-            cv2.imshow("rgb-" + str(i + 1), in_rgb.getCvFrame())
-
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-# Close the connection to all devices
-for device in device_list:
-    device.close()
+        if cv2.waitKey(1) == ord('q'):
+            break
