43aff8142630beedc5e4ae522ad885c6b5cb171b
Richard Audette <richard_audette@yahoo.ca>
2021-04-24 21:00:52 -0400

    Updated to Gen2.

--- mjpeg-streaming/main.py
+++ mjpeg-streaming/main.py
@@ -4,9 +4,11 @@ import threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from io import BytesIO
 from pathlib import Path
+import sys
 from socketserver import ThreadingMixIn
 from time import sleep
-import depthai
+import depthai as dai
+import numpy as np
 import cv2
 from PIL import Image
 
@@ -62,50 +64,92 @@ th2 = threading.Thread(target=server_HTTP.serve_forever)
 th2.daemon = True
 th2.start()
 
-device = depthai.Device('', False)
 
-pipeline = device.create_pipeline(config={
-    "streams": ["metaout", "previewout"],
-    "ai": {
-        "blob_file": str(Path('./mobilenet-ssd/model.blob').resolve().absolute()),
-        "blob_file_config": str(Path('./mobilenet-ssd/config.json').resolve().absolute())
-    }
-})
+# Get argument first
+mobilenet_path = str((Path(__file__).parent / Path('mobilenet-ssd/model.blob')).resolve().absolute())
+if len(sys.argv) > 1:
+    mobilenet_path = sys.argv[1]
 
-if pipeline is None:
-    raise RuntimeError("Error initializing pipelne")
+# Start defining a pipeline
+pipeline = dai.Pipeline()
 
-detections = []
+# Define a source - color camera
+cam_rgb = pipeline.createColorCamera()
+cam_rgb.setPreviewSize(300, 300)
+cam_rgb.setInterleaved(False)
 
-while True:
-    nnet_packets, data_packets = pipeline.get_available_nnet_and_data_packets()
+# Define a neural network that will make predictions based on the source frames
+detection_nn = pipeline.createNeuralNetwork()
+detection_nn.setBlobPath(mobilenet_path)
+cam_rgb.preview.link(detection_nn.input)
 
-    for nnet_packet in nnet_packets:
-        detections = list(nnet_packet.getDetectedObjects())
+# Create outputs
+xout_rgb = pipeline.createXLinkOut()
+xout_rgb.setStreamName("rgb")
+cam_rgb.preview.link(xout_rgb.input)
 
-    for packet in data_packets:
-        if packet.stream_name == 'previewout':
-            data = packet.getData()
-            data0 = data[0, :, :]
-            data1 = data[1, :, :]
-            data2 = data[2, :, :]
-            frame = cv2.merge([data0, data1, data2])
+xout_nn = pipeline.createXLinkOut()
+xout_nn.setStreamName("nn")
+detection_nn.out.link(xout_nn.input)
 
-            img_h = frame.shape[0]
-            img_w = frame.shape[1]
+# MobilenetSSD label texts
+texts = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow",
+         "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
 
-            for detection in detections:
-                left, top = int(detection.x_min * img_w), int(detection.y_min * img_h)
-                right, bottom = int(detection.x_max * img_w), int(detection.y_max * img_h)
 
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+# Pipeline defined, now the device is connected to
+with dai.Device(pipeline) as device:
+    # Start pipeline
+    device.startPipeline()
 
-            server_TCP.datatosend = json.dumps([detection.get_dict() for detection in detections])
+    # Output queues will be used to get the rgb frames and nn data from the outputs defined above
+    q_rgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+    q_nn = device.getOutputQueue(name="nn", maxSize=4, blocking=False)
+
+    frame = None
+    bboxes = []
+    confidences = []
+    labels = []
+
+    # nn data, being the bounding box locations, are in <0..1> range - they need to be normalized with frame width/height
+    def frame_norm(frame, bbox):
+        norm_vals = np.full(len(bbox), frame.shape[0])
+        norm_vals[::2] = frame.shape[1]
+        return (np.clip(np.array(bbox), 0, 1) * norm_vals).astype(int)
+
+
+    while True:
+        # instead of get (blocking) used tryGet (nonblocking) which will return the available data or None otherwise
+        in_rgb = q_rgb.tryGet()
+        in_nn = q_nn.tryGet()
+
+        if in_rgb is not None:
+            # if the data from the rgb camera is available, transform the 1D data into a HxWxC frame
+            shape = (3, in_rgb.getHeight(), in_rgb.getWidth())
+            frame = in_rgb.getData().reshape(shape).transpose(1, 2, 0).astype(np.uint8)
+            frame = np.ascontiguousarray(frame)
+
+        if in_nn is not None:
+            # one detection has 7 numbers, and the last detection is followed by -1 digit, which later is filled with 0
+            bboxes = np.array(in_nn.getFirstLayerFp16())
+            # transform the 1D array into Nx7 matrix
+            bboxes = bboxes.reshape((bboxes.size // 7, 7))
+            # filter out the results which confidence less than a defined threshold
+            bboxes = bboxes[bboxes[:, 2] > 0.5]
+            # Cut bboxes and labels
+            labels = bboxes[:, 1].astype(int)
+            confidences = bboxes[:, 2]
+            bboxes = bboxes[:, 3:7]
+
+        if frame is not None:
+            # if the frame is available, draw bounding boxes on it and show the frame
+            for raw_bbox, label, conf in zip(bboxes, labels, confidences):
+                bbox = frame_norm(frame, raw_bbox)
+                cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
+                cv2.putText(frame, texts[label], (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+                cv2.putText(frame, f"{int(conf * 100)}%", (bbox[0] + 10, bbox[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+                server_TCP.datatosend = texts[label] +"," + f"{int(conf * 100)}%"
             server_HTTP.frametosend = frame
-            cv2.imshow('previewout', frame)
 
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-del pipeline
-del device
+        if cv2.waitKey(1) == ord('q'):
+            break
