2b3390cf26626408ba38c37455047ef2d3ac622e
alex-luxonis <alex@luxonis.com>
2021-05-05 03:29:52 +0300

    Add arguments to set stereo options and camera tuning

--- stereo_on_host/spatial_calculator_rgb.py
+++ stereo_on_host/spatial_calculator_rgb.py
@@ -3,7 +3,17 @@
 import cv2
 import depthai as dai
 import numpy as np
+import argparse
 import cmapy
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-lrc', '--lrcheck',  default=False, action="store_true")
+parser.add_argument('-ext', '--extended', default=False, action="store_true")
+parser.add_argument('-sub', '--subpixel', default=False, action="store_true")
+parser.add_argument('-dct', '--disp-confidence-thr', default=230, type=int)
+parser.add_argument('-fd',  '--force-depth-with-subpixel', default=False, action="store_true")
+parser.add_argument('-tun', '--camera-tuning', type=str)
+args = parser.parse_args()
 
 print(dai.__version__)
 
@@ -17,19 +27,21 @@ SAVE_EVERY = 30 # AKA 30FPS / 30 SAVE_EVERY => 1 log per second
 
 stepSize = 0.05
 
-showDisparity = 1  # Instead of passthrough depth (note: may not be properly synced)
-lrcheck  = 0
-extended = 0
-subpixel = 0 # Warning: if enabled, showDisparity will be disabled, as disparity and depth can't work together
-disparityConfidenceThreshold = 200
 if 1:  # OpenCV predefined maps
     colorMap = cv2.COLORMAP_JET  # COLORMAP_HOT, COLORMAP_TURBO, ...
 else:  # matplotlib maps, see: https://matplotlib.org/stable/tutorials/colors/colormaps.html
     colorMap = cmapy.cmap('inferno')
 
-if showDisparity and subpixel:
-    print("Can't use both disparity and depth with subpixel enabled -- reverting to showing depth")
-    showDisparity = False
+showDisparity = True
+averageDepthCalc = True
+if args.subpixel:
+    if args.force_depth_with_subpixel:
+        print("Subpixel enabled and `force_depth_with_subpixel` set")
+        showDisparity = False
+    else:
+        print("Subpixel enabled and average depth calculator disabled.")
+        print("To enable it (and view inverted depth instead of disparity), set `-fd`")
+        averageDepthCalc = False
 
 # Start defining a pipeline
 pipeline = dai.Pipeline()
@@ -52,22 +64,23 @@ xinSpatialCalcConfig.setStreamName("spatialCalcConfig")
 monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
 monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
 monoLeft.setFps(FPS)
-monoLeft.setImageOrientation(dai.CameraImageOrientation.ROTATE_180_DEG);
+monoLeft.setImageOrientation(dai.CameraImageOrientation.ROTATE_180_DEG)
 
 monoRight.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
 monoRight.setBoardSocket(dai.CameraBoardSocket.RGB)
 monoRight.setFps(FPS)
 monoRight.setIspScale(1, 3)
-monoRight.setImageOrientation(dai.CameraImageOrientation.ROTATE_180_DEG);
+monoRight.setImageOrientation(dai.CameraImageOrientation.ROTATE_180_DEG)
 monoRight.initialControl.setManualFocus(135)
-
+if args.camera_tuning is not None:
+    monoRight.setCameraTuningBlobPath(args.camera_tuning)
 
 # StereoDepth
-stereo.setConfidenceThreshold(disparityConfidenceThreshold)
+stereo.setConfidenceThreshold(args.disp_confidence_thr)
 stereo.setRectifyEdgeFillColor(0)
-stereo.setLeftRightCheck(lrcheck)
-stereo.setExtendedDisparity(extended)
-stereo.setSubpixel(subpixel)
+stereo.setLeftRightCheck(args.lrcheck)
+stereo.setExtendedDisparity(args.extended)
+stereo.setSubpixel(args.subpixel)
 
 monoLeft.out.link(stereo.left)
 monoRight.isp.link(stereo.right)
@@ -76,8 +89,10 @@ if showDisparity:
     stereo.disparity.link(xoutDepth.input)
 else:
     spatialLocationCalculator.passthroughDepth.link(xoutDepth.input)
-    
-stereo.depth.link(spatialLocationCalculator.inputDepth)
+
+if averageDepthCalc:
+    stereo.depth.link(spatialLocationCalculator.inputDepth)
+
 
 topLeft = dai.Point2f(0.5, 0.5)
 bottomRight = dai.Point2f(0.6, 0.6)
@@ -115,13 +130,13 @@ with dai.Device(pipeline) as device, open("log.txt", "a") as file:
     while True:
         out = ""
         inDepth = depthQueue.get() # Blocking call, will wait until a new data has arrived
-        inDepthAvg = spatialCalcQueue.get() # Blocking call, will wait until a new data has arrived
+        inDepthAvg = spatialCalcQueue.tryGet() # Blocking call, will wait until a new data has arrived
 
         depthFrame = inDepth.getFrame()
         if showDisparity:
             maxDisparity = 95
-            if extended: maxDisparity *= 2
-            if subpixel: maxDisparity *= 32
+            if args.extended: maxDisparity *= 2
+            if args.subpixel: maxDisparity *= 32
             depthFrame = (depthFrame * 255. / maxDisparity).astype(np.uint8)
             depthFrameColor = cv2.applyColorMap(depthFrame, colorMap)
         else:
@@ -129,22 +144,23 @@ with dai.Device(pipeline) as device, open("log.txt", "a") as file:
             depthFrameColor = cv2.equalizeHist(depthFrameColor)
             depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_HOT)
 
-        spatialData = inDepthAvg.getSpatialLocations()
-        for depthData in spatialData:
-            roi = depthData.config.roi
-            roi = roi.denormalize(width=depthFrameColor.shape[1], height=depthFrameColor.shape[0])
-            xmin = int(roi.topLeft().x)
-            ymin = int(roi.topLeft().y)
-            xmax = int(roi.bottomRight().x)
-            ymax = int(roi.bottomRight().y)
-
-            fontType = cv2.FONT_HERSHEY_TRIPLEX
-            cv2.rectangle(depthFrameColor, (xmin, ymin), (xmax, ymax), color, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX)
-            cv2.putText(depthFrameColor, f"X: {int(depthData.spatialCoordinates.x)} mm", (xmin + 10, ymin + 20), fontType, 0.5, color)
-            cv2.putText(depthFrameColor, f"Y: {int(depthData.spatialCoordinates.y)} mm", (xmin + 10, ymin + 35), fontType, 0.5, color)
-            cv2.putText(depthFrameColor, f"Z: {int(depthData.spatialCoordinates.z)} mm", (xmin + 10, ymin + 50), fontType, 0.5, color)
-            out += "------------------------------------------------------------------------------------\n"
-            out += f"Timestamp: {str(inDepth.getTimestamp())}, Average depth: {depthData.spatialCoordinates.z} mm\n"
+        if inDepthAvg is not None:
+            spatialData = inDepthAvg.getSpatialLocations()
+            for depthData in spatialData:
+                roi = depthData.config.roi
+                roi = roi.denormalize(width=depthFrameColor.shape[1], height=depthFrameColor.shape[0])
+                xmin = int(roi.topLeft().x)
+                ymin = int(roi.topLeft().y)
+                xmax = int(roi.bottomRight().x)
+                ymax = int(roi.bottomRight().y)
+    
+                fontType = cv2.FONT_HERSHEY_TRIPLEX
+                cv2.rectangle(depthFrameColor, (xmin, ymin), (xmax, ymax), color, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX)
+                cv2.putText(depthFrameColor, f"X: {int(depthData.spatialCoordinates.x)} mm", (xmin + 10, ymin + 20), fontType, 0.5, color)
+                cv2.putText(depthFrameColor, f"Y: {int(depthData.spatialCoordinates.y)} mm", (xmin + 10, ymin + 35), fontType, 0.5, color)
+                cv2.putText(depthFrameColor, f"Z: {int(depthData.spatialCoordinates.z)} mm", (xmin + 10, ymin + 50), fontType, 0.5, color)
+                out += "------------------------------------------------------------------------------------\n"
+                out += f"Timestamp: {str(inDepth.getTimestamp())}, Average depth: {depthData.spatialCoordinates.z} mm\n"
 
         info = q_sysinfo.tryGet()
         if info is not None:
