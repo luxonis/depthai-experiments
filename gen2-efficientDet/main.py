cf0c6f6657b6878713507edb7d3c2a6f1ffbfcc9
Erol444 <erol123444@gmail.com>
2021-05-11 23:29:35 +0100

    added efficientDet demo gif/video, replaced model for the one compiled with 6shaves, added readme and a bit of code cleanup

--- gen2-efficientDet/main.py
+++ gen2-efficientDet/main.py
@@ -16,11 +16,9 @@ class FPSHandler:
         self.timestamp = time.time()
         self.start = time.time()
         self.frame_cnt = 0
-
     def next_iter(self):
         self.timestamp = time.time()
         self.frame_cnt += 1
-
     def fps(self):
         return self.frame_cnt / (self.timestamp - self.start)
 
@@ -29,23 +27,22 @@ camRgb.setPreviewSize(SHAPE, SHAPE)
 camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
 camRgb.setInterleaved(False)
 camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
-camRgb.setFp16(True)
-
-# Send bouding box from the NN to host via XLink
-rgb_xout = p.createXLinkOut()
-rgb_xout.setStreamName("rgb")
-camRgb.preview.link(rgb_xout.input)
+camRgb.setFp16(True) # Model requires FP16 input
 
 # NN that detects faces in the image
 nn = p.createNeuralNetwork()
-nn.setBlobPath(str(Path("models/efficientdet_lite0.blob").resolve().absolute()))
+nn.setBlobPath(str(Path("models/efficientdet_lite0_2021.3_6shaves.blob").resolve().absolute()))
 nn.setNumInferenceThreads(2)
 camRgb.preview.link(nn.input)
 
-# Send bouding box from the NN to host via XLink
+# Send bouding box from the NN to the host via XLink
 nn_xout = p.createXLinkOut()
 nn_xout.setStreamName("nn")
 nn.out.link(nn_xout.input)
+# Send rgb frames to the host
+rgb_xout = p.createXLinkOut()
+rgb_xout.setStreamName("rgb")
+nn.passthrough.link(rgb_xout.input)
 
 # Pipeline is defined, now we can connect to the device
 with dai.Device(p) as device:
@@ -53,20 +50,21 @@ with dai.Device(p) as device:
     qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
     qNn = device.getOutputQueue(name="nn", maxSize=4, blocking=False)
     fps = FPSHandler()
+    shape = (3, SHAPE, SHAPE)
 
     while True:
         inRgb = qRgb.get()
-        shape = (3, SHAPE, SHAPE)
         data = inRgb.getData()
-        # TODO: FIx this mess
-        frame = np.array(data).astype(np.uint8).view(np.float16).reshape(shape).transpose(1, 2, 0).astype(np.uint8).copy()
+        # Model needs FP16 so we have to convert color frame back to U8 on the host
+        frame = np.array(data).view(np.float16).reshape(shape).transpose(1, 2, 0).astype(np.uint8).copy()
 
         data = qNn.tryGet()
-
         if data is not None:
-            # for layer in data.getAllLayers(): print(f"Layer name: {layer.name}, Type: {layer.dataType}, Dimensions: {layer.dims}, Offset: {layer.offset}")
             fps.next_iter()
             cv2.putText(frame, "Fps: {:.2f}".format(fps.fps()), (2, SHAPE - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color=(255, 255, 255))
+
+            # You can use the line below to print all the (output) layers and their info
+            # [print(f"Layer name: {l.name}, Type: {l.dataType}, Dimensions: {l.dims}") for l in data.getAllLayers()]
 
             bb = np.array(data.getLayerFp16('Identity')).reshape(25, 4)
             label = data.getLayerInt32('Identity_1')
@@ -75,12 +73,12 @@ with dai.Device(p) as device:
             for i in range(len(conf)):
                 if CONF_THRESHOLD < conf[i]:
                     bb_det = bb[i]
-                    # print("original BB output:", bb_det)
+                    # Limit the bounding box to 0..1
                     bb_det[bb_det > 1] = 1
                     bb_det[bb_det < 0] = 0
-                    # print("min/max limited BB:", bb_det)
                     xy_min = (int(bb_det[1]*SHAPE), int(bb_det[0]*SHAPE))
                     xy_max = (int(bb_det[3]*SHAPE), int(bb_det[2]*SHAPE))
+                    # Display detection's BB, label and confidence on the frame
                     cv2.rectangle(frame, xy_min , xy_max, (255, 0, 0), 2)
                     cv2.putText(frame, coco_90[label[i]], (xy_min[0] + 10, xy_min[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
                     cv2.putText(frame, f"{int(conf[i] * 100)}%", (xy_min[0] + 10, xy_min[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
