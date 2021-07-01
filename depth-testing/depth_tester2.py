65df55d92b3a7f13dd755227e42ad395cbf731f5
Sachin <sachin.guruswamy@gmail.com>
2021-07-01 13:54:03 -0700

    Converted to Mtrs and added RMSE error

--- depth-testing/depth_tester2.py
+++ depth-testing/depth_tester2.py
@@ -5,6 +5,7 @@ import time
 import cv2
 import argparse
 import depthai as dai
+import math
 
 X = np.random.rand(100, 3)*10
 Y = np.random.rand(100, 3)*5
@@ -14,7 +15,7 @@ ebox = None
 color = (255, 0, 0)
 parser = argparse.ArgumentParser()
 parser.add_argument("-gt", "--groundTruth", type=float,required=True,
-                            help="Set the actual disatance of the destination flat textured surface in cm")
+                            help="Set the actual disatance of the destination flat textured surface in mtrs")
 args = parser.parse_args()
 gtNormal = np.array([0, 0, 1], dtype=np.float32)
 gtD = - args.groundTruth
@@ -79,6 +80,9 @@ def on_mouse(event, x, y, flags, params):
     elif event == cv2.EVENT_LBUTTONUP:
         print('End Mouse Position: '+str(x)+', '+str(y))
         ebox = (x, y)
+        temp_start = (min(sbox[0], ebox[0]), min(sbox[1], ebox[1]))
+        ebox = (max(sbox[0], ebox[0]), max(sbox[1], ebox[1]))
+        sbox = temp_start
         # boxes.append(ebox)
 
 def fitPlaneLTSQ(XYZ):
@@ -95,7 +99,7 @@ def fitPlaneLTSQ(XYZ):
 
 # Plot Setup
 plt.ion()
-x, y, z = [],[], []
+x, y, z = [], [], []
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 sc = ax.scatter(x, y, z)
@@ -103,7 +107,7 @@ ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
 ax.set_zlim(10,400)
-plane = None
+plotFitPlane = None
 
 # stereo mode configs
 lrcheck = True
@@ -174,82 +178,92 @@ with dai.Device(pipeline) as device:
             break
 
         if sbox is not None and ebox is not None:
-            coord = pixel_coord_np(*sbox, *ebox)
+            print("Start - {} and end - {}".format(sbox, ebox))
 
-            temp = depth_img.copy()
-            temp = temp[sbox[1] : ebox[1], sbox[0] : ebox[0]]
-            cam_coords = np.dot(inter_conv, coord) * temp.flatten() * 0.1 # [x, y, z]
-            # points = Points([[0, 0, 0], [1, 3, 5], [-5, 6, 3], [3, 6, 7], [-2, 6, 7]])
-            print(cam_coords.shape)
-            subsampled_pixels = []
-            subsampled_pixels_depth = []
+            coord = pixel_coord_np(*sbox, *ebox)
+            roi = depth_img.copy()
+            roi = roi[sbox[1] : ebox[1], sbox[0] : ebox[0]]
+            
+            camCoords = np.dot(inter_conv, coord) * roi.flatten() / 1000.0 # [x, y, z]
+            # Removing Zeros from coordinates
+            validCamCoords = np.delete(camCoords, np.where(camCoords[2, :] == 0.0), axis=1)
+            # Removing outliers from Z coordinates. top and bottoom 0.5 percentile of valid depth
+            validCamCoords = np.delete(validCamCoords, np.where(validCamCoords[2, :] <= np.percentile(validCamCoords[2, :], 0.5)), axis=1)
+            validCamCoords = np.delete(validCamCoords, np.where(validCamCoords[2, :] >= np.percentile(validCamCoords[2, :], 99.5)), axis=1)
+
+            # Subsampling 4x4 grid points in the selected ROI
+            subsampledPixels = []
+            subsampledPixelsDepth = []
             x_diff = ebox[0] - sbox[0]
             y_diff = ebox[1] - sbox[1]
-            print("Start - {} and end - {}".format(sbox, ebox))
             for i in range(4):
-                print(i)
                 x_loc = sbox[0] + int(x_diff * (i/3)) 
                 for j in range(4):
-                    y_loc = sbox[1] + int(y_diff * (j/3))         
-                    subsampled_pixels.append([x_loc, y_loc, 1])
+                    y_loc = sbox[1] + int(y_diff * (j/3))
+                    subsampledPixels.append([x_loc, y_loc, 1])
                     if depth_img[y_loc, x_loc] == 0:
                         x_loc, y_loc = search_depth(x_loc, y_loc, depth_img)
-                    subsampled_pixels_depth.append(depth_img[y_loc, x_loc])
-                    print("Coordinate at {}, {} is {} & {} with depth of {}".format(i, j, x_loc, y_loc, depth_img[y_loc, x_loc]))
-            subsampled_pixels_depth = np.array(subsampled_pixels_depth)
-            subsampled_pixels = np.array(subsampled_pixels).transpose()
+                    subsampledPixelsDepth.append(depth_img[y_loc, x_loc])
+                    # print("Coordinate at {}, {} is {} & {} with depth of {}".format(i, j, x_loc, y_loc, depth_img[y_loc, x_loc]))
 
-            sub_points3D = np.dot(inter_conv, subsampled_pixels) * subsampled_pixels_depth.flatten() * 0.1 # [x, y, z]
-            print(sub_points3D.shape)
-            print(sub_points3D.transpose())
+            subsampledPixelsDepth = np.array(subsampledPixelsDepth)
+            subsampledPixels = np.array(subsampledPixels).transpose()
+
+            sub_points3D = np.dot(inter_conv, subsampledPixels) * subsampledPixelsDepth.flatten() / 1000.0 # [x, y, z]
             sub_points3D = sub_points3D.transpose()
-            print(sub_points3D.shape)
-            # points = Points(sub_points3D.transpose())
-            sc._offsets3d = (sub_points3D[:,0], sub_points3D[:,1], sub_points3D[:,2])
-
+            sc._offsets3d = (sub_points3D[:, 0], sub_points3D[:, 1], sub_points3D[:, 2])
+            
+            # Fitting the plane to the subsampled RPOI points
             c, normal = fitPlaneLTSQ(sub_points3D)
             maxx = np.max(sub_points3D[:,0])
             maxy = np.max(sub_points3D[:,1])
             minx = np.min(sub_points3D[:,0])
             miny = np.min(sub_points3D[:,1])
 
-            point = np.array([0.0, 0.0, c])
-            d = -point.dot(normal)
+            d = -np.array([0.0, 0.0, c]).dot(normal)
+            fitPlane = (normal, d)
             xx, yy = np.meshgrid([minx, maxx], [miny, maxy])
             z = (-normal[0]*xx - normal[1]*yy - d)*1. / normal[2]
-            print(xx, yy)
-            if plane is None:
-                plane = ax.plot_surface(xx, yy, z, alpha=0.2)
+
+            # Plotting the fit plane
+            if plotFitPlane is None:
+                plotFitPlane = ax.plot_surface(xx, yy, z, alpha=0.2)
             else:
-                plane_corners = np.vstack((xx.flatten(),yy.flatten(), z.flatten(), np.ones((1,4))))
-                temp_col3 = plane_corners[:, 2].copy()
-                plane_corners[:, 2] = plane_corners[:, 3]
-                plane_corners[:, 3] = temp_col3
-                # print(xx, yy)
-                print(xx.flatten(), yy)
-                print("Before")
-                print(plane._vec)
-                plane._vec = plane_corners
-                print("After")
-                print(plane._vec)
+                fitPlaneCorners = np.vstack((xx.flatten(),yy.flatten(), z.flatten(), np.ones((1,4))))
+                temp_col3 = fitPlaneCorners[:, 2].copy()
+                fitPlaneCorners[:, 2] = fitPlaneCorners[:, 3]
+                fitPlaneCorners[:, 3] = temp_col3
+                plotFitPlane._vec = fitPlaneCorners
+
 
             print("Distance of subpixel from plane")
             print(sub_points3D[0].dot(normal) + d)
 
-
             plt.draw()
-            print("Normal ~>")
-            print(normal)
-            print(d)
-            print("Finding distance from all the points to the plane")
-            print(cam_coords.shape)
+
+            # Calculating the error
             planeOffsetError = 0
             gtOffsetError = 0
+            planeRmsOffsetError = 0
+            gtRmsOffsetError = 0
 
-            for roi_point in cam_coords.transpose():
-                planeOffsetError += roi_point.dot(normal) + d
-                gtOffsetError += roi_point.dot(gtPlane[0]) + gtPlane[1]
-            print("Total Distance from the fitted plane = {}, Avg distance from the fitted plane = {}".format(planeOffsetError, planeOffsetError / cam_coords.shape[1]))
-            print("Total Distance from the GT plane = {}, Avg distance from the GT plane = {}".format(gtOffsetError, gtOffsetError / cam_coords.shape[1]))
+            for roiPoint in validCamCoords.transpose():
+                fitPlaneDist = roiPoint.dot(normal) + d
+                gtPlaneDist = roiPoint.dot(gtPlane[0]) + gtPlane[1]
+                planeOffsetError += fitPlaneDist
+                gtOffsetError += gtPlaneDist
+                planeRmsOffsetError += fitPlaneDist**2
+                gtRmsOffsetError += gtPlaneDist**2
+
+            print("Plane Fit MSE: Avg depth pixels deviation from the calculated Plane Fit = {}".format(planeOffsetError / validCamCoords.shape[1]))
+            print("GT Plane MSE: Avg depth pixels deviation from the GT plane = {}".format(gtOffsetError / validCamCoords.shape[1]))
+            print("Plane Fit RMSE: Avg distance of points in ROI from the fitted plane = {}".format(math.sqrt(planeRmsOffsetError / validCamCoords.shape[1])))
+            print("GT Plane RMSE: Avg distance of points in ROI from the GT plane = {}".format(math.sqrt(gtRmsOffsetError / validCamCoords.shape[1])))
             
+            totalPixels = (ebox[0] - sbox[0]) * (ebox[1] - sbox[1])
+            flatRoi = roi.flatten()
+            sampledPixels = np.delete(flatRoi, np.where(flatRoi == 0))
+            fill_rate = 100 * sampledPixels.shape[0] / totalPixels
+            print('fill rate is {}'.format(fill_rate))
+
             plt.pause(0.1)
