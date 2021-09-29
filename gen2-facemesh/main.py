f6c0d0d23e2e86a5434c65347cbe365ce0729138
tersekmatija <mt2421@student.uni-lj.si>
2021-09-29 14:13:02 +0200

    Add gen2 face mesh

---
+++ gen2-facemesh/main.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+
+import cv2
+import depthai as dai
+import argparse
+import time
+import numpy as np
+
+'''
+YoloV5 object detector running on selected camera.
+Run as:
+python3 -m pip install -r requirements.txt
+python3 main.py -cam rgb
+Possible input choices (-cam):
+'rgb', 'left', 'right'
+
+Blob is taken from ML training examples:
+https://github.com/luxonis/depthai-ml-training/tree/master/colab-notebooks
+
+You can clone the YoloV5_training.ipynb notebook and try training the model yourself.
+
+'''
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-conf", "--confidence_thresh", help="set the confidence threshold", default=10, type=float)
+
+args = parser.parse_args()
+CONF_THRESH = args.confidence_thresh
+NN_PATH = "models/face_landmark_openvino_2021.4_6shave.blob"
+NN_WIDTH, NN_HEIGHT = 192, 192
+PREVIEW_WIDTH, PREVIEW_HEIGHT = 416, 416
+
+
+# Start defining a pipeline
+pipeline = dai.Pipeline()
+pipeline.setOpenVINOVersion(version = dai.OpenVINO.VERSION_2021_4)
+
+
+# Color camera
+cam = pipeline.createColorCamera()
+cam.setPreviewSize(PREVIEW_WIDTH, PREVIEW_HEIGHT)
+cam.setInterleaved(False)
+#cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+cam.setFps(30)
+
+
+# Neural Network for Landmark detection
+nn = pipeline.createNeuralNetwork()
+nn.setBlobPath(NN_PATH)
+nn.setNumPoolFrames(4)
+nn.input.setBlocking(False)
+nn.setNumInferenceThreads(2)
+
+
+# Image Manip
+manip = pipeline.createImageManip()
+manip.initialConfig.setResizeThumbnail(NN_WIDTH, NN_HEIGHT)
+manip.initialConfig.setFrameType(dai.ImgFrame.Type.RGB888p)
+
+
+# Create outputs
+
+xin_rgb = pipeline.createXLinkIn()
+xin_rgb.setStreamName("xin")
+
+
+xout_rgb = pipeline.createXLinkOut()
+xout_rgb.setStreamName("cam")
+xout_rgb.input.setBlocking(False)
+
+xout_nn = pipeline.createXLinkOut()
+xout_nn.setStreamName("nn")
+xout_nn.input.setBlocking(False)
+
+
+# Linking
+cam.preview.link(xout_rgb.input)
+cam.preview.link(manip.inputImage)
+manip.out.link(nn.input)
+nn.out.link(xout_nn.input)
+
+
+# Pipeline defined, now the device is assigned and pipeline is started
+with dai.Device(pipeline) as device:
+
+    # Output queues
+    q_cam = device.getOutputQueue(name="cam", maxSize=4)
+    q_nn = device.getOutputQueue(name="nn", maxSize=4)
+
+    # FPS Init
+    start_time = time.time()
+    counter = 0
+    fps = 0
+    layer_info_printed = False
+
+    while True:
+        in_cam = q_cam.get()
+        in_nn = q_nn.get()
+
+        # Get frame
+        frame = in_cam.getCvFrame()
+
+        # Get outputs
+        score = np.array(in_nn.getLayerFp16('conv2d_31')).reshape((1,))
+        score = score[0]
+        landmarks = np.array(in_nn.getLayerFp16('conv2d_21')).reshape((468, 3))
+
+        if score > CONF_THRESH:
+            ldms = landmarks.copy()
+            ldms *= np.array([PREVIEW_WIDTH/NN_WIDTH, PREVIEW_HEIGHT/NN_HEIGHT, 1])
+
+            for ldm in ldms:
+                cv2.circle(frame, (int(ldm[0]), int(ldm[1])), 1, (255, 0, 0) if ldm[2] > 0 else (255, 255, 0), 1)
+
+        # Show FPS and score
+        color_black, color_white = (0, 0, 0), (255, 255, 255)
+        label_fps = "Fps: {:.2f}".format(fps)
+        label_count = "Score: {:.2f}".format(score)
+
+        (w1, h1), _ = cv2.getTextSize(label_fps, cv2.FONT_HERSHEY_TRIPLEX, 0.4, 1)
+        (w2, h2), _ = cv2.getTextSize(label_count, cv2.FONT_HERSHEY_TRIPLEX, 0.4, 1)
+
+        cv2.rectangle(frame, (0, frame.shape[0] - h1 - 6), (w1 + 2, frame.shape[0]), color_white, -1)
+        cv2.rectangle(frame, (0, 0), (w2 + 2, h2 + 6), color_white, -1)
+
+        cv2.putText(frame, label_fps, (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX,
+                    0.4, color_black)
+        cv2.putText(frame, label_count, (2, 12), cv2.FONT_HERSHEY_TRIPLEX,
+                    0.4, color_black)
+
+        cv2.imshow("Landmarks", frame)
+
+        counter += 1
+        if (time.time() - start_time) > 1:
+            fps = counter / (time.time() - start_time)
+
+            counter = 0
+            start_time = time.time()
+
+
+        if cv2.waitKey(1) == ord('q'):
+            break
