73df6e7f80831fd937cc44a7547cc9a52a4d45fd
Erol444 <erol123444@gmail.com>
2021-11-30 18:23:52 +0100

    Added MJPEG option (on by default), added demo video and specified requirements

--- gen2-lossless-zooming/README.md
+++ gen2-lossless-zooming/README.md
@@ -1,10 +1,14 @@
 # Lossless Zooming
 
-This demo shows how you can achieve lossless zooming on the device. Demo will zoom into the first face it detects. It will crop 4K frames into 1080P, centered around the face.
+This demo shows how you can achieve lossless zooming on the device. Demo will zoom into the first face it detects. It will crop 4K frames into 1080P, centered around the face. Demo uses [face-detection-retail-0004](https://docs.openvino.ai/latest/omz_models_model_face_detection_retail_0004.html) NN model.
 
 ## Demo
 
-![Lossless Zooming]()
+![Lossless Zooming](https://user-images.githubusercontent.com/18037362/144095838-d082040a-9716-4f8e-90e5-15bcb23115f9.gif)
+
+### MJPEG
+
+You can turn `MJPEG` on or off. It's set to `True` by default, so cropped 1080P stream will get encoded into MJPEG on the device. On the host, it will just get decoded and shown to the user, but you could also save the MJPEG stream or stream it elsewhere.
 
 
 ## Installation
@@ -19,12 +23,4 @@ Run the application
 
 ```
 python3 main.py
-```
-
-To see the streamed frames, open [localhost:8090](http://localhost:8090).  This works in Chrome, but not Firefox or Safari.
-
-To see the streamed data, use
-
-```
-nc localhost 8070
 ```
--- gen2-lossless-zooming/main.py
+++ gen2-lossless-zooming/main.py
@@ -1,10 +1,9 @@
 import depthai as dai
 import cv2
-import numpy as np
-import time
 import blobconverter
 
-print(dai.__file__)
+# Stream MJPEG from the device, useful for saving / forwarding stream
+MJPEG = False
 
 # Constants
 SCENE_SIZE = (1920, 1080)
@@ -31,7 +30,7 @@ cam.preview.link(mobilenet.input)
 # Script node
 script = pipeline.create(dai.node.Script)
 mobilenet.out.link(script.inputs['dets'])
-script.setScript("""
+script.setScript(f"""
 ORIGINAL_SIZE = (3840, 2160) # 4K
 SCENE_SIZE = (1920, 1080) # 1080P
 x_arr = []
@@ -69,7 +68,7 @@ while True:
     coords = dets[0] # take first
     # Get detection center
     x = (coords.xmin + coords.xmax) / 2 * ORIGINAL_SIZE[0]
-    y = (coords.ymin + coords.ymax) / 2 * ORIGINAL_SIZE[1] + 200
+    y = (coords.ymin + coords.ymax) / 2 * ORIGINAL_SIZE[1] + 100
 
     x_avg, y_avg = average_filter(x,y)
 
@@ -77,18 +76,27 @@ while True:
     rect.size = size
     rect.center = Point2f(x_avg, y_avg)
     cfg.setCropRotatedRect(rect, False)
+    {"cfg.setFrameType(ImgFrame.Type.NV12)" if MJPEG else ""}
     node.io['cfg'].send(cfg)
 """)
 crop_manip = pipeline.create(dai.node.ImageManip)
 crop_manip.setMaxOutputFrameSize(3110400)
 crop_manip.initialConfig.setResize(1920, 1080)
+if MJPEG:
+    crop_manip.initialConfig.setFrameType(dai.RawImgFrame.Type.NV12)
 script.outputs['cfg'].link(crop_manip.inputConfig)
 cam.isp.link(crop_manip.inputImage)
 
-# Link
 xout = pipeline.createXLinkOut()
 xout.setStreamName('1080P')
-crop_manip.out.link(xout.input)
+if MJPEG:
+    videoEnc = pipeline.create(dai.node.VideoEncoder)
+    videoEnc.setDefaultProfilePreset(20, dai.VideoEncoderProperties.Profile.MJPEG)
+    crop_manip.out.link(videoEnc.input)
+    # Link
+    videoEnc.bitstream.link(xout.input)
+else:
+    crop_manip.out.link(xout.input)
 
 xoutFull = pipeline.createXLinkOut()
 xoutFull.setStreamName('full')
@@ -100,9 +108,20 @@ with dai.Device(pipeline) as device:
     # Main loop
     while True:
         if qHq.has():
-            cv2.imshow('cropped', cv2.resize(qHq.get().getCvFrame(), (640,360)))
+            frameIn = qHq.get()
+            if MJPEG:
+                # Instead of decoding, you could also save the MJPEG or stream it elsewhere. For this demo,
+                # we just want to display the stream, so we need to decode it.
+                frame = cv2.imdecode(frameIn.getData(), cv2.IMREAD_COLOR)
+            else:
+                frame = frameIn.getCvFrame()
+
+            # Remove this line if you would like to see 1080P (not downscaled)
+            frame = cv2.resize(frame, (640,360))
+
+            cv2.imshow('Lossless zoom 1080P', frame)
         if qFull.has():
-            cv2.imshow('full', qFull.get().getCvFrame())
+            cv2.imshow('Preview', qFull.get().getCvFrame())
         # Update GUI and handle keypresses
         if cv2.waitKey(1) == ord('q'):
             break
