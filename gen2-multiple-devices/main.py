bffe4303cfdc735d2742c94c045591e9449ce6c5
alex-luxonis <alex@luxonis.com>
2022-01-06 23:15:49 +0200

    gen2-multiple-devices: multithreaded init, to speedup PoE startup time `python3 main.py` ``` Found 2 devices Starting thread for device 0 192.168.100.133 Starting thread for device 1 192.168.100.63 [192.168.100.133] [180.614] [system] [warning] Calibration Data on device is empty 0 === Connected to 192.168.100.133 0    >>> MXID: 14442C102180A7D200 0    >>> Cameras: RGB 0    >>> USB speed: UNKNOWN 0    >>> Loading pipeline for: OAK-1-POE 0    >>> Pipeline started, created stream rgb-14442C102180A7D200-OAK-1-POE 1 === Connected to 192.168.100.63 1    >>> MXID: 14442C1031A3A7D200 1    >>> Cameras: RGB LEFT RIGHT 1    >>> USB speed: UNKNOWN 1    >>> Loading pipeline for: OAK-D-POE 1    >>> Pipeline started, created stream rgb-14442C1031A3A7D200-OAK-D-POE ```

--- gen2-multiple-devices/main.py
+++ gen2-multiple-devices/main.py
@@ -2,7 +2,8 @@
 
 import cv2
 import depthai as dai
-import contextlib
+import threading
+import time
 
 # This can be customized to pass multiple parameters
 def getPipeline(device_type):
@@ -28,30 +29,22 @@ def getPipeline(device_type):
     return pipeline
 
 q_rgb_list = []
+alive = True
 
-# https://docs.python.org/3/library/contextlib.html#contextlib.ExitStack
-with contextlib.ExitStack() as stack:
-    device_infos = dai.Device.getAllAvailableDevices()
-    if len(device_infos) == 0:
-        raise RuntimeError("No devices found!")
-    else:
-        print("Found", len(device_infos), "devices")
-
-    for device_info in device_infos:
-        # Note: the pipeline isn't set here, as we don't know yet what device it is.
-        # The extra arguments passed are required by the existing overload variants
-        openvino_version = dai.OpenVINO.Version.VERSION_2021_4
-        usb2_mode = False
-        device = stack.enter_context(dai.Device(openvino_version, device_info, usb2_mode))
-
+def runDevice(index, device_info):
+    # Note: the pipeline isn't set here, as we don't know yet what device it is.
+    # The extra arguments passed are required by the existing overload variants
+    openvino_version = dai.OpenVINO.Version.VERSION_2021_4
+    usb2_mode = False
+    with dai.Device(openvino_version, device_info, usb2_mode) as device:
         # Note: currently on POE, DeviceInfo.getMxId() and Device.getMxId() are different!
-        print("=== Connected to " + device_info.getMxId())
+        print(index, "=== Connected to " + device_info.getMxId())
         mxid = device.getMxId()
         cameras = device.getConnectedCameras()
         usb_speed = device.getUsbSpeed()
-        print("   >>> MXID:", mxid)
-        print("   >>> Cameras:", *[c.name for c in cameras])
-        print("   >>> USB speed:", usb_speed.name)
+        print(index, "   >>> MXID:", mxid)
+        print(index, "   >>> Cameras:", *[c.name for c in cameras])
+        print(index, "   >>> USB speed:", usb_speed.name)
 
         device_type = "unknown"
         if   len(cameras) == 1: device_type = "OAK-1"
@@ -61,19 +54,36 @@ with contextlib.ExitStack() as stack:
 
         # Get a customized pipeline based on identified device type
         pipeline = getPipeline(device_type)
-        print("   >>> Loading pipeline for:", device_type)
+        print(index, "   >>> Loading pipeline for:", device_type)
         device.startPipeline(pipeline)
 
         # Output queue will be used to get the rgb frames from the output defined above
         q_rgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
         stream_name = "rgb-" + mxid + "-" + device_type
         q_rgb_list.append((q_rgb, stream_name))
+        print(index, "   >>> Pipeline started, created stream", stream_name)
 
-    while True:
-        for q_rgb, stream_name in q_rgb_list:
-            in_rgb = q_rgb.tryGet()
-            if in_rgb is not None:
-                cv2.imshow(stream_name, in_rgb.getCvFrame())
+        # TODO better
+        while alive:
+            time.sleep(0.1)
 
-        if cv2.waitKey(1) == ord('q'):
-            break
+device_infos = dai.Device.getAllAvailableDevices()
+if len(device_infos) == 0:
+    raise RuntimeError("No devices found!")
+else:
+    print("Found", len(device_infos), "devices")
+
+for index, device_info in enumerate(device_infos):
+    print("Starting thread for device", index, device_info.getMxId())
+    t = threading.Thread(target=runDevice, args=(index, device_info,))
+    t.start()
+
+while True:
+    for q_rgb, stream_name in q_rgb_list:
+        in_rgb = q_rgb.tryGet()
+        if in_rgb is not None:
+            cv2.imshow(stream_name, in_rgb.getCvFrame())
+
+    if cv2.waitKey(1) == ord('q'):
+        alive = False
+        break
