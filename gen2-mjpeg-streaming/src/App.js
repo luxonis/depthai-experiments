191214b2bc3e158751bab20913e48aea4c5f9dda
Kamil Kuklewski <kkuklewski@gmail.com>
2022-02-28 15:44:50 +0100

    first final touch

--- gen2-mjpeg-streaming/src/App.js
+++ gen2-mjpeg-streaming/src/App.js
@@ -1,32 +1,18 @@
 import React from "react";
 import './main.scss'
-import {Tooltip, Button, Menu, Dropdown, Radio, Input, Divider} from 'antd';
+import {Tooltip, Button, Radio, Input, Divider} from 'antd';
 import {
-  QuestionCircleFilled,
-  LeftOutlined,
-  RightOutlined,
   CameraOutlined,
   FolderOutlined,
+  CaretLeftOutlined,
+  CaretRightOutlined,
 } from '@ant-design/icons';
 import recordIcon from "./svg/recording.svg"
 import {useDispatch, useSelector} from "react-redux";
-import {sendConfig, sendDynamicConfig, updateConfig} from "./store";
+import {sendConfig, sendDynamicConfig} from "./store";
+import autoIcon from "./../src/svg/auto-width.svg"
 
 const text = <span>Description</span>;
-
-const menu = (
-  <Menu>
-    <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">exposure</a>
-    </Menu.Item>
-    <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">focus length</a>
-    </Menu.Item>
-    <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">white balance</a>
-    </Menu.Item>
-  </Menu>
-);
 
 function App() {
   const config = useSelector((state) => state.app.config)
@@ -49,17 +35,6 @@ function App() {
         <Button size="large" type="text" ghost icon={<FolderOutlined/>}>
           <Tooltip placement="bottomLeft" title={text}>Change Path</Tooltip>
         </Button>
-        <Radio.Group onChange={onChange} value={value}>
-          <Radio value={1}> <Tooltip placement="bottomLeft" title={text}>
-            left
-          </Tooltip></Radio>
-          <Radio checked="true" value={2}> <Tooltip placement="bottomLeft" title={text}>
-            center
-          </Tooltip></Radio>
-          <Radio value={3}> <Tooltip placement="bottomLeft" title={text}>
-            right
-          </Tooltip></Radio>
-        </Radio.Group>
         <a href="/still" download="photo.jpg">
           <Button size="large" type="text" ghost icon={<CameraOutlined/>}>
             <Tooltip placement="bottomLeft" title={text}>
@@ -72,40 +47,55 @@ function App() {
         </Button>
       </div>
       <div className="upperRow">
-        <Dropdown overlay={menu} placement="bottomLeft" arrow>
-          <Button>set auto</Button>
-        </Dropdown>
+        {/* którą camere chcesz widzieć, obrazek jedną z trzech kamer tak jak ikonka saturacji tylko te niekatywne wyszarzone*/}
+        <div className="cameraBox">
+          <span className="cameraHeading">camera switch</span>
+          <div className="cameraSwitch">
+            <Radio.Group onChange={onChange} value={value}>
+              <Radio value={1}> <Tooltip placement="bottomLeft" title={text}>
+              </Tooltip></Radio>
+              <Radio checked="true" value={2}> <Tooltip placement="bottomLeft" title={text}>
+              </Tooltip></Radio>
+              <Radio value={3}> <Tooltip placement="bottomLeft" title={text}>
+              </Tooltip></Radio>
+            </Radio.Group>
+          </div>
+        </div>
+        {/* dodać auto checkbox pod każdą opcją zamiast tego*/}
         <ul className="cameraSettings_features">
-          <li>
-            <Tooltip placement="bottomLeft" title={text}>
-              exp
-            </Tooltip>
-            <Button icon={<LeftOutlined/>} ghost="true" onClick={() => update({values: {exp: config.values.exp - 500, iso: config.values.iso}})}/>
-            <Input size="small" value={config.values.exp} onChange={exp => updateDyn({values: {exp, iso: config.values.iso}})}/>
-            <Button icon={<RightOutlined/>} ghost="true" onClick={() => update({values: {exp: config.values.exp + 500, iso: config.values.iso}})}/>
-          </li>
-          <Divider type="vertical"/>
           <li>
             <Tooltip placement="bottomLeft" title={text}>
               iso
             </Tooltip>
-            <Button icon={<LeftOutlined/>} ghost="true" onClick={() => update({values: {exp: config.values.exp, iso: config.values.iso - 100}})}/>
+            <Button icon={<CaretLeftOutlined />} ghost="true" onClick={() => update({values: {exp: config.values.exp, iso: config.values.iso - 100}})}/>
             <Input size="small" value={config.values.iso} onChange={iso => updateDyn({values: {exp: config.values.exp, iso}})}/>
-            <Button icon={<RightOutlined/>} ghost="true" onClick={() => update({values: {exp: config.values.exp, iso: config.values.iso + 100}})}/>
+            <Button icon={<CaretRightOutlined />} ghost="true" onClick={() => update({values: {exp: config.values.exp, iso: config.values.iso + 100}})}/>
           </li>
           <li>
+            <img className="autoIcon" src={autoIcon} alt=""/>
+            <Tooltip placement="bottomLeft" title={text}>
+              exp
+            </Tooltip>
+            <Button icon={<CaretLeftOutlined />} ghost="true" onClick={() => update({values: {exp: config.values.exp - 500, iso: config.values.iso}})}/>
+            <Input size="small" value={config.values.exp} onChange={exp => updateDyn({values: {exp, iso: config.values.iso}})}/>
+            <Button icon={<CaretRightOutlined />} ghost="true" onClick={() => update({values: {exp: config.values.exp + 500, iso: config.values.iso}})}/>
+          </li>
+          <Divider type="vertical"/>
+          <li>
+            <img className="autoIcon" src={autoIcon} alt=""/>
             <Tooltip placement="bottomLeft" title={text}>
               wb
             </Tooltip>
-            <Button icon={<LeftOutlined/>} ghost="true" onClick={() => update({values: {wb: config.values.wb - 400}})}/>
+            <Button icon={<CaretLeftOutlined />} ghost="true" onClick={() => update({values: {wb: config.values.wb - 400}})}/>
             <Input size="small" value={config.values.wb} onChange={wb => updateDyn({values: {wb}})}/>
-            <Button icon={<RightOutlined/>} ghost="true" onClick={() => update({values: {wb: config.values.wb + 400}})}/>
+            <Button icon={<CaretRightOutlined />} ghost="true" onClick={() => update({values: {wb: config.values.wb + 400}})}/>
           </li>
           <li>
+            <img className="autoIcon" src={autoIcon} alt=""/>
             <Tooltip placement="bottomLeft" title={text}>f</Tooltip>
-            <Button icon={<LeftOutlined/>} ghost="true" onClick={() => update({values: {focus: config.values.focus - 3}})}/>
+            <Button icon={<CaretLeftOutlined />} ghost="true" onClick={() => update({values: {focus: config.values.focus - 3}})}/>
             <Input size="small" value={config.values.focus} onChange={focus => updateDyn({values: {focus}})}/>
-            <Button icon={<RightOutlined/>} ghost="true" onClick={() => update({values: {focus: config.values.focus + 3}})}/>
+            <Button icon={<CaretRightOutlined />} ghost="true" onClick={() => update({values: {focus: config.values.focus + 3}})}/>
           </li>
         </ul>
       </div>
