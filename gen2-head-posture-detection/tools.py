6f40f9004745fb53807e8d4391245a6a7f7d9327
Erol444 <erol123444@gmail.com>
2022-06-06 12:58:19 +0200

    Refactored head pose estimation

--- gen2-head-posture-detection/tools.py
+++ gen2-head-posture-detection/tools.py
@@ -1,85 +1,43 @@
 import numpy as np
 import cv2
-import math
 
-# World Coordinate System (UVW): fill in the 3D reference point, the model reference http://aifi.isr.uc.pt/Downloads/OpenGL/glAnthropometric3DModel.cpp
-object_pts = np.float32([[6.825897, 6.760612, 4.402142],    # Upper left corner of left eyebrow
-                         [1.330353, 7.122144, 6.903745],    # Left eyebrow right corner
-                         [-1.330353, 7.122144, 6.903745],   # Right eyebrow left corner
-                         [-6.825897, 6.760612, 4.402142],   # Upper right corner of right eyebrow
-                         [5.311432, 5.485328, 3.987654],    # Upper left corner of left eye
-                         [1.789930, 5.393625, 4.413414],    # Upper right corner of left eye
-                         [-1.789930, 5.393625, 4.413414],   # Upper left corner of right eye
-                         [-5.311432, 5.485328, 3.987654],   # Upper right corner of right eye
-                         [2.005628, 1.409845, 6.165652],    # Upper left corner of nose
-                         [-2.005628, 1.409845, 6.165652],   # Upper right corner of nose
-                         [2.774015, -2.080775, 5.048531],   # Upper left corner of mouth
-                         [-2.774015, -2.080775, 5.048531],  # Upper right corner of mouth
-                         [0.000000, -3.116408, 6.097667],   # Lower corner of mouth
-                         [0.000000, -7.415691, 4.070434]])  # Chin angle
+MIN_THRESHOLD = 15. # Degrees in yaw/pitch/roll to be considered as head movement
 
-# Camera coordinate system (XYZ): add camera internal parameters
-K = [6.5308391993466671e+002, 0.0, 3.1950000000000000e+002,
-     0.0, 6.5308391993466671e+002, 2.3950000000000000e+002,
-     0.0, 0.0, 1.0]# Equivalent to matrix [fx, 0, cx; 0, fy, cy; 0, 0, 1]
-# Image center coordinate system (uv): camera distortion parameters [k1, k2, p1, p2, k3]
-D = [7.0834633684407095e-002, 6.9140193737175351e-002, 0.0, 0.0, -1.3073460323689292e+000]
-
-# Pixel coordinate system (xy): fill in the eigen and distortion coefficient of the cam
-cam_matrix = np.array(K).reshape(3, 3).astype(np.float32)
-dist_coeffs = np.array(D).reshape(5, 1).astype(np.float32)
+bg_color = (0, 0, 0)
+color = (255, 255, 255)
+text_type = cv2.FONT_HERSHEY_SIMPLEX
+line_type = cv2.LINE_AA
+def putText(frame, text, coords, size=0.6, bold=False):
+     mult = 2 if bold else 1
+     cv2.putText(frame, text, coords, text_type, size, bg_color, 3*mult, line_type)
+     cv2.putText(frame, text, coords, text_type, size, color, 1*mult, line_type)
 
 
+def frame_norm(frame, bbox):
+    normVals = np.full(len(bbox), frame.shape[0])
+    normVals[::2] = frame.shape[1]
+    return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
 
-# Reproject the world coordinate axis of the 3D point to verify the resulting pose
-reprojectsrc = np.float32([[10.0, 10.0, 10.0],
-                           [10.0, 10.0, -10.0],
-                           [10.0, -10.0, -10.0],
-                           [10.0, -10.0, 10.0],
-                           [-10.0, 10.0, 10.0],
-                           [-10.0, 10.0, -10.0],
-                           [-10.0, -10.0, -10.0],
-                           [-10.0, -10.0, 10.0]])
+def decode_pose(yaw, pitch, roll, face_bbox, frame):
+     """
+     pitch > 0 Head down, < 0 look up
+     yaw > 0 Turn right < 0 Turn left
+     roll > 0 Tilt right, < 0 Tilt left
+     """
+     putText(frame,"pitch:{:.0f}, yaw:{:.0f}, roll:{:.0f}".format(pitch,yaw,roll),(face_bbox[0]+10-15,face_bbox[1]-15))
 
-def get_head_pose(shape):# Head pose estimation
-    # (Set of pixel coordinates) Fill in the 2D reference point, and the notes follow https://ibug.doc.ic.ac.uk/resources/300-W/
-    # 0 Left eyebrow upper left corner
-    # 1 Left eyebrow right corner
-    # 2 Right eyebrow upper left corner
-    # 3 Right eyebrow upper right corner
-    # 4 Left eye upper left corner
-    # 5 Left eye upper right corner
-    # 6 Right eye upper left corner
-    # 7 Upper right corner of the right eye
-    # 8 Upper left corner of the nose
-    # 9 Upper right corner of the nose
-    # 10 Upper left corner
-    # 11 Upper right corner of the mouth
-    # 12 Lower central corner of the mouth
-    # 13 Chin corner
-    image_pts = np.float32([shape[0], shape[1], shape[2], shape[3], shape[4],
-                            shape[5], shape[6], shape[7], shape[8], shape[9],
-                            shape[10], shape[11], shape[12], shape[13]])
-    # solvePnP Calculate the pose-solve the rotation and translation matrix：
-    # rotation_vec Represents the rotation matrix，translation_vec Represents the translation matrix，cam_matrix Corresponds to K matrix，dist_coeffs Corresponds to D matrix。
-    _, rotation_vec, translation_vec = cv2.solvePnP(object_pts, image_pts, cam_matrix, dist_coeffs)
-    # projectPointsRe-projection error: the distance between the original 2d point and the reprojected 2d point (input 3d point, camera internal parameters, camera distortion, r, t, output reprojected 2d point)
-    reprojectdst, _ = cv2.projectPoints(reprojectsrc, rotation_vec, translation_vec, cam_matrix,dist_coeffs)
-    reprojectdst = tuple(map(tuple, reprojectdst.reshape(8, 2)))# Display in 8 rows and 2 columns
+     vals = np.array([abs(pitch),abs(yaw),abs(roll)])
+     max_index = np.argmax(vals)
 
-    # Calculate Euler angle calc euler angle
-    # reference https://docs.opencv.org/2.4/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html#decomposeprojectionmatrix
-    rotation_mat, _ = cv2.Rodrigues(rotation_vec)# Rodriguez formula (convert the rotation matrix to a rotation vector)
-    pose_mat = cv2.hconcat((rotation_mat, translation_vec))# Horizontal splicing, vconcat vertical splicing
-    # decomposeProjectionMatrix Decompose projection matrix into rotation matrix and camera matrix
-    _, _, _, _, _, _, euler_angle = cv2.decomposeProjectionMatrix(pose_mat)
-    
-    pitch, yaw, roll = [math.radians(_) for _ in euler_angle]
- 
- 
-    pitch = math.degrees(math.asin(math.sin(pitch)))
-    roll = -math.degrees(math.asin(math.sin(roll)))
-    yaw = math.degrees(math.asin(math.sin(yaw)))
-    # print('pitch:{}, yaw:{}, roll:{}'.format(pitch, yaw, roll))
+     if vals[max_index] < MIN_THRESHOLD: return
 
-    return reprojectdst, euler_angle, pitch, yaw, roll# Projection error, Euler angle
+     if max_index == 0:
+          if pitch > 0: txt = "Look down"
+          else: txt = "Look up"
+     elif max_index == 1:
+          if yaw > 0: txt = "Turn right"
+          else: txt = "Turn left"
+     elif max_index == 2:
+          if roll > 0: txt = "Tilt right"
+          else: txt = "Tilt left"
+     putText(frame,txt, (face_bbox[0]+10,face_bbox[1]+30), size=1, bold=True)
