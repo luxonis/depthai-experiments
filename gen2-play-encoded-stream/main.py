20c3e98271196e235eac4db1f58ef55980297974
alex-luxonis <alex@luxonis.com>
2022-06-14 19:18:48 +0300

    gen2-play-encoded-stream: add `-res`/-`enc`/`-fps` options. Usage:
    usage: main.py [-h] [-res {13mp,12mp,4k,1080}] [-enc {h265,h264,jpeg}] [-fps FPS]
    
    options:
      -h, --help            show this help message and exit
      -res {13mp,12mp,4k,1080}, --resolution {13mp,12mp,4k,1080}
                            Select color camera resolution. Default: 4k
      -enc {h265,h264,jpeg}, --encode {h265,h264,jpeg}
                            Select encoding format. Default: h264
      -fps FPS, --fps FPS   Camera FPS to configure. Default: 30.0

--- gen2-play-encoded-stream/main.py
+++ gen2-play-encoded-stream/main.py
@@ -3,6 +3,31 @@
 import depthai as dai
 import subprocess as sp
 from os import name as osName
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-res', '--resolution', default='4k',
+                    choices={'1080', '4k', '12mp', '13mp'},
+                    help="Select color camera resolution. Default: %(default)s")
+parser.add_argument('-enc', '--encode', default='h264',
+                    choices={'h264', 'h265', 'jpeg'},
+                    help="Select encoding format. Default: %(default)s")
+parser.add_argument('-fps', '--fps', type=float, default=30.0,
+                    help="Camera FPS to configure. Default: %(default)s")
+args = parser.parse_args()
+
+res_opts = {
+    '1080': dai.ColorCameraProperties.SensorResolution.THE_1080_P,
+    '4k':   dai.ColorCameraProperties.SensorResolution.THE_4_K,
+    '12mp': dai.ColorCameraProperties.SensorResolution.THE_12_MP,
+    '13mp': dai.ColorCameraProperties.SensorResolution.THE_13_MP,
+}
+
+enc_opts = {
+    'h264': dai.VideoEncoderProperties.Profile.H264_MAIN,
+    'h265': dai.VideoEncoderProperties.Profile.H265_MAIN,
+    'jpeg': dai.VideoEncoderProperties.Profile.MJPEG,
+}
 
 # Create pipeline
 pipeline = dai.Pipeline()
@@ -11,13 +36,13 @@ pipeline = dai.Pipeline()
 camRgb = pipeline.create(dai.node.ColorCamera)
 videoEnc = pipeline.create(dai.node.VideoEncoder)
 xout = pipeline.create(dai.node.XLinkOut)
-
-xout.setStreamName("h264")
+xout.setStreamName("enc")
 
 # Properties
 camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
-camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
-videoEnc.setDefaultProfilePreset(camRgb.getVideoSize(), camRgb.getFps(), dai.VideoEncoderProperties.Profile.H264_MAIN)
+camRgb.setResolution(res_opts[args.resolution])
+camRgb.setFps(args.fps)
+videoEnc.setDefaultProfilePreset(camRgb.getFps(), enc_opts[args.encode])
 
 # Linking
 camRgb.video.link(videoEnc.input)
@@ -47,7 +72,7 @@ except:
 # Connect to device and start pipeline
 with dai.Device(pipeline) as device:
     # Output queue will be used to get the encoded data from the output defined above
-    q = device.getOutputQueue(name="h264", maxSize=30, blocking=True)
+    q = device.getOutputQueue(name="enc", maxSize=30, blocking=True)
 
     try:
         while True:
