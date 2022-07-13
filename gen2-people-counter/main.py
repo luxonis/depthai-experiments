bb291cbc36422ff96e27bf0e19318825c7e9ced8
Erol444 <erol123444@gmail.com>
2022-07-13 18:50:03 +0200

    Updated pedestrian reid demo and people counter demo

--- gen2-people-counter/main.py
+++ gen2-people-counter/main.py
@@ -1,73 +1,32 @@
 #!/usr/bin/env python3
-from pathlib import Path
 
-import blobconverter
-import cv2
+from depthai_sdk import Camera
 import depthai as dai
-import numpy as np
-import argparse
-from time import monotonic
-import itertools
 
-from depthai_sdk import PipelineManager, NNetManager, PreviewManager
-from depthai_sdk import cropToAspectRatio
+with Camera(input='images/') as cam:
+    rgb_camera = cam.create_camera(args=True, out=True) # args = True to enable option to stream from mp4
+    person_det = cam.create_nn('person-detection-retail-0013', rgb_camera, out=True) # Path to json
+    
+    cam.start()
+    
+    visualizer = cam.create_visualizer(rgb_camera, person_det)
 
-parentDir = Path(__file__).parent
+    num = ''
+    def cb_calc(msg: dai.ImgDetections):
+        global num
+        # Calc whether person has gone up/down/left/right
+        num = len(msg)
 
-#=====================================================================================
-# To use a different NN, change `size` and `nnPath` here:
-size = (544, 320)
-nnPath = blobconverter.from_zoo("person-detection-retail-0013", shaves=8)
-#=====================================================================================
+    def vis_callback(frame):
+        global num
+        # Add some overlay to the frame
+        visualizer.print(f"Number of people: {num}") # Write some stuff to the frame
+        visualizer.show(frame) # Now do the cv2.imshow
 
-# Labels
-labelMap = ["background", "person"]
+    cam.create_msg_callback(person_det, cb_calc)
 
-# Get argument first
-parser = argparse.ArgumentParser()
-parser.add_argument('-nn', '--nn', type=str, help=".blob path")
-parser.add_argument('-i', '--image', type=str,
-                    help="Path to an image file to be used for inference (conflicts with -cam)")
-parser.add_argument('-cam', '--camera', action="store_true",
-                    help="Use DepthAI RGB camera for inference (conflicts with -vid)")
-args = parser.parse_args()
+    while cam.running():
+        msgs = cam.get_synced_msgs()
+        visualizer.visualize(msgs, vis_callback) # Don't show frame just yet
 
-# Whether we want to use images from host or rgb camera
-IMAGE = not args.camera
-nnSource = "host" if IMAGE else "color"
-
-# Start defining a pipeline
-pm = PipelineManager()
-if not IMAGE:
-    pm.createColorCam(previewSize=size, xout=True)
-    pv = PreviewManager(display=["color"], nnSource=nnSource)
-
-nm = NNetManager(inputSize=size, nnFamily="mobilenet", labels=labelMap, confidence=0.5)
-nn = nm.createNN(pm.pipeline, pm.nodes, blobPath=nnPath, source=nnSource)
-pm.setNnManager(nm)
-pm.addNn(nn)
-
-# Pipeline defined, now the device is connected to
-with dai.Device(pm.pipeline) as device:
-    nm.createQueues(device)
-    if IMAGE:
-        imgPaths = [args.image] if args.image else list(parentDir.glob('images/*.jpeg'))
-        og_frames = itertools.cycle([cropToAspectRatio(cv2.imread(str(imgPath)), size) for imgPath in imgPaths])
-    else:
-        pv.createQueues(device)
-
-    while True:
-        if IMAGE:
-            frame = next(og_frames).copy()
-            nm.sendInputFrame(frame)
-        else:
-            pv.prepareFrames(blocking=True)
-            frame = pv.get("color")
-
-        nn_data = nm.decode(nm.outputQueue.get())
-        nm.draw(frame, nn_data)
-        cv2.putText(frame, f"People count: {len(nn_data)}", (5, 30), cv2.FONT_HERSHEY_TRIPLEX, 1, (0,0,255))
-        cv2.imshow("color", frame)
-
-        if cv2.waitKey(3000 if IMAGE else 1) == ord('q'):
-            break
+        cam.poll() # name tbd (does its GUI stuff)
