48fc2df8516ae09c914e1ea2e07d725bc82787ae
Erol444 <erol123444@gmail.com>
2022-08-24 14:48:40 +0200

    Updated blob inputs ouptus script

--- random-scripts/blob_inputs_ouputs.py
+++ random-scripts/blob_inputs_ouputs.py
@@ -5,7 +5,6 @@ modelPath = blobconverter.from_zoo(name="mobilenet-ssd")
 
 blob = dai.OpenVINO.Blob(modelPath)
 print('Inputs')
-[print(f"Name: {name}, Type: {vec.dataType}, Shape: {vec.dims}") for name, vec in blob.networkInputs.items()]
+[print(f"Name: {name}, Type: {vec.dataType}, Shape: {vec.dims} ({vec.order})") for name, vec in blob.networkInputs.items()]
 print('Outputs')
-[print(f"Name: {name}, Type: {vec.dataType}, Shape: {vec.dims}") for name, vec in blob.networkOutputs.items()]
-
+[print(f"Name: {name}, Type: {vec.dataType}, Shape: {vec.dims} ({vec.order})") for name, vec in blob.networkOutputs.items()]
