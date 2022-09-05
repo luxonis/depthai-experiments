61305cf1ef7ccc74db9d0da5a8e4839606964c2e
Erol444 <erol123444@gmail.com>
2022-09-05 23:36:08 +0200

    Update people-counter demo. Works as expected with latest depthai-sdk

--- gen2-people-counter/main.py
+++ gen2-people-counter/main.py
@@ -1,32 +1,18 @@
 #!/usr/bin/env python3
 
-from depthai_sdk import Camera
-import depthai as dai
+import cv2
+from depthai_sdk import OakCamera, BaseVisualizer, FramePosition
 
-with Camera(input='images/') as cam:
-    rgb_camera = cam.create_camera(args=True, out=True) # args = True to enable option to stream from mp4
-    person_det = cam.create_nn('person-detection-retail-0013', rgb_camera, out=True) # Path to json
-    
-    cam.start()
-    
-    visualizer = cam.create_visualizer(rgb_camera, person_det)
+with OakCamera(recording='images/') as oak:
+    color = oak.create_camera('color', out='color')
+    nn = oak.create_nn('person-detection-retail-0013', color, out='dets')
+    oak.replay.setFps(3)
 
-    num = ''
-    def cb_calc(msg: dai.ImgDetections):
-        global num
-        # Calc whether person has gone up/down/left/right
-        num = len(msg)
+    def cb(msgs, frame):
+        num = len(msgs['dets'].detections)
+        print('New msgs! Number of people detected:', num)
+        BaseVisualizer.print(frame, f"Number of people: {num}", FramePosition.BottomMid)
+        cv2.imshow(f'frame {frame.shape[0] }', frame)
 
-    def vis_callback(frame):
-        global num
-        # Add some overlay to the frame
-        visualizer.print(f"Number of people: {num}") # Write some stuff to the frame
-        visualizer.show(frame) # Now do the cv2.imshow
-
-    cam.create_msg_callback(person_det, cb_calc)
-
-    while cam.running():
-        msgs = cam.get_synced_msgs()
-        visualizer.visualize(msgs, vis_callback) # Don't show frame just yet
-
-        cam.poll() # name tbd (does its GUI stuff)
+    oak.create_visualizer([color, nn], fps=True, callback=cb)
+    oak.start(blocking=True)
