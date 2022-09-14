9959e1a23b9242932644ed6b1821e2ae84ae2b02
Erol444 <erol123444@gmail.com>
2022-09-15 00:54:41 +0200

    Updated age-gender demo

--- gen2-age-gender/main.py
+++ gen2-age-gender/main.py
@@ -1,19 +1,32 @@
-#!/usr/bin/env python3
+from depthai_sdk import OakCamera, TwoStagePacket, AspectRatioResizeMode, Visualizer
+import depthai as dai
+import numpy as np
+import cv2
 
-from depthai_sdk import Camera
+with OakCamera() as oak:
+    color = oak.create_camera('color', out='color')
 
-with Camera() as cam:
-    rgb_camera = cam.create_camera(out=True)
-    face_det = cam.create_nn("face-detection-retail-0004", rgb_camera, spatials=True, out=True)
-    # If input from an object detection NN, crop around BB from HQ frames for new model
-    age_gender_nn = cam.create_nn("age_gender_recognition_0001", input=face_det, out=True)
+    det = oak.create_nn('face-detection-retail-0004', color, out='dets')
+    # Passthrough is enabled for debugging purposes
+    # AspectRatioResizeMode has to be CROP for 2-stage pipelines at the moment
+    det.config_nn(passthroughOut=True, aspectRatioResizeMode=AspectRatioResizeMode.CROP)
 
-    cam.start()
-    
-    face_visualizer = cam.create_visualizer(rgb_camera, face_det, age_gender_nn)
+    age_gender = oak.create_nn('age-gender-recognition-retail-0013', input=det, out='reid')
+    age_gender.config_multistage_nn(show_cropped_frames=True) # For debugging
 
-    while cam.running():
-        msgs = cam.get_synced_msgs()
-        face_visualizer.visualize(msgs)
+    def cb(packet: TwoStagePacket):
+        for det in packet.detections:
+            rec = det.nn_data
+            age = int(float(np.squeeze(np.array(rec.getLayerFp16('age_conv3')))) * 100)
+            gender = np.squeeze(np.array(rec.getLayerFp16('prob')))
+            gender_str = "woman" if gender[0] > gender[1] else "man"
+            Visualizer.putText(packet.frame, f"{gender_str}, age: {age}", (det.topLeft[0]+5, det.topLeft[1]+45), scale=0.8)
 
-        cam.poll()
+        cv2.imshow(packet.name, packet.frame)
+
+
+    # Visualize detections on the frame. Also display FPS on the frame. Don't show the frame but send the packet
+    # to the callback function (where it will be displayed)
+    oak.visualize([color, det, age_gender], fps=True, callback=cb)
+    # oak.show_graph() # Show pipeline graph, no need for now
+    oak.start(blocking=True) # This call will block until the app is stopped (by pressing 'Q' button)
