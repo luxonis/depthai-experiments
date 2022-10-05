4d8fc191c60029aa498a262ec5bed2b6a31b1c6c
Nejc Jezeršek <njezersek@gmail.com>
2022-10-05 13:36:08 +0200

    removed unnecessary transformations

--- gen2-multiple-devices/box-measurement/box_estimator.py
+++ gen2-multiple-devices/box-measurement/box_estimator.py
@@ -3,6 +3,7 @@ import open3d as o3d
 import cv2
 import copy
 import random
+import config
 
 DISTANCE_THRESHOLD_PLANE = 0.02 # Defines the maximum distance a point can have 
                                 # to an estimated plane to be considered an inlier
@@ -13,11 +14,12 @@ N_POINTS_SAMPLED_PLANE = 3 # Defines the number of points that are randomly samp
 class BoxEstimator():
 
     def __init__(self, max_distance):
-        self.raw_pcl = None
-        self.roi_pcl = None
-        self.plane_pcl = None
-        self.box_pcl = None
-        self.top_side_pcl = None
+        self.raw_pcl = o3d.geometry.PointCloud()
+        self.plane_pcl = o3d.geometry.PointCloud()
+        self.plane_outliers_pcl = o3d.geometry.PointCloud()
+        self.box_pcl = o3d.geometry.PointCloud()
+        self.top_side_pcl = o3d.geometry.PointCloud()
+        self.visualization_pcl = o3d.geometry.PointCloud()
 
         self.is_calibrated = False
         self.ground_plane_eq = None
@@ -34,7 +36,10 @@ class BoxEstimator():
 
         self.vis = o3d.visualization.Visualizer()
         self.vis.create_window()
-        self.isstarted = False
+        self.vis.get_view_control().set_constant_z_far(config.max_range*2)
+        self.vis.add_geometry(self.visualization_pcl)
+        origin = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.3, origin=[0, 0, 0])
+        self.vis.add_geometry(origin)
 
     def vizualise_box(self):
         bounding_box = self.bounding_box
@@ -42,20 +47,7 @@ class BoxEstimator():
         points_top = np.c_[bounding_box, self.height * np.ones(4)]
         box_points = np.concatenate((points_top, points_floor))
 
-        lines = [
-            [0, 4],
-            [1, 5],
-            [2, 6],
-            [3, 7],
-            [0,1],
-            [1,2],
-            [2,3],
-            [3,0],
-            [4,5],
-            [5,6],
-            [6,7],
-            [7,4],
-        ]
+        lines = [[0,4], [1,5], [2,6], [3,7], [0,1], [1,2], [2,3], [3,0], [4,5], [5,6], [6,7], [7,4]]
 
         line_set = o3d.geometry.LineSet(
             points = o3d.utility.Vector3dVector(box_points),
@@ -65,17 +57,14 @@ class BoxEstimator():
         colors = [[1,0,0] for i in range(len(lines))]
         line_set.colors = o3d.utility.Vector3dVector(colors)
 
-        if not self.isstarted:
-            self.vis.add_geometry(self.raw_pcl)
-            origin = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.3, origin=[0, 0, 0])
-            self.vis.add_geometry(origin)
-            self.isstarted = True
-        else:
-            self.vis.add_geometry(line_set, reset_bounding_box=False)
-            self.vis.update_geometry(self.raw_pcl)
-            self.vis.poll_events()
-            self.vis.update_renderer()
-            self.vis.remove_geometry(line_set, reset_bounding_box=False)
+        self.visualization_pcl.points = self.raw_pcl.points
+        self.visualization_pcl.colors = self.raw_pcl.colors
+
+        self.vis.add_geometry(line_set, reset_bounding_box=False)
+        self.vis.update_geometry(self.visualization_pcl)
+        self.vis.poll_events()
+        self.vis.update_renderer()
+        self.vis.remove_geometry(line_set, reset_bounding_box=False)
 
     def vizualise_box_2d(self, intrinsic_mat, img):
         bounding_box = self.bounding_box
@@ -92,20 +81,7 @@ class BoxEstimator():
         bbox_pcl.translate(inverse_translation)
         bbox_pcl.rotate(inverse_rot_mat, center=(0,0,0))
 
-        lines = [
-            [0, 4],
-            [1, 5],
-            [2, 6],
-            [3, 7],
-            [0,1],
-            [1,2],
-            [2,3],
-            [3,0],
-            [4,5],
-            [5,6],
-            [6,7],
-            [7,4],
-        ]
+        lines = [[0,4], [1,5], [2,6], [3,7], [0,1], [1,2], [2,3], [3,0], [4,5], [5,6], [6,7], [7,4]]
         intrinsic_mat = np.array(intrinsic_mat)
 
         # object along negative z-axis so need to correct perspective when plotting using OpenCV
@@ -126,78 +102,43 @@ class BoxEstimator():
         if len(raw_pcl.points) < 100:
             return 0,0,0 # No box
 
-        self.crop_plc()
-        if len(self.roi_pcl.points) < 100:
+        if len(self.raw_pcl.points) < 100:
             return 0,0,0 # No box
 
-        if not self.is_calibrated:
-            plane_eq, plane_inliers = self.detect_ground_plane()
-        else:
-            plane_eq = self.ground_plane_eq
-            plane_inliers = self.get_plane_inliers(plane_eq, np.asarray(self.roi_pcl.points), DISTANCE_THRESHOLD_PLANE)
+        self.detect_ground_plane()
 
-        box = self.get_box_pcl(plane_eq, plane_inliers) # TODO, check if there is a reasonable box even
+        box = self.get_box_pcl() # TODO, check if there is a reasonable box even
         if box is None:
             return 0,0,0 # No box
 
-        self.get_box_top(plane_eq)
+        self.get_box_top()
         dimensions = self.get_dimensions()
         return dimensions
 
-    def calibrate(self, raw_pcl):
-        self.raw_pcl = raw_pcl
-        if len(raw_pcl.points) < 100:
-            print("Cannot calibrate, too little points in the point cloud.")
-            return False
-
-        self.crop_plc()
-        if len(self.roi_pcl.points) < 100:
-            print("Cannot calibrate, too little points in the ROI point cloud")
-            return False
-        plane_eq, _ = self.detect_ground_plane()
-        self.ground_plane_eq = plane_eq
-        self.is_calibrated = True
-        return True
-
-    def create_rotation_matrix(self, vec_in, vec_target):
-        # Create a rotation matrix that rotates vec_in to vec_target
-        # https://math.stackexchange.com/questions/180418/calculate-rotation-matrix-to-align-vector-a-to-vector-b-in-3d
-        v = np.cross(vec_in, vec_target)
-        s = np.linalg.norm(v)
-        c = np.matmul(vec_in, vec_target)
-        v_mat = np.array([[0    ,-v[2], v[1] ],
-                        [v[2] , 0   , -v[0]],
-                        [-v[1], v[0], 0    ]])
-        R = np.identity(3) + v_mat + (np.matmul(v_mat, v_mat) * (1 / (1+c)))
-        self.rotation_matrix = R
-        return R
-
-    def crop_plc(self):
-        # Crop only for the ROI
-        raw_pcl = self.raw_pcl
-        raw_pcl_np = np.asarray(raw_pcl.points)
-
-        # Calculate point distances
-        pcl_dist = np.sqrt(np.sum(np.square(raw_pcl_np), axis=1))
-
-        # TODO, ROI should be done somewhere else
-        # (probably already on RGBD according to user specified bounding box)
-        indices = np.nonzero(pcl_dist < self.max_distance)[0]
-        self.roi_pcl = raw_pcl.select_by_index(indices)
-        return self.roi_pcl
     
     def detect_ground_plane(self):
-        roi_pcl = self.roi_pcl
-        # Get the ground plane
-        plane_eq, plane_inliers = roi_pcl.segment_plane(
-            DISTANCE_THRESHOLD_PLANE, N_POINTS_SAMPLED_PLANE, MAX_ITER_PLANE)
+        points = o3d.utility.Vector3dVector(np.array([
+            [config.point_cloud_range["x_min"], config.point_cloud_range["y_min"], config.point_cloud_range["z_min"]],
+            [config.point_cloud_range["x_max"], config.point_cloud_range["y_max"], 0.01]
+        ]))
+        
+        self.plane_pcl = self.raw_pcl.crop(
+            bounding_box = o3d.geometry.AxisAlignedBoundingBox.create_from_points(points)
+        )
 
-        self.plane_pcl = roi_pcl.select_by_index(plane_inliers)
-
-        return plane_eq, plane_inliers
+        points = o3d.utility.Vector3dVector(np.array([
+            [config.point_cloud_range["x_min"], config.point_cloud_range["y_min"], 0.01],
+            [config.point_cloud_range["x_max"], config.point_cloud_range["y_max"], config.point_cloud_range["z_max"]]
+        ]))
+        self.plane_outliers_pcl = self.raw_pcl.crop(
+            bounding_box = o3d.geometry.AxisAlignedBoundingBox.create_from_points(points)
+        )
+        
+        return self.plane_pcl, self.plane_outliers_pcl
     
-    def get_box_pcl(self, plane_eq, plane_inliers):
-        plane_outliers = self.roi_pcl.select_by_index(plane_inliers, invert=True)
+    def get_box_pcl(self):
+        plane_outliers = self.plane_outliers_pcl
+
         # Cluster the outliers, the biggest in good conditions should be the box
         # TODO, if the biggest one is not over the threshold -> no box
         labels = np.array(plane_outliers.cluster_dbscan(eps=0.02, min_points=10))
@@ -211,22 +152,7 @@ class BoxEstimator():
         self.box_pcl = plane_outliers.select_by_index(box_indices)
         return self.box_pcl
 
-    def get_box_top(self, plane_eq):
-        rot_matrix = self.create_rotation_matrix(plane_eq[0:3], [0,0,1]) #
-        self.plane_pcl = self.plane_pcl.rotate(rot_matrix, center=(0,0,0))
-        avg_z = np.average(np.asarray(self.plane_pcl.points)[:, 2])
-
-        translate_vector = [0, 0, -avg_z]
-        self.translate_vector = np.array(translate_vector)
-
-        self.plane_pcl = self.plane_pcl.translate(translate_vector)
-
-        self.box_pcl = self.box_pcl.rotate(rot_matrix, center=(0,0,0))
-        self.box_pcl = self.box_pcl.translate(translate_vector)
-
-        self.raw_pcl = self.raw_pcl.rotate(rot_matrix, center=(0,0,0))
-        self.raw_pcl = self.raw_pcl.translate(translate_vector)
-
+    def get_box_top(self):
         points_np = np.asarray(self.box_pcl.points)
         top_plane_eq, top_plane_inliers = self.fit_plane_vec_constraint([0, 0, 1], points_np, 0.03, 30)
 
