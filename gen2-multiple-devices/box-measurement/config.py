4d1462ba95f4659493e39b7b0aa370fc8108fc2c
Nejc Jezeršek <njezersek@gmail.com>
2022-10-05 14:48:50 +0200

    updated readme

--- gen2-multiple-devices/box-measurement/config.py
+++ gen2-multiple-devices/box-measurement/config.py
@@ -7,8 +7,8 @@ lrcheck  = True   			# Better handling for occlusions
 extended = False  			# Closer-in minimum depth, disparity range is doubled
 subpixel = True   			# Better accuracy for longer distance, fractional disparity 32-levels
 confidence_threshold = 250 	# 0-255, 255 = low confidence, 0 = high confidence
-min_range = 100 			# mm
-max_range = 2000			# mm
+min_range = 100 			# mm from the camera
+max_range = 2000			# mm from the camera
 
 # Median filter
 # Options: MEDIAN_OFF, KERNEL_3x3, KERNEL_5x5, KERNEL_7x7
