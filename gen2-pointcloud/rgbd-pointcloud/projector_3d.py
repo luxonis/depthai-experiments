34ceb8f67d27f54a829c6c5c487e1948e1fe2015
Nejc Jezeršek <njezersek@gmail.com>
2022-11-02 11:39:56 +0100

    Revert "Increased depth limit, added depth view"

--- gen2-pointcloud/rgbd-pointcloud/projector_3d.py
+++ gen2-pointcloud/rgbd-pointcloud/projector_3d.py
@@ -32,7 +32,7 @@ class PointCloudVisualizer():
         depth_o3d = o3d.geometry.Image(depth_map)
 
         rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
-            rgb_o3d, depth_o3d, convert_rgb_to_intensity=(len(rgb.shape) != 3), depth_trunc=20000, depth_scale=1000.0
+            rgb_o3d, depth_o3d, convert_rgb_to_intensity=(len(rgb.shape) != 3)
         )
 
         pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, self.pinhole_camera_intrinsic)
