3f76382d259132d1c55a983b78a32161175e4624
Vita Komel <69462196+vitakomel@users.noreply.github.com>
2022-11-07 09:27:23 +0100

    Adding an equation for computing std of spatial coordinates, using the message timestamp for current time, not filtering new tracklets

--- gen2-bounding-box-filtering/kalman/kalman_filter.py
+++ gen2-bounding-box-filtering/kalman/kalman_filter.py
@@ -1,18 +1,11 @@
 import depthai as dai
 import blobconverter
 import cv2
-import time
 import numpy as np
-import pandas as pd
-import imageio
 
 
 label_map = ['background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow',
             'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
-
-# For OAK-D camera
-df = pd.read_csv('errors.csv')
-error_poly = np.poly1d(np.polyfit(df['Ground truth'], df['OAK-D Error [%]'], 1))
 
 
 class KalmanFilter(object):
@@ -88,7 +81,6 @@ mono_right.setBoardSocket(dai.CameraBoardSocket.RIGHT)
 
 stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
 stereo.setDepthAlign(dai.CameraBoardSocket.RGB)
-stereo.setOutputSize(mono_left.getResolutionWidth(), mono_left.getResolutionHeight())
 
 detection_network.setBlobPath(blobconverter.from_zoo(name='mobilenet-ssd', shaves=5))
 detection_network.setConfidenceThreshold(0.7)
@@ -117,41 +109,38 @@ stereo.depth.link(detection_network.inputDepth)
 # Connect to device and start pipeline
 with dai.Device(pipeline) as device:
 
+    calibration_handler = device.readCalibration()
+    baseline = calibration_handler.getBaselineDistance() * 10
+    focal_length = calibration_handler.getCameraIntrinsics(dai.CameraBoardSocket.RGB, 1920, 1080)[0][0]
+
     q_rgb  = device.getOutputQueue(name='rgb', maxSize=4, blocking=False)
     q_tracklets = device.getOutputQueue(name='tracklets', maxSize=4, blocking=False)
 
     kalman_filters = {}
-    # frames = []
 
     while(True):
         frame = q_rgb.get().getCvFrame()
-        tracks = q_tracklets.get().tracklets
+        tracklets = q_tracklets.get()
+        current_time = tracklets.getTimestamp()
 
-        for t in tracks:
+        h, w = frame.shape[:2]
+
+        for t in tracklets.tracklets:
             roi = t.roi.denormalize(frame.shape[1], frame.shape[0])
             x1 = int(roi.topLeft().x)
             y1 = int(roi.topLeft().y)
             x2 = int(roi.bottomRight().x)
             y2 = int(roi.bottomRight().y)
 
-            x_center = (x1 + x2) / 2
-            y_center = (y1 + y2) / 2
-            width = x2 - x1
-            height = y2 - y1
-
             x_space = t.spatialCoordinates.x
             y_space = t.spatialCoordinates.y
             z_space = t.spatialCoordinates.z
 
-            meas_vec_bbox = np.array([[x_center], [y_center], [width], [height]])
+            meas_vec_bbox = np.array([[(x1 + x2) / 2], [(y1 + y2) / 2], [x2 - x1], [y2 - y1]])
             meas_vec_space = np.array([[x_space], [y_space], [z_space]])
-
-            current_time = time.monotonic()
-            meas_std_space =  error_poly(z_space) * z_space / 100
+            meas_std_space =  z_space ** 2 / (baseline * focal_length)
 
             if t.status.name == 'NEW':
-                dt = 1e-2
-
                 # Adjust these parameters
                 acc_std_space = 10
                 acc_std_bbox = 0.1
@@ -162,6 +151,7 @@ with dai.Device(pipeline) as device:
 
             else:
                 dt = current_time - kalman_filters[t.id]['bbox'].time
+                dt = dt.total_seconds()
                 kalman_filters[t.id]['space'].meas_std = meas_std_space
 
                 if t.status.name != 'TRACKED':
@@ -172,22 +162,28 @@ with dai.Device(pipeline) as device:
                     meas_vec_space = None
 
 
-            kalman_filters[t.id]['bbox'].predict(dt)
-            kalman_filters[t.id]['bbox'].update(meas_vec_bbox)
+                kalman_filters[t.id]['bbox'].predict(dt)
+                kalman_filters[t.id]['bbox'].update(meas_vec_bbox)
 
-            kalman_filters[t.id]['space'].predict(dt)
-            kalman_filters[t.id]['space'].update(meas_vec_space)
+                kalman_filters[t.id]['space'].predict(dt)
+                kalman_filters[t.id]['space'].update(meas_vec_space)
 
-            kalman_filters[t.id]['bbox'].time = current_time
-            kalman_filters[t.id]['space'].time = current_time
+                kalman_filters[t.id]['bbox'].time = current_time
+                kalman_filters[t.id]['space'].time = current_time
 
-            vec_bbox = kalman_filters[t.id]['bbox'].x
-            vec_space = kalman_filters[t.id]['space'].x
+                vec_bbox = kalman_filters[t.id]['bbox'].x
+                vec_space = kalman_filters[t.id]['space'].x
 
-            x1_filter = int(vec_bbox[0] - vec_bbox[2] / 2)
-            x2_filter = int(vec_bbox[0] + vec_bbox[2] / 2)
-            y1_filter = int(vec_bbox[1] - vec_bbox[3] / 2)
-            y2_filter = int(vec_bbox[1] + vec_bbox[3] / 2)
+                x1_filter = int(vec_bbox[0] - vec_bbox[2] / 2)
+                x2_filter = int(vec_bbox[0] + vec_bbox[2] / 2)
+                y1_filter = int(vec_bbox[1] - vec_bbox[3] / 2)
+                y2_filter = int(vec_bbox[1] + vec_bbox[3] / 2)
+
+                cv2.rectangle(frame, (x1_filter, y1_filter), (x2_filter, y2_filter), (0, 0, 255), 2)
+                cv2.putText(frame, f'X: {int(vec_space[0])} mm', (x1 + 10, y1 + 110), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0, 0, 255))
+                cv2.putText(frame, f'Y: {int(vec_space[1])} mm', (x1 + 10, y1 + 125), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0, 0, 255))
+                cv2.putText(frame, f'Z: {int(vec_space[2])} mm', (x1 + 10, y1 + 140), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0, 0, 255))
+                
 
             try:
                 label = label_map[t.label]
@@ -198,21 +194,13 @@ with dai.Device(pipeline) as device:
             cv2.putText(frame, f'ID: {[t.id]}', (x1 + 10, y1 + 35), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
             cv2.putText(frame, t.status.name, (x1 + 10, y1 + 50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
             cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0))
-            cv2.rectangle(frame, (x1_filter, y1_filter), (x2_filter, y2_filter), (0, 0, 255), 2)
 
             cv2.putText(frame, f'X: {int(x_space)} mm', (x1 + 10, y1 + 65), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
             cv2.putText(frame, f'Y: {int(y_space)} mm', (x1 + 10, y1 + 80), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
             cv2.putText(frame, f'Z: {int(z_space)} mm', (x1 + 10, y1 + 95), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
 
-            cv2.putText(frame, f'X: {int(vec_space[0])} mm', (x1 + 10, y1 + 110), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0, 0, 255))
-            cv2.putText(frame, f'Y: {int(vec_space[1])} mm', (x1 + 10, y1 + 125), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0, 0, 255))
-            cv2.putText(frame, f'Z: {int(vec_space[2])} mm', (x1 + 10, y1 + 140), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0, 0, 255))
 
         cv2.imshow('tracker', frame)
-        # rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # frames.append(rgb_frame)
 
         if cv2.waitKey(1) == ord('q'):
             break
-
-# imageio.mimsave('demo.gif', frames, fps=30)
