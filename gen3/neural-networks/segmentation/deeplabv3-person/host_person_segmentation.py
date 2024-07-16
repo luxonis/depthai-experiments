3f10f0b250d6394ed02a7ca4e717b7666e891c85
NicikD <daniel.cinik@gmail.com>
2024-07-16 15:07:35 +0200

    Rewritten deeplabv3-person to v3

---
+++ gen3/neural-networks/segmentation/deeplabv3-person/host_person_segmentation.py
@@ -0,0 +1,45 @@
+import cv2
+import depthai as dai
+import numpy as np
+
+class PersonSegmentation(dai.node.HostNode):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def build(self, preview: dai.Node.Output, nn: dai.Node.Output, nn_shape: tuple[int, int]) -> "PersonSegmentation":
+        self.link_args(preview, nn)
+        self.sendProcessingToPipeline(True)
+        self.nn_shape = nn_shape
+        return self
+
+    # preview is actually type dai.ImgFrame here
+    def process(self, preview: dai.Buffer, nn: dai.NNData) -> None:
+        assert(isinstance(preview, dai.ImgFrame))
+
+        # If the data from the rgb camera is available, transform the 1D data into a HxWxC frame
+        shape = (3, preview.getHeight(), preview.getWidth())
+        frame = preview.getData().reshape(shape).transpose(1, 2, 0).astype(np.uint8)
+        frame = np.ascontiguousarray(frame)
+
+        # Get first layer data
+        first_layer = nn.getAllLayers()[0]
+        layer1 = nn.getTensor(first_layer.name).astype(np.int32).flatten()
+        dims = first_layer.dims[::-1]
+        layer1 = np.asarray(layer1, dtype=np.int32).reshape(dims)
+
+        output_colors = self.decode_deeplabv3p(layer1)
+        frame = cv2.addWeighted(frame,1, output_colors,0.2,0)
+
+        cv2.imshow("Preview", frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            print("Pipeline exited.")
+            self.stopPipeline()
+
+    def decode_deeplabv3p(self, output_tensor):
+        class_colors = [[0, 0, 0], [0, 255, 0]]
+        class_colors = np.asarray(class_colors, dtype=np.uint8)
+
+        output = output_tensor.reshape(self.nn_shape)
+        output_colors = np.take(class_colors, output, axis=0)
+        return output_colors
