848ac527d8ee5fd5309f9f7d7495553fde29a5e7
ttomkk <klimotomas@pobox.sk>
2024-07-17 14:08:29 +0200

    concat and diff working after fixing multiple NN inputs

--- gen3/tutorial/custom-models/concat.py
+++ gen3/tutorial/custom-models/concat.py
@@ -10,12 +10,14 @@ class DisplayConcat(dai.node.HostNode):
         self.shape = (3, SHAPE, SHAPE * 3)
         super().__init__()
 
+
     def build(self, nn_out : dai.Node.Output) -> "DisplayConcat":
         self.link_args(nn_out)
         self.sendProcessingToPipeline(True)
-        return self # crashes after this line, probably problem with self.link_args(nn_out)
+        return self
     
-    def process(self, nn_data : dai.NNData): # program doesn't get here
+    
+    def process(self, nn_data : dai.NNData):
         inNn = np.array(nn_data.getData())
         frame = inNn.view(np.float16).reshape(self.shape).transpose(1, 2, 0).astype(np.uint8).copy()
 
@@ -40,7 +42,7 @@ with dai.Pipeline() as pipeline:
 
     # ImageManip for cropping (face detection NN requires input image of 300x300) and to change frame type
     manipLeft = pipeline.create(dai.node.ImageManip)
-    manipLeft.initialConfig.setResize(300, 300)
+    manipLeft.initialConfig.setResize(SHAPE, SHAPE)
     # The NN model expects BGR input. By default ImageManip output type would be same as input (gray in this case)
     manipLeft.initialConfig.setFrameType(dai.ImgFrame.Type.BGR888p)
     left.out.link(manipLeft.inputImage)
@@ -51,17 +53,17 @@ with dai.Pipeline() as pipeline:
 
     # ImageManip for cropping (face detection NN requires input image of 300x300) and to change frame type
     manipRight = pipeline.create(dai.node.ImageManip)
-    manipRight.initialConfig.setResize(300, 300)
+    manipRight.initialConfig.setResize(SHAPE, SHAPE)
     # The NN model expects BGR input. By default ImageManip output type would be same as input (gray in this case)
     manipRight.initialConfig.setFrameType(dai.ImgFrame.Type.BGR888p)
     right.out.link(manipRight.inputImage)
 
     nn = pipeline.create(dai.node.NeuralNetwork)
     # to test from vs code
-    # dirname = os.path.dirname(__file__)
-    # path = os.path.join(dirname, "models", "concat_openvino_2021.4_6shave.blob")
-    # nn.setBlobPath(path)
-    nn.setBlobPath("models/concat_openvino_2021.4_6shave.blob")
+    dirname = os.path.dirname(__file__)
+    path = os.path.join(dirname, "models", "concat_openvino_2021.4_6shave.blob")
+    nn.setBlobPath(path)
+    # nn.setBlobPath("models/concat_openvino_2021.4_6shave.blob")
     nn.setNumInferenceThreads(2)
 
     manipLeft.out.link(nn.inputs['img1'])
@@ -73,62 +75,3 @@ with dai.Pipeline() as pipeline:
     )
 
     pipeline.run()
-
-
-# p = dai.Pipeline()
-# p.setOpenVINOVersion(dai.OpenVINO.VERSION_2021_4)
-
-# camRgb = p.create(dai.node.ColorCamera)
-# camRgb.setPreviewSize(SHAPE, SHAPE)
-# camRgb.setInterleaved(False)
-# camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
-
-# left = p.create(dai.node.MonoCamera)
-# left.setBoardSocket(dai.CameraBoardSocket.LEFT)
-# left.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-
-# # ImageManip for cropping (face detection NN requires input image of 300x300) and to change frame type
-# manipLeft = p.create(dai.node.ImageManip)
-# manipLeft.initialConfig.setResize(300, 300)
-# # The NN model expects BGR input. By default ImageManip output type would be same as input (gray in this case)
-# manipLeft.initialConfig.setFrameType(dai.RawImgFrame.Type.BGR888p)
-# left.out.link(manipLeft.inputImage)
-
-# right = p.create(dai.node.MonoCamera)
-# right.setBoardSocket(dai.CameraBoardSocket.RIGHT)
-# right.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-
-# # ImageManip for cropping (face detection NN requires input image of 300x300) and to change frame type
-# manipRight = p.create(dai.node.ImageManip)
-# manipRight.initialConfig.setResize(300, 300)
-# # The NN model expects BGR input. By default ImageManip output type would be same as input (gray in this case)
-# manipRight.initialConfig.setFrameType(dai.RawImgFrame.Type.BGR888p)
-# right.out.link(manipRight.inputImage)
-
-# NN that concatenates inputs
-# nn = p.create(dai.node.NeuralNetwork)
-# nn.setBlobPath("models/concat_openvino_2021.4_6shave.blob")
-# nn.setNumInferenceThreads(2)
-
-# manipLeft.out.link(nn.inputs['img1'])
-# camRgb.preview.link(nn.inputs['img2'])
-# manipRight.out.link(nn.inputs['img3'])
-
-# Send bouding box from the NN to the host via XLink
-# nn_xout = p.create(dai.node.XLinkOut)
-# nn_xout.setStreamName("nn")
-# nn.out.link(nn_xout.input)
-
-# Pipeline is defined, now we can connect to the device
-# with dai.Device(p) as device:
-#     qNn = device.getOutputQueue(name="nn", maxSize=4, blocking=False)
-#     shape = (3, SHAPE, SHAPE * 3)
-
-#     while True:
-#         inNn = np.array(qNn.get().getData())
-#         frame = inNn.view(np.float16).reshape(shape).transpose(1, 2, 0).astype(np.uint8).copy()
-
-#         cv2.imshow("Concat", frame)
-
-#         if cv2.waitKey(1) == ord('q'):
-#             break
--- gen3/tutorial/custom-models/diff.py
+++ gen3/tutorial/custom-models/diff.py
@@ -1,27 +1,30 @@
-import os
 import numpy as np
 import cv2
 import depthai as dai
+
 
 class DisplayDiff(dai.node.HostNode):
     def __init__(self):
         super().__init__()
     
+
     def build(self, colorOut : dai.Node.Output, nnOut : dai.Node.Output) -> "DisplayDiff":
         self.link_args(colorOut, nnOut)
         self.sendProcessingToPipeline(True)
         return self
     
+
     def process(self, camColor : dai.ImgFrame, nnData : dai.NNData) -> None:
         cv2.imshow("Diff", self.get_frame(nnData, (720, 720)))
         cv2.imshow("Color", camColor.getCvFrame())
 
         if cv2.waitKey(1) == ord('q'):
-            self.stopPipeline()
-        
+            self.stopPipeline()      
 
-    def get_frame(data: dai.NNData, shape):
-        diff = np.array(data.getFirstLayerFp16()).reshape(shape)
+
+    def get_frame(self, data: dai.NNData, shape):
+        first_layer_name = data.getAllLayerNames()[0]
+        diff = np.array(data.getTensor(first_layer_name).astype(np.float16).flatten()).reshape(shape)
         colorize = cv2.normalize(diff, None, 255, 0, cv2.NORM_INF, cv2.CV_8UC1)
         return cv2.applyColorMap(colorize, cv2.COLORMAP_JET)
 
@@ -36,10 +39,6 @@ with dai.Pipeline() as pipeline:
 
     # NN that detects faces in the image
     nn = pipeline.create(dai.node.NeuralNetwork)
-    # testing from vs code
-    # dirname = os.path.dirname(__file__)
-    # path = os.path.join(dirname, "models", "diff_openvino_2021.4_6shave.blob")
-    # nn.setBlobPath(path)
     nn.setBlobPath("models/diff_openvino_2021.4_6shave.blob")
 
     script = pipeline.create(dai.node.Script)
@@ -55,28 +54,9 @@ with dai.Pipeline() as pipeline:
     script.outputs['img1'].link(nn.inputs['img1'])
     script.outputs['img2'].link(nn.inputs['img2'])
 
-
     pipeline.create(DisplayDiff).build(
         camRgb.preview,
         nn.out
     )
 
     pipeline.run()
-
-
-# # Pipeline is defined, now we can connect to the device
-# with dai.Device(p) as device:
-#     qNn = device.getOutputQueue(name="nn", maxSize=4, blocking=False)
-#     qCam = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
-
-#     def get_frame(data: dai.NNData, shape):
-#         diff = np.array(data.getFirstLayerFp16()).reshape(shape)
-#         colorize = cv2.normalize(diff, None, 255, 0, cv2.NORM_INF, cv2.CV_8UC1)
-#         return cv2.applyColorMap(colorize, cv2.COLORMAP_JET)    
-
-#     while True:
-#         cv2.imshow("Diff", get_frame(qNn.get(), (720, 720)))
-#         cv2.imshow("Color", qCam.get().getCvFrame())
-
-#         if cv2.waitKey(1) == ord('q'):
-#             break
