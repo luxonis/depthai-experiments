61955d6c06a79fa231468a63fd7ebb6a16adadac
dominik737 <dominik737@gmail.com>
2024-07-22 23:40:23 +0200

    face recognition refactor

--- gen3/neural-networks/facial-detections/face-recognition/detected_recognitions.py
+++ gen3/neural-networks/facial-detections/face-recognition/detected_recognitions.py
@@ -4,7 +4,7 @@ import depthai as dai
 class DetectedRecognitions(dai.Buffer):
     def __init__(self, detections: dai.ImgDetections, nn_data: list[dai.NNData]) -> None:
         super().__init__(0)
-        self.detections: dai.ImgDetections = detections
+        self.img_detections: dai.ImgDetections = detections
         self.nn_data: list[dai.NNData] = nn_data
         
         self.setTimestampDevice(detections.getTimestampDevice())
