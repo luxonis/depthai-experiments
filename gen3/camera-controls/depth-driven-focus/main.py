6095a58312f6c364d3aae9c647b73ade1ef6826b
NicikD <daniel.cinik@gmail.com>
2024-07-25 12:00:03 +0200

    Rewritten fatigue-detection to v3

--- gen3/camera-controls/depth-driven-focus/main.py
+++ gen3/camera-controls/depth-driven-focus/main.py
@@ -26,11 +26,7 @@ with dai.Pipeline() as pipeline:
 
     face_det_nn = pipeline.create(dai.node.MobileNetSpatialDetectionNetwork).build()
     face_det_nn.setConfidenceThreshold(0.4)
-    face_det_nn.setBlobPath(blobconverter.from_zoo(
-        name="face-detection-retail-0004",
-        shaves=5,
-        version='2021.4'
-    ))
+    face_det_nn.setBlobPath(blobconverter.from_zoo(name="face-detection-retail-0004", shaves=5, version='2021.4'))
     face_det_nn.setBoundingBoxScaleFactor(0.5)
     face_det_nn.setDepthLowerThreshold(200)
     face_det_nn.setDepthUpperThreshold(3000)
---
+++ gen3/neural-networks/facial-detections/fatigue-detection/README.md
@@ -0,0 +1,21 @@
+# Fatigue Detection
+
+This example demonstrates running [face detection network](https://docs.openvinotoolkit.org/2019_R1/_face_detection_retail_0004_description_face_detection_retail_0004.html) and head detection network on DepthAI
+
+## Demo:
+
+![Fatigue detection](fatigue.gif)
+
+## Installation
+
+```
+python3 -m pip install -r requirements.txt
+```
+
+## Usage
+
+Run the application
+
+```
+python3 main.py
+```
