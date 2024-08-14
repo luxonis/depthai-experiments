2398ff7545fb9ca31d7363b7c10fc1d9f696ef8e
ttomkk <klimotomas@pobox.sk>
2024-08-14 09:21:56 +0200

    car detection new model running

--- gen3/neural-networks/object-detection/yolo/car-detection/main.py
+++ gen3/neural-networks/object-detection/yolo/car-detection/main.py
@@ -1,15 +1,14 @@
 import argparse
-from pathlib import Path
-import json
 import depthai as dai
 from car_detection import CarDetection
 from video import Video
 
+modelDescription = dai.NNModelDescription(modelSlug="yolov6-nano", platform="RVC2")
+archivePath = dai.getModelFromZoo(modelDescription)
+nn_archive = dai.NNArchive(archivePath)
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--model", help="Provide model path for inference",
-                    default='models/yolo_v4_tiny_openvino_2021.3_6shave.blob', type=str)
-parser.add_argument("-c", "--config", help="Provide config path for inference",
-                    default='yolo-tiny.json', type=str)
 parser.add_argument("-vid", "--vid", help="Provide Luxonis video name or path to video that should be used instead of camera",
                     default="cars-tracking-above-01", type=str)
 parser.add_argument("-cam", "--cam", help="Use camera instead of video.", 
@@ -23,10 +22,10 @@ if not cam:
     video_path = Video(video).get_path()
 
 with dai.Pipeline() as pipeline:
-    nn_input_shape = (512, 320)
+    nn_input_shape = (512, 288)
 
     if cam:
-        cam = pipeline.create(dai.node.ColorCamera).build()
+        cam = pipeline.create(dai.node.ColorCamera)
         cam.setBoardSocket(dai.CameraBoardSocket.CAM_A)
         cam.setPreviewSize(nn_input_shape)
         cam.setInterleaved(False)
@@ -37,19 +36,8 @@ with dai.Pipeline() as pipeline:
         replay.setSize(nn_input_shape)
         replay.setOutFrameType(dai.ImgFrame.Type.BGR888p)
     
-    nn = pipeline.create(dai.node.YoloDetectionNetwork)
-    nn.setBlobPath(args.model)
-    
-    with open(str(Path(args.config).resolve())) as file:
-        conf: dict = json.load(file)['nn_config']['NN_specific_metadata']
-        nn.setNumClasses(conf['classes'])
-        nn.setCoordinateSize(conf['coordinates'])
-        nn.setAnchors(conf['anchors'])
-        nn.setAnchorMasks(conf['anchor_masks'])
-        nn.setIouThreshold(conf['iou_threshold'])
-
-        if conf['confidence_threshold'] is not None:
-            nn.setConfidenceThreshold(conf['confidence_threshold'])
+    nn = pipeline.create(dai.node.DetectionNetwork)
+    nn.setNNArchive(nn_archive)
 
     if cam:
         output = cam.preview
@@ -58,5 +46,11 @@ with dai.Pipeline() as pipeline:
         output = replay.out
         replay.out.link(nn.input)
 
-    pipeline.create(CarDetection).build(output, nn.out)
+    pipeline.create(CarDetection).build(
+        img_frame=output, 
+        detections=nn.out
+        )
+    
+    print("Pipeline created")
     pipeline.run()
+    print("Pipeline finished")
