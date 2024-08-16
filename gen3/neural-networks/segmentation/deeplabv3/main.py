78e51468aef22229abef28577c8ab66203ebb9f3
ttomkk <klimotomas@pobox.sk>
2024-08-16 15:23:32 +0200

    deeplabv3 (appart from multiclass) models updated (localy)

--- gen3/neural-networks/segmentation/deeplabv3/main.py
+++ gen3/neural-networks/segmentation/deeplabv3/main.py
@@ -1,13 +1,12 @@
 import depthai as dai
 import argparse
-import blobconverter
 
 from pathlib import Path
 from host_deeplab_segmentation import DeeplabSegmentation
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-nn", "--neural-network", choices=["multiclass", "person_small", "person_large"]
-                    , default="multiclass", type=str
+                    , default="person_small", type=str
                     , help="Choose the neural network model used for segmentation (multiclass is default)")
 parser.add_argument("-cam", "--cam_input", choices=["left", "rgb", "right"], default="rgb", type=str
                     , help="Choose camera for inference source (rgb is default)")
@@ -18,20 +17,22 @@ nn_shape = None
 nn_path = None
 
 if args.neural_network == "multiclass":
+    raise NotImplementedError("Multiclass model is not supported yet")
     nn_shape = (256, 256)
     nn_path = Path("model/deeplab_v3_plus_mnv2_decoder_256_openvino_2021.4.blob").resolve().absolute()
 elif args.neural_network == "person_small":
     nn_shape = (256, 256)
-    nn_path = blobconverter.from_zoo(name="deeplab_v3_mnv2_256x256", zoo_type="depthai", shaves=6)
+    nn_path = Path("./model/deeplab_v3_mnv2_256x256.rvc2.tar.xz").resolve().absolute()
 elif args.neural_network == "person_large":
     nn_shape = (513, 513)
-    nn_path = blobconverter.from_zoo(name="deeplab_v3_mnv2_513x513", zoo_type="depthai", shaves=6)
+    nn_path = Path("./model/deeplab_v3_mnv2_513x513.rvc2.tar.xz").resolve().absolute()
 
 with dai.Pipeline() as pipeline:
 
     print("Creating pipeline...")
     nn = pipeline.create(dai.node.NeuralNetwork)
-    nn.setBlobPath(nn_path)
+    # nn.setBlobPath(nn_path)
+    nn.setNNArchive(dai.NNArchive(str(nn_path)))
     nn.setNumPoolFrames(4)
     nn.input.setBlocking(False)
     nn.setNumInferenceThreads(2)
