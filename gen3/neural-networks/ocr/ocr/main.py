6b1e6ecd655065f440af615c2cc3c705f1be55be
nixelone <nixelone@gmail.com>
2024-08-20 17:06:28 +0200

    Model exported and fully functional

--- gen3/neural-networks/ocr/ocr/main.py
+++ gen3/neural-networks/ocr/ocr/main.py
@@ -8,10 +8,18 @@ from detections_recognitions_sync import DetectionsRecognitionsSync
 
 FPS = 10
 
+detection_model_description = dai.NNModelDescription(modelSlug="east-text-detection", platform="RVC2", modelVersionSlug="256x256")
+detection_archive_path = dai.getModelFromZoo(detection_model_description)
+detection_nn_archive = dai.NNArchive(detection_archive_path)
+
+recognition_model_description = dai.NNModelDescription(modelSlug="text-recognition", platform="RVC2", modelVersionSlug="0012")
+recognition_archive_path = dai.getModelFromZoo(recognition_model_description)
+recognition_nn_archive = dai.NNArchive(recognition_archive_path)
+
 with dai.Pipeline() as pipeline:
 
     print("Creating pipeline...")
-    cam = pipeline.create(dai.node.ColorCamera).build()
+    cam = pipeline.create(dai.node.ColorCamera)
     cam.setPreviewSize(256, 256)
     cam.setVideoSize(1024, 1024)  # 4 times larger in both axis
     cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
@@ -20,7 +28,8 @@ with dai.Pipeline() as pipeline:
     cam.setFps(FPS)
 
     detection_nn = pipeline.create(dai.node.NeuralNetwork)
-    detection_nn.setBlobPath(blobconverter.from_zoo(name="east_text_detection_256x256", zoo_type="depthai", shaves=6, version="2021.4"))
+    # detection_nn.setBlobPath(blobconverter.from_zoo(name="east_text_detection_256x256", zoo_type="depthai", shaves=6, version="2021.4"))
+    detection_nn.setNNArchive(detection_nn_archive)
     cam.preview.link(detection_nn.input)
 
     east = pipeline.create(East).build(
@@ -42,7 +51,8 @@ with dai.Pipeline() as pipeline:
     process_detections.passthrough.link(manip.inputImage)
 
     recognition_nn = pipeline.create(dai.node.NeuralNetwork)
-    recognition_nn.setBlobPath(blobconverter.from_zoo(name="text-recognition-0012", shaves=6, version="2021.4"))
+    # recognition_nn.setBlobPath(blobconverter.from_zoo(name="text-recognition-0012", shaves=6, version="2021.4"))
+    recognition_nn.setNNArchive(recognition_nn_archive)
     recognition_nn.setNumInferenceThreads(2)
     manip.out.link(recognition_nn.input)
 
