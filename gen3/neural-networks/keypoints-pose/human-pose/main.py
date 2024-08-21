960fa31185e256591c86feb5b3a75ae7482b64f1
ttomkk <klimotomas@pobox.sk>
2024-08-21 14:44:45 +0200

    replaced blobconverter

--- gen3/neural-networks/keypoints-pose/human-pose/main.py
+++ gen3/neural-networks/keypoints-pose/human-pose/main.py
@@ -6,6 +6,10 @@ from host_human_pose import HumanPose
 from display import Display
 from pathlib import Path
 
+model_description = dai.NNModelDescription(modelSlug="human-pose-estimation", platform="RVC2", modelVersionSlug="0001-456x256")
+archive_path = dai.getModelFromZoo(model_description)
+nn_archive = dai.NNArchive(archive_path)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-vid', '--video', type=str
                     , help="Path to video file to be used for inference (otherwise uses the DepthAI 4K color camera)")
@@ -25,7 +29,7 @@ with dai.Pipeline() as pipeline:
 
         replay.out.link(manip.inputImage)
     else:
-        cam = pipeline.create(dai.node.ColorCamera).build()
+        cam = pipeline.create(dai.node.ColorCamera)
         cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
         cam.setIspScale(1, 5)
         cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
@@ -36,7 +40,7 @@ with dai.Pipeline() as pipeline:
         cam.preview.link(manip.inputImage)
 
     nn = pipeline.create(dai.node.NeuralNetwork)
-    nn.setBlobPath(blobconverter.from_zoo(name="human-pose-estimation-0001", shaves=5))
+    nn.setNNArchive(nn_archive)
     nn.setNumInferenceThreads(2)
     nn.input.setBlocking(False)
     manip.out.link(nn.input)
