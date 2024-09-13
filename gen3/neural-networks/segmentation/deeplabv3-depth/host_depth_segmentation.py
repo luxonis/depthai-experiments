7cd6e825737ae5f36acb8cc487cde4fb95fc530a
NicikD <daniel.cinik@gmail.com>
2024-09-13 17:13:12 +0200

    Fixed deeplabv3 depth segmentatiné to work with new NN model

--- gen3/neural-networks/segmentation/deeplabv3-depth/host_depth_segmentation.py
+++ gen3/neural-networks/segmentation/deeplabv3-depth/host_depth_segmentation.py
@@ -6,34 +6,32 @@ import numpy as np
 JET_CUSTOM = cv2.applyColorMap(np.arange(256, dtype=np.uint8), cv2.COLORMAP_JET)
 JET_CUSTOM[0] = [0, 0, 0]
 
+
 class DepthSegmentation(dai.node.HostNode):
     def __init__(self) -> None:
         super().__init__()
 
         self.output = self.createOutput(possibleDatatypes=[dai.Node.DatatypeHierarchy(dai.DatatypeEnum.ImgFrame, True)])
 
-    def build(self, preview: dai.Node.Output, nn: dai.Node.Output, disparity: dai.Node.Output
-              , nn_shape: tuple[int, int], max_disparity: int) -> "DepthSegmentation":
-        self.link_args(preview, nn, disparity)
+    def build(self, preview: dai.Node.Output, disparity: dai.Node.Output, mask: dai.Node.Output, max_disparity: int) -> "DepthSegmentation":
+        self.link_args(preview, disparity, mask)
 
-        self.nn_shape = nn_shape
         self.disp_multiplier = 255 / max_disparity
         return self
 
-    def process(self, preview: dai.ImgFrame, nn: dai.NNData, disparity: dai.ImgFrame) -> None:
+    def process(self, preview: dai.ImgFrame, disparity: dai.ImgFrame, mask: dai.Buffer) -> None:
         frame = preview.getCvFrame()
-        target_shape = (frame.shape[1], frame.shape[0])
-        data = nn.getFirstTensor().astype(np.int32).flatten().reshape(self.nn_shape)
-        output_colors = cv2.resize(self.decode_deeplabv3p(data), target_shape)
+
+        mask_data = mask.getFrame()
+        mask_data = cv2.resize(mask_data, frame.shape[:2])
+
+        class_colors = np.asarray([[0, 0, 0], [0, 255, 0]], dtype=np.uint8)
+        output_colors = np.take(class_colors, mask_data, axis=0)
+        colored_frame = cv2.addWeighted(frame, 1, output_colors, 0.5, 0)
 
         disp_frame = (disparity.getFrame() * self.disp_multiplier).astype(np.uint8)
-        multiplier = self.get_multiplier(data)
-        multiplier = cv2.resize(multiplier, target_shape)
-
-        colored_frame = cv2.addWeighted(frame, 1, output_colors, 0.5, 0)
         depth_frame = cv2.applyColorMap(disp_frame, JET_CUSTOM)
-        cutout_frame = cv2.applyColorMap(disp_frame * multiplier, JET_CUSTOM)
-        # You can add custom code here, for example depth averaging
+        cutout_frame = cv2.applyColorMap(disp_frame * mask_data, JET_CUSTOM)
 
         combined_frame = np.concatenate((colored_frame, cutout_frame, depth_frame), axis=1)
 
@@ -41,18 +39,3 @@ class DepthSegmentation(dai.node.HostNode):
         output.setCvFrame(combined_frame, dai.ImgFrame.Type.BGR888p)
         output.setTimestamp(preview.getTimestamp())
         self.output.send(output)
-
-    def get_multiplier(self, output_tensor):
-        class_binary = [[0], [1]]
-        class_binary = np.asarray(class_binary, dtype=np.uint8)
-        output = output_tensor.reshape(self.nn_shape)
-        output_colors = np.take(class_binary, output, axis=0)
-        return output_colors
-
-    def decode_deeplabv3p(self, output_tensor):
-        class_colors = [[0, 0, 0], [0, 255, 0]]
-        class_colors = np.asarray(class_colors, dtype=np.uint8)
-
-        output = output_tensor.reshape(self.nn_shape)
-        output_colors = np.take(class_colors, output, axis=0)
-        return output_colors
--- gen3/neural-networks/segmentation/deeplabv3-depth/main.py
+++ gen3/neural-networks/segmentation/deeplabv3-depth/main.py
@@ -1,24 +1,26 @@
 import depthai as dai
 
+from depthai_nodes import SegmentationParser
 from host_depth_segmentation import DepthSegmentation
 from host_fps_drawer import FPSDrawer
 from host_display import Display
 
-model_description = dai.NNModelDescription(modelSlug="deeplabv3", platform="RVC2", modelVersionSlug="256x256")
+model_description = dai.NNModelDescription(modelSlug="deeplab-v3-plus", platform="RVC2", modelVersionSlug="person-256x256")
 archive_path = dai.getModelFromZoo(model_description)
 nn_archive = dai.NNArchive(archive_path)
 
-output_shape = (400, 400)
+output_shape = (512, 512)
+mono_shape = (640, 400)
 nn_shape = (256, 256)
 
 with dai.Pipeline() as pipeline:
 
     print("Creating pipeline...")
     color = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_A)
+    color_output = color.requestOutput(output_shape, dai.ImgFrame.Type.BGR888p)
+
     left = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_B)
     right = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_C)
-
-    color_output = color.requestOutput(output_shape)
 
     manip = pipeline.create(dai.node.ImageManip)
     manip.initialConfig.setResize(*nn_shape)
@@ -26,15 +28,20 @@ with dai.Pipeline() as pipeline:
     manip.setMaxOutputFrameSize(nn_shape[0] * nn_shape[1] * 3)
     color_output.link(manip.inputImage)
 
-    nn = pipeline.create(dai.node.NeuralNetwork)
-    nn.setNNArchive(nn_archive)
+    nn = pipeline.create(dai.node.NeuralNetwork).build(
+        nnArchive=nn_archive,
+        input=manip.out
+    )
     nn.input.setBlocking(False)
-    nn.setNumInferenceThreads(2)
-    manip.out.link(nn.input)
+    nn.input.setMaxSize(1)
+
+    parser = pipeline.create(SegmentationParser)
+    nn.out.link(parser.input)
+    parser.setBackgroundClass(True)
 
     stereo = pipeline.create(dai.node.StereoDepth).build(
-        left=left.requestOutput(output_shape),
-        right=right.requestOutput(output_shape)
+        left=left.requestOutput(mono_shape),
+        right=right.requestOutput(mono_shape)
     )
     stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
     stereo.setDepthAlign(dai.CameraBoardSocket.CAM_A)
@@ -42,14 +49,12 @@ with dai.Pipeline() as pipeline:
 
     depth_segmentation = pipeline.create(DepthSegmentation).build(
         preview=color_output,
-        nn=nn.out,
+        mask=parser.out,
         disparity=stereo.disparity,
-        nn_shape=nn_shape,
         max_disparity=stereo.initialConfig.getMaxDisparity()
     )
 
     fps_drawer = pipeline.create(FPSDrawer).build(depth_segmentation.output)
-
     display = pipeline.create(Display).build(fps_drawer.output)
     display.setName("Depth Segmentation")
 
