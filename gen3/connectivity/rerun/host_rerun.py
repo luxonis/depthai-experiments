8920cc40367a624f1d975378abe7b3763903548c
NicikD <daniel.cinik@gmail.com>
2024-09-16 17:20:09 +0200

    Rewritten rerun to use PointCloud node and new Camera node

--- gen3/connectivity/rerun/host_rerun.py
+++ gen3/connectivity/rerun/host_rerun.py
@@ -3,56 +3,20 @@ import numpy as np
 import rerun as rr
 import cv2
 
+
 class Rerun(dai.node.HostNode):
     def __init__(self) -> None:
         super().__init__()
 
-    def build(self, color: dai.Node.Output, depth: dai.Node.Output, device: dai.Device) -> "Rerun":
-        self.link_args(color, depth)
-        self.sendProcessingToPipeline(True)
-        self.device = device
-        self.xyz = None
+    def build(self, color: dai.Node.Output, pcl: dai.Node.Output) -> "Rerun":
+        self.link_args(color, pcl)
         return self
 
-    def process(self, color: dai.ImgFrame, depth: dai.ImgFrame) -> None:
-        depth_frame = depth.getCvFrame()
-
-        if self.xyz is None:
-            self.xyz = create_xyz(self.device, depth_frame.shape[1], depth_frame.shape[0])
-
-        depth_frame = np.expand_dims(np.array(depth_frame), axis=-1)
-        # To meters and reshape for rerun
-        pcl = (self.xyz * depth_frame / 1000.0).reshape(-1, 3)
+    def process(self, color: dai.ImgFrame, pcl: dai.PointCloudData) -> None:
         colors = cv2.cvtColor(color.getCvFrame(), cv2.COLOR_BGR2RGB).reshape(-1, 3)
-        rr.log("Pointcloud", rr.Points3D(pcl, colors=colors))
 
+        points = pcl.getPoints().astype(np.float64)
+        # The point cloud comes back reversed, should be fixed in the future
+        points[:, 0] = -points[:, 0]
 
-def create_xyz(device, width, height):
-    calibData = device.readCalibration()
-    M_right = calibData.getCameraIntrinsics(
-        dai.CameraBoardSocket.RIGHT, dai.Size2f(width, height))
-    camera_matrix = np.array(M_right).reshape(3, 3)
-
-    xs = np.linspace(0, width - 1, width, dtype=np.float32)
-    ys = np.linspace(0, height - 1, height, dtype=np.float32)
-
-    # generate grid by stacking coordinates
-    base_grid = np.stack(np.meshgrid(xs, ys))  # WxHx2
-    points_2d = base_grid.transpose(1, 2, 0)  # 1xHxWx2
-
-    # unpack coordinates
-    u_coord: np.array = points_2d[..., 0]
-    v_coord: np.array = points_2d[..., 1]
-
-    # unpack intrinsics
-    fx: np.array = camera_matrix[0, 0]
-    fy: np.array = camera_matrix[1, 1]
-    cx: np.array = camera_matrix[0, 2]
-    cy: np.array = camera_matrix[1, 2]
-
-    # projective
-    x_coord: np.array = (u_coord - cx) / fx
-    y_coord: np.array = (v_coord - cy) / fy
-
-    xyz = np.stack([x_coord, y_coord], axis=-1)
-    return np.pad(xyz, ((0, 0), (0, 0), (0, 1)), "constant", constant_values=1.0)
+        rr.log("Pointcloud", rr.Points3D(points, colors=colors))
--- gen3/connectivity/rerun/main.py
+++ gen3/connectivity/rerun/main.py
@@ -1,28 +1,26 @@
 import depthai as dai
 import asyncio
 import rerun as rr
+
 from host_rerun import Rerun
+
 
 async def main():
     device = dai.Device()
     with dai.Pipeline(device) as pipeline:
 
         print("Creating pipeline...")
-        cam = pipeline.create(dai.node.ColorCamera)
-        cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-        cam.setIspScale(1, 3)
-        cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
-        cam.initialControl.setManualFocus(130)
+        device.setIrLaserDotProjectorIntensity(1)
 
-        left = pipeline.create(dai.node.MonoCamera)
-        left.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-        left.setBoardSocket(dai.CameraBoardSocket.CAM_B)
+        rgb = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_A)
+        left = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_B)
+        right = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_C)
 
-        right = pipeline.create(dai.node.MonoCamera)
-        right.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-        right.setBoardSocket(dai.CameraBoardSocket.CAM_C)
-
-        stereo = pipeline.create(dai.node.StereoDepth).build(left=left.out, right=right.out)
+        stereo = pipeline.create(dai.node.StereoDepth).build(
+            left=left.requestOutput(size=(640, 400)),
+            right=right.requestOutput(size=(640, 400))
+        )
+        stereo.setOutputSize(1920, 1080)
         stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
         stereo.setDepthAlign(dai.CameraBoardSocket.CAM_A)
         stereo.setLeftRightCheck(True)
@@ -40,15 +38,13 @@ async def main():
         stereo.initialConfig.postProcessing.thresholdFilter.maxRange = 200000
         stereo.initialConfig.postProcessing.decimationFilter.decimationFactor = 1
 
-        rerun = pipeline.create(Rerun).build(
-            color=cam.isp,
-            depth=stereo.depth,
-            device=device
+        pcl = pipeline.create(dai.node.PointCloud)
+        stereo.depth.link(pcl.inputDepth)
+
+        pipeline.create(Rerun).build(
+            color=rgb.requestOutput((1920, 1080)),
+            pcl=pcl.outputPointCloud
         )
-        rerun.inputs["color"].setBlocking(False)
-        rerun.inputs["color"].setMaxSize(1)
-        rerun.inputs["depth"].setBlocking(False)
-        rerun.inputs["depth"].setMaxSize(1)
 
         print("Pipeline created.")
         pipeline.run()
