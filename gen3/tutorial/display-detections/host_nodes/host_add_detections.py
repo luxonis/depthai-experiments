c8b6bab2b434f39e9ffc3ff76bade84eff00b3a1
ttomkk <klimotomas@pobox.sk>
2024-09-17 11:39:36 +0200

    integrated new draw detection node to display-detections

--- gen3/tutorial/display-detections/host_nodes/host_add_detections.py
+++ gen3/tutorial/display-detections/host_nodes/host_add_detections.py
@@ -1,9 +1,83 @@
 import depthai as dai
 import cv2
 import numpy as np
+from enum import Enum
 
 
-class AddDetections(dai.node.HostNode):
+class MANIP_MODE(Enum):
+    CROP, \
+    LETTERBOX, \
+    STRETCH = range(3)
+
+
+class NormaliezeBbox(dai.node.HostNode):
+    def __init__(self) -> None:
+        super().__init__()
+        
+        self.output = self.createOutput(possibleDatatypes=[dai.Node.DatatypeHierarchy(dai.DatatypeEnum.ImgDetections, True)])
+        
+
+    def build(self, frame : dai.Node.Output, nn : dai.Node.Output, manip_mode : MANIP_MODE = dai.ImgResizeMode.CROP) -> "NormaliezeBbox":
+        self.manip_mode = manip_mode
+
+        self.link_args(frame, nn)
+        self.sendProcessingToPipeline(True)
+        return self
+
+
+    def process(self, frame : dai.ImgFrame, detections : dai.ImgDetections) -> None:
+        frame = frame.getCvFrame()
+        
+        normalized_dets = dai.ImgDetections()
+        normalized_dets.setTimestamp(detections.getTimestamp())
+        normalized_dets.setSequenceNum(detections.getSequenceNum())
+        dets = []
+        
+        for detection in detections.detections:
+            bbox = self._frame_norm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
+            
+            det = dai.ImgDetection()
+            det.label = detection.label
+            det.confidence = detection.confidence
+            det.xmin = bbox[0]
+            det.ymin = bbox[1]
+            det.xmax = bbox[2]
+            det.ymax = bbox[3]
+            
+            dets.append(det)
+            
+        normalized_dets.detections = dets
+        self.output.send(normalized_dets)
+    
+    
+    def _frame_norm(self, frame : np.ndarray, bbox : list[dai.ImgDetection]) -> np.ndarray:
+        # moves the bounding box to equalize the crop
+        if self.manip_mode == dai.ImgResizeMode.CROP:
+            normVals = np.full(4, frame.shape[0])
+            ret = (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
+            ret[::2] += (frame.shape[1] - frame.shape[0])//2
+            return ret
+        
+        # stretches the bounding box to equalize the letterbox
+        elif self.manip_mode == dai.ImgResizeMode.LETTERBOX:
+            normVals = np.full(4, frame.shape[0])
+            normVals[::2] = frame.shape[1]
+            bbox = (bbox[0]
+                    , 0.5 + (bbox[1]-0.5)*frame.shape[1]/frame.shape[0]
+                    , bbox[2]
+                    , 0.5 + (bbox[3]-0.5)*frame.shape[1]/frame.shape[0])
+            return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
+
+        # resizes the bounding box based on the frame size
+        else:
+            normVals = np.full(4, frame.shape[0])
+            normVals[::2] = frame.shape[1]
+            ret = (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
+            return ret
+
+
+
+class DrawDetections(dai.node.HostNode):
     def __init__(self) -> None:
         super().__init__()
         
@@ -12,7 +86,7 @@ class AddDetections(dai.node.HostNode):
         self.output = self.createOutput(possibleDatatypes=[dai.Node.DatatypeHierarchy(dai.DatatypeEnum.ImgFrame, True)])
 
 
-    def build(self, frame: dai.Node.Output, nn: dai.Node.Output, label_map: list[str]) -> "AddDetections":
+    def build(self, frame: dai.Node.Output, nn: dai.Node.Output, label_map: list[str]) -> "DrawDetections":
         self.label_map = label_map
         
         self.link_args(frame, nn)
@@ -22,25 +96,17 @@ class AddDetections(dai.node.HostNode):
 
     def process(self, in_frame : dai.ImgFrame, in_detections: dai.ImgDetections) -> None:
         frame = in_frame.getCvFrame()
-        out_frame = self.add_detections(frame, in_detections.detections)
+        out_frame = self.draw_detections(frame, in_detections.detections)
         
-        img = dai.ImgFrame()
-        img.setCvFrame(out_frame, dai.ImgFrame.Type.BGR888p)
+        img = self._create_img_frame(out_frame, dai.ImgFrame.Type.BGR888p)
         
         self.output.send(img)
         
-        
-    def frame_norm(self, frame : np.ndarray, bbox : list[dai.ImgDetection]) -> np.ndarray:
-        norm_vals = np.full(len(bbox), frame.shape[0])
-        norm_vals = (np.clip(np.array(bbox), 0, 1) * norm_vals).astype(int)
-        norm_vals[::2] += (frame.shape[1] - frame.shape[0])//2
-    
-        return norm_vals
 
-
-    def add_detections(self, frame : np.ndarray, detections : list[dai.ImgDetection]) -> np.ndarray:
+    def draw_detections(self, frame : np.ndarray, detections : list[dai.ImgDetection]) -> np.ndarray:
         for detection in detections:
-            bbox = self.frame_norm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
+            bbox = (detection.xmin, detection.ymin, detection.xmax, detection.ymax)
+            bbox = (np.clip(np.array(bbox), 0, 1) * bbox).astype(int)
             cv2.putText(frame, self.label_map[detection.label], (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, self.color)
             cv2.putText(frame, f"{int(detection.confidence * 100)}%", (bbox[0] + 10, bbox[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, self.color)
             cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), self.color, self.thickness)
@@ -54,3 +120,9 @@ class AddDetections(dai.node.HostNode):
         
     def set_thickness(self, thickness : int) -> None:
         self.thickness = thickness
+        
+        
+    def _create_img_frame(self, frame: np.ndarray, type : dai.ImgFrame.Type) -> dai.ImgFrame:
+        img_frame = dai.ImgFrame()
+        img_frame.setCvFrame(frame, type)
+        return img_frame
--- gen3/tutorial/display-detections/main.py
+++ gen3/tutorial/display-detections/main.py
@@ -1,6 +1,6 @@
 import depthai as dai
 
-from host_nodes.host_add_detections import AddDetections
+from host_nodes.host_add_detections import NormaliezeBbox, DrawDetections
 from host_nodes.host_display import Display
 
 model_description = dai.NNModelDescription(modelSlug="mobilenet-ssd", platform="RVC2")
@@ -10,34 +10,28 @@ nn_archive = dai.NNArchive(archivePath=archive_path)
 with dai.Pipeline() as pipeline:
 
     cam_rgb = pipeline.create(dai.node.Camera).build(boardSocket=dai.CameraBoardSocket.CAM_A)
-    preview = cam_rgb.requestOutput(size=(812, 608), type=dai.ImgFrame.Type.BGR888p, resizeMode=dai.ImgResizeMode.CROP)
+    preview = cam_rgb.requestOutput(size=(812, 608), type=dai.ImgFrame.Type.BGR888p)
+    square = cam_rgb.requestOutput(size=(720, 720), type=dai.ImgFrame.Type.BGR888p)
+    cam_nn = cam_rgb.requestOutput(size=(300, 300), type=dai.ImgFrame.Type.BGR888p)
     
-    # Crop video to match aspect ratio of the detection network (1:1)
-    crop_square = pipeline.create(dai.node.ImageManip)
-    crop_square.initialConfig.setResize(720, 720)
-    crop_square.setMaxOutputFrameSize(720*720*3)
-    preview.link(crop_square.inputImage)
-
-    # Crop video to match detection network
-    crop_nn = pipeline.create(dai.node.ImageManip)
-    crop_nn.initialConfig.setResize(300, 300)
-    preview.link(crop_nn.inputImage)
-    
-    nn = pipeline.create(dai.node.DetectionNetwork).build(input=crop_nn.out, nnArchive=nn_archive)
+    nn = pipeline.create(dai.node.DetectionNetwork).build(input=cam_nn, nnArchive=nn_archive)
     label_map = nn.getClasses()
     
     color = pipeline.create(Display).build(frame=preview)
     color.setName("Color")
       
-    full_dets = pipeline.create(AddDetections).build(frame=preview, nn=nn.out, label_map=label_map)
+    norm_full = pipeline.create(NormaliezeBbox).build(frame=preview, nn=nn.out)
+    full_dets = pipeline.create(DrawDetections).build(frame=preview, nn=norm_full.output, label_map=label_map)
     full = pipeline.create(Display).build(frame=full_dets.output)
     full.setName("Full")
     
-    square_dets = pipeline.create(AddDetections).build(frame=crop_square.out, nn=nn.out, label_map=label_map)
+    norm_square = pipeline.create(NormaliezeBbox).build(frame=square, nn=nn.out)
+    square_dets = pipeline.create(DrawDetections).build(frame=square, nn=norm_square.output, label_map=label_map)
     square = pipeline.create(Display).build(frame=square_dets.output)
     square.setName("Square")
     
-    passthrough_dets = pipeline.create(AddDetections).build(frame=crop_nn.out, nn=nn.out, label_map=label_map)
+    norm_passthrough = pipeline.create(NormaliezeBbox).build(frame=cam_nn, nn=nn.out)
+    passthrough_dets = pipeline.create(DrawDetections).build(frame=cam_nn, nn=norm_passthrough.output, label_map=label_map)
     passthrough = pipeline.create(Display).build(frame=passthrough_dets.output)
     passthrough.setName("Passthrough")
        
