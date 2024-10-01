813daa2e6407e7fd4fe6a8089e90fb7793e34a9b
Tadeas Pala <tadeas.pala@luxonis.com>
2024-10-01 14:11:18 +0200

    reformatted text-blur with ruff

--- gen3/neural-networks/object-detection/text-blur/main.py
+++ gen3/neural-networks/object-detection/text-blur/main.py
@@ -15,15 +15,35 @@ rvc2 = platform == dai.Platform.RVC2
 
 NN_WIDTH, NN_HEIGHT = (256, 256) if rvc2 else (960, 544)
 
-modelDescription = dai.NNModelDescription(modelSlug="paddle-text-detection", platform=device.getPlatform().name, modelVersionSlug=f"{NN_HEIGHT}x{NN_WIDTH}")
+modelDescription = dai.NNModelDescription(
+    modelSlug="paddle-text-detection",
+    platform=device.getPlatform().name,
+    modelVersionSlug=f"{NN_HEIGHT}x{NN_WIDTH}",
+)
 archivePath = dai.getModelFromZoo(modelDescription)
 nn_archive = dai.NNArchive(archivePath)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-bt", "--box_thresh", help="set the confidence threshold of boxes", default=0.2, type=float)
-parser.add_argument("-t", "--thresh", help="set the bitmap threshold", default=0.01, type=float)
-parser.add_argument("-ms", "--min_size", default=1, type=int, help='set min size of box')
-parser.add_argument("-mc", "--max_candidates", default=75, type=int, help='maximum number of candidate boxes')
+parser.add_argument(
+    "-bt",
+    "--box_thresh",
+    help="set the confidence threshold of boxes",
+    default=0.2,
+    type=float,
+)
+parser.add_argument(
+    "-t", "--thresh", help="set the bitmap threshold", default=0.01, type=float
+)
+parser.add_argument(
+    "-ms", "--min_size", default=1, type=int, help="set min size of box"
+)
+parser.add_argument(
+    "-mc",
+    "--max_candidates",
+    default=75,
+    type=int,
+    help="maximum number of candidate boxes",
+)
 
 
 args = parser.parse_args()
@@ -31,14 +51,19 @@ args = parser.parse_args()
 with dai.Pipeline(device) as pipeline:
     # Define camera
     cam = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_A)
-    output = cam.requestOutput((NN_WIDTH, NN_HEIGHT), dai.ImgFrame.Type.BGR888p if rvc2 else dai.ImgFrame.Type.BGR888i)
+    output = cam.requestOutput(
+        (NN_WIDTH, NN_HEIGHT),
+        dai.ImgFrame.Type.BGR888p if rvc2 else dai.ImgFrame.Type.BGR888i,
+    )
 
     # Define a neural network that will detect text
     detection_nn = pipeline.create(dai.node.NeuralNetwork).build(output, nn_archive)
     detection_nn.setNumPoolFrames(4)
     detection_nn.input.setBlocking(False)
     detection_nn.setNumInferenceThreads(2)
-    parser = pipeline.create(TextDetectionParser).build(nn=detection_nn.out, nn_size=(NN_HEIGHT, NN_WIDTH))
+    parser = pipeline.create(TextDetectionParser).build(
+        nn=detection_nn.out, nn_size=(NN_HEIGHT, NN_WIDTH)
+    )
     parser.setConfidenceThreshold(args.thresh)
 
     mask_to_bbox = pipeline.create(MaskToBbox).build(nn=parser.output)
@@ -47,8 +72,12 @@ with dai.Pipeline(device) as pipeline:
     mask_to_bbox.setMinSize(args.min_size)
     mask_to_bbox.setMaxCandidates(args.max_candidates)
     mask_to_bbox.setPadding(5)
-    normalize_bbox = pipeline.create(NormalizeBbox).build(nn=mask_to_bbox.output, frame=output, manip_mode=dai.ImgResizeMode.CROP)
-    blur_bboxes = pipeline.create(BlurBboxes).build(nn=normalize_bbox.output, frame=output)
+    normalize_bbox = pipeline.create(NormalizeBbox).build(
+        nn=mask_to_bbox.output, frame=output, manip_mode=dai.ImgResizeMode.CROP
+    )
+    blur_bboxes = pipeline.create(BlurBboxes).build(
+        nn=normalize_bbox.output, frame=output
+    )
 
     output_fps = pipeline.create(FPSDrawer).build(blur_bboxes.output)
     display_preds = pipeline.create(Display).build(output_fps.output)
