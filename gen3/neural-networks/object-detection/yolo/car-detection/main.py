7b803dd638a3c6649af9e43da6a015ebc5ba3dcd
Tadeas Pala <tadeas.pala@luxonis.com>
2024-10-10 12:29:08 +0200

    split logic into reusable host nodes

--- gen3/neural-networks/object-detection/yolo/car-detection/main.py
+++ gen3/neural-networks/object-detection/yolo/car-detection/main.py
@@ -1,7 +1,10 @@
 import argparse
 
 import depthai as dai
-from car_detection import CarDetection
+from host_node.draw_detections import DrawDetections
+from host_node.host_display import Display
+from host_node.host_fps_drawer import FPSDrawer
+from host_node.normalize_bbox import NormalizeBbox
 from video import Video
 
 device = dai.Device()
@@ -50,16 +53,24 @@ with dai.Pipeline(device) as pipeline:
         replay.setLoop(False)
         replay.setSize(nn_input_shape)
         replay.setOutFrameType(dai.ImgFrame.Type.BGR888p)
-        color_out = replay.out
+        img_manip = pipeline.create(dai.node.ImageManip)
+        img_manip.initialConfig.setFrameType(dai.ImgFrame.Type.BGR888p)
+        replay.out.link(img_manip.inputImage)
+        color_out = img_manip.out
 
     nn = pipeline.create(dai.node.DetectionNetwork)
     nn.setNNArchive(nn_archive)
     color_out.link(nn.input)
 
-    pipeline.create(CarDetection).build(
-        img_frame=color_out,
-        detections=nn.out,
+    fps = pipeline.create(FPSDrawer).build(preview=color_out)
+    normalize_bbox = pipeline.create(NormalizeBbox).build(frame=color_out, nn=nn.out)
+    draw_detections = pipeline.create(DrawDetections).build(
+        frame=fps.output, nn=normalize_bbox.output, label_map=nn.getClasses()
     )
+    draw_detections.set_draw_labels(False)
+    draw_detections.set_draw_confidence(False)
+    draw_detections.set_color((0, 255, 0))
+    display = pipeline.create(Display).build(frames=draw_detections.output)
 
     print("Pipeline created")
     pipeline.run()
