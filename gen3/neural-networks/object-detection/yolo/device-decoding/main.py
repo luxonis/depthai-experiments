8a47c68db7f54470cb024a0ed53a514d959cefc7
Matevz Morato <matevz.morato@gmail.com>
2024-10-17 17:05:31 +0200

    Add pipeline schema and update DAI

--- gen3/neural-networks/object-detection/yolo/device-decoding/main.py
+++ gen3/neural-networks/object-detection/yolo/device-decoding/main.py
@@ -1,4 +1,5 @@
 import depthai as dai
+import cv2
 
 device = dai.Device()
 visualizer = dai.RemoteConnection()
@@ -11,11 +12,14 @@ with dai.Pipeline(device) as pipeline:
         dai.NNModelDescription("yolov6-nano")
     )
 
-    color_out = cam.requestOutput(
-        size=(1280, 720), type=dai.ImgFrame.Type.BGR888i, fps=30
-    )
-    visualizer.addTopic("Color camera", color_out)
+    color_out = cam.requestOutput(size=(1280, 720), type=dai.ImgFrame.Type.BGR888i, fps=30) # Would be better to use NV12, but visualizer doesn't support strided NV12 yet
+    visualizer.addTopic("Camera", color_out)
     visualizer.addTopic("Detections", nn.out)
     print("Pipeline created.")
-    pipeline.run()
+    pipeline.start()
+    visualizer.registerPipeline(pipeline)
+    while pipeline.isRunning():
+        key = visualizer.waitKey(1)
+        if key == ord("q"):
+            break
     print("Pipeline finished.")
