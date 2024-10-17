563e4dc69ba4ebd365900b937f22333e725f656c
dominik737 <dominik737@gmail.com>
2024-10-17 13:15:43 +0200

    yolo device decoding visualizer integrated

--- gen3/neural-networks/object-detection/yolo/device-decoding/main.py
+++ gen3/neural-networks/object-detection/yolo/device-decoding/main.py
@@ -1,37 +1,21 @@
 import depthai as dai
-from host_node.draw_detections import DrawDetections
-from host_node.host_display import Display
-from host_node.normalize_bbox import NormalizeBbox
 
 device = dai.Device()
-
-modelDescription = dai.NNModelDescription(
-    modelSlug="yolov6-nano",
-    platform=device.getPlatform().name,
-    modelVersionSlug="r2-coco-512x288",
-)
-archivePath = dai.getModelFromZoo(modelDescription)
-nn_archive = dai.NNArchive(archivePath)
-
-
+visualizer = dai.RemoteConnection()
 with dai.Pipeline(device) as pipeline:
     cam = pipeline.create(dai.node.Camera).build(
         boardSocket=dai.CameraBoardSocket.CAM_A
     )
-    color_out = cam.requestOutput(
-        size=(512, 288), type=dai.ImgFrame.Type.BGR888p, fps=40
-    )
-
     nn = pipeline.create(dai.node.DetectionNetwork).build(
-        input=color_out, nnArchive=nn_archive
+        cam,
+        dai.NNModelDescription("yolov6-nano")
     )
 
-    norm_bbox = pipeline.create(NormalizeBbox).build(frame=color_out, nn=nn.out)
-    draw_detections = pipeline.create(DrawDetections).build(
-        frame=color_out, nn=norm_bbox.output, label_map=nn.getClasses()
+    color_out = cam.requestOutput(
+        size=(1280, 720), type=dai.ImgFrame.Type.BGR888i, fps=30
     )
-    display = pipeline.create(Display).build(frames=draw_detections.output)
-
+    visualizer.addTopic("Color camera", color_out)
+    visualizer.addTopic("Detections", nn.out)
     print("Pipeline created.")
     pipeline.run()
     print("Pipeline finished.")
