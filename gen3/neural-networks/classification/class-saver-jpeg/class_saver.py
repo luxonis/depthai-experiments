6ad9325456829ca7b615493c69975f275224f206
Tadeas Pala <tadeas.pala@luxonis.com>
2024-10-23 14:09:12 +0200

    split functionality into reusable host nodes

--- gen3/neural-networks/classification/class-saver-jpeg/class_saver.py
+++ gen3/neural-networks/classification/class-saver-jpeg/class_saver.py
@@ -1,7 +1,8 @@
+import time
+from concurrent.futures import ThreadPoolExecutor
 from csv import DictWriter
 from pathlib import Path
-import threading
-import time
+
 import cv2
 import depthai as dai
 import numpy as np
@@ -10,64 +11,96 @@ import numpy as np
 class ClassSaver(dai.node.HostNode):
     def __init__(self) -> None:
         super().__init__()
-        self._thread = None
+        self._thread_pool_executor = ThreadPoolExecutor(max_workers=10)
         self._data_folder_path = Path("./data")
+        self._dict_writer = None
+        self._dataset_file = None
 
-
-    def build(self, rgb: dai.Node.Output, nn_out: dai.Node.Output, dataset: DictWriter, texts: list[str]) -> "ClassSaver":
-        self.link_args(rgb, nn_out)
+    def build(
+        self, frames: dai.Node.Output, nn: dai.Node.Output, classes: list[str]
+    ) -> "ClassSaver":
+        self.link_args(frames, nn)
         self.sendProcessingToPipeline(True)
-        self._texts = texts
-        self._dataset = dataset
+        self._classes = classes
         return self
-    
+
+    def onStart(self):
+        self._init_folders()
+
+    def onStop(self):
+        if self._dataset_file is not None:
+            self._dataset_file.close()
+        self._thread_pool_executor.shutdown()
+
+    def _init_folders(self):
+        for i in self._classes:
+            (self._data_folder_path / i).mkdir(parents=True, exist_ok=True)
+        (self._data_folder_path / "raw").mkdir(parents=True, exist_ok=True)
+
+    def _get_dict_writer(self) -> DictWriter:
+        if self._dict_writer is not None:
+            return self._dict_writer
+        self._dataset_file = open(self._data_folder_path / "dataset.csv", "w")
+        self._dict_writer = DictWriter(
+            self._dataset_file,
+            [
+                "timestamp",
+                "label",
+                "left",
+                "top",
+                "right",
+                "bottom",
+                "raw_frame",
+                "overlay_frame",
+                "cropped_frame",
+            ],
+        )
+        self._dict_writer.writeheader()
+        return self._dict_writer
 
     def process(self, img_frame: dai.ImgFrame, nn_in: dai.ImgDetections):
-        if img_frame is not None:
-            # if the data from the rgb camera is available, transform the 1D data into a HxWxC frame
-            shape = (3, img_frame.getHeight(), img_frame.getWidth())
-            frame = img_frame.getData().reshape(shape).transpose(1, 2, 0).astype(np.uint8)
-            frame = np.ascontiguousarray(frame)
+        self._thread_pool_executor.submit(
+            self._store_data, img_frame.getCvFrame(), nn_in.detections
+        )
 
-        if nn_in is not None:
-            detections = nn_in.detections
-
-            if frame is not None:
-                thread = threading.Thread(target=self._store_data, args=(frame, detections))
-                thread.start()
-
-        if frame is not None:
-            debug_frame = frame.copy()
-            # if the frame is available, draw bounding boxes on it and show the frame
-            if detections:
-                for detection in detections:
-                    bbox = self._frame_norm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
-                    cv2.rectangle(debug_frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
-                    cv2.putText(debug_frame, self._texts[detection.label], (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-            cv2.imshow("rgb", debug_frame)
-
-        if cv2.waitKey(1) == ord('q'):
-            self.stopPipeline()
-
-
-    def _store_data(self, in_frame: np.ndarray, detections: list[dai.ImgDetection]) -> None:
+    def _store_data(
+        self, in_frame: np.ndarray, detections: list[dai.ImgDetection]
+    ) -> None:
         timestamp = int(time.time() * 10000)
-        raw_frame_path = self._data_folder_path / f'raw/{timestamp}.jpg'
+        raw_frame_path = self._data_folder_path / f"raw/{timestamp}.jpg"
         cv2.imwrite(raw_frame_path, in_frame)
         for detection in detections:
             debug_frame = in_frame.copy()
-            bbox = self._frame_norm(in_frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
-            det_frame = debug_frame[bbox[1]:bbox[3], bbox[0]:bbox[2]]
-            cropped_path = self._data_folder_path / f'{self._texts[detection.label]}/{timestamp}_cropped.jpg'
+            bbox = self._frame_norm(
+                in_frame,
+                (detection.xmin, detection.ymin, detection.xmax, detection.ymax),
+            )
+            det_frame = debug_frame[bbox[1] : bbox[3], bbox[0] : bbox[2]]
+            cropped_path = (
+                self._data_folder_path
+                / f"{self._classes[detection.label]}/{timestamp}_cropped.jpg"
+            )
             cv2.imwrite(cropped_path, det_frame)
-            cv2.rectangle(debug_frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
-            cv2.putText(debug_frame, self._texts[detection.label], (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-            overlay_path = self._data_folder_path / f'{self._texts[detection.label]}/{timestamp}_overlay.jpg'
+            cv2.rectangle(
+                debug_frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2
+            )
+            cv2.putText(
+                debug_frame,
+                self._classes[detection.label],
+                (bbox[0] + 10, bbox[1] + 20),
+                cv2.FONT_HERSHEY_TRIPLEX,
+                0.5,
+                255,
+            )
+            overlay_path = (
+                self._data_folder_path
+                / f"{self._classes[detection.label]}/{timestamp}_overlay.jpg"
+            )
             cv2.imwrite(overlay_path, debug_frame)
 
             data = {
                 "timestamp": timestamp,
-                "label": self._texts[detection.label],
+                "label": self._classes[detection.label],
                 "left": bbox[0],
                 "top": bbox[1],
                 "right": bbox[2],
@@ -76,19 +109,10 @@ class ClassSaver(dai.node.HostNode):
                 "overlay_frame": overlay_path,
                 "cropped_frame": cropped_path,
             }
-            self._dataset.writerow(data)
-
+            self._get_dict_writer().writerow(data)
 
     # nn data, being the bounding box locations, are in <0..1> range - they need to be normalized with frame width/height
     def _frame_norm(self, frame: np.ndarray, bbox: tuple):
         norm_vals = np.full(len(bbox), frame.shape[0])
         norm_vals[::2] = frame.shape[1]
         return (np.clip(np.array(bbox), 0, 1) * norm_vals).astype(int)
-    
-
-    def get_thread(self) -> threading.Thread | None:
-        return self._thread
-    
-
-    def set_datafolder_path(self, path: Path) -> None:
-        self._data_folder_path = path
--- gen3/neural-networks/classification/class-saver-jpeg/main.py
+++ gen3/neural-networks/classification/class-saver-jpeg/main.py
@@ -1,56 +1,56 @@
-import csv
 import depthai as dai
-
-from pathlib import Path
 from class_saver import ClassSaver
-
+from host_node.visualize_detections import VisualizeDetections
 
 device = dai.Device()
 platform = device.getPlatform().name
 
-#model_description = dai.NNModelDescription(modelSlug="mobilenet-ssd", platform="RVC2", modelVersionSlug="300x300") # faster for RVC2
-model_description = dai.NNModelDescription(modelSlug="yolov10-nano", platform=platform, modelVersionSlug="coco-512x288") 
+model_description = dai.NNModelDescription(
+    modelSlug="yolov10-nano", platform=platform, modelVersionSlug="coco-512x288"
+)
 archive_path = dai.getModelFromZoo(model_description)
 nn_archive = dai.NNArchive(archive_path)
 
+VIDEO_SIZE = (1280, 720)
+YOLO_SIZE = (512, 288)
+
+visualizer = dai.RemoteConnection()
+
 # Start defining a pipeline
 with dai.Pipeline(device) as pipeline:
+    cam_rgb = pipeline.create(dai.node.Camera).build(
+        boardSocket=dai.CameraBoardSocket.CAM_A
+    )
+    color_out = cam_rgb.requestOutput(
+        size=VIDEO_SIZE, type=dai.ImgFrame.Type.BGR888p, fps=15
+    )
 
-    cam_rgb = pipeline.create(dai.node.Camera).build(boardSocket=dai.CameraBoardSocket.CAM_A)
-    rgb_preview = cam_rgb.requestOutput(size=(512, 288), type=dai.ImgFrame.Type.BGR888p)
-    
-    detection_nn = pipeline.create(dai.node.DetectionNetwork).build(input=rgb_preview, nnArchive=nn_archive, confidenceThreshold=0.5)
+    detection_manip = pipeline.create(dai.node.ImageManip)
+    detection_manip.initialConfig.setResize(*YOLO_SIZE)
+    color_out.link(detection_manip.inputImage)
 
-    # MobilenetSSD label texts
-    texts = detection_nn.getClasses()
-    
-    data_folder_path = Path(__file__).parent / Path('data/')
+    detection_nn = pipeline.create(dai.node.DetectionNetwork).build(
+        input=detection_manip.out, nnArchive=nn_archive, confidenceThreshold=0.5
+    )
+    detection_nn.input.setBlocking(False)
+    detection_nn.input.setMaxSize(2)
 
-    for text in texts:
-        (data_folder_path / Path(text)).mkdir(parents=True, exist_ok=True)
-    (data_folder_path/ Path("raw")).mkdir(parents=True, exist_ok=True)
+    visualize_detections = pipeline.create(VisualizeDetections).build(
+        nn=detection_nn.out, label_map=detection_nn.getClasses()
+    )
 
+    class_saver = pipeline.create(ClassSaver).build(
+        frames=color_out, nn=detection_nn.out, classes=detection_nn.getClasses()
+    )
 
-    with open(data_folder_path / Path("dataset.csv'"), "w") as dataset_file:
-        dataset = csv.DictWriter(
-            dataset_file,
-            ["timestamp", "label", "left", "top", "right", "bottom", "raw_frame", "overlay_frame", "cropped_frame"]
-        )
-        dataset.writeheader()
+    visualizer.addTopic("Detections", visualize_detections.output)
+    visualizer.addTopic("Color", color_out)
 
-        class_saver = pipeline.create(ClassSaver).build(
-            rgb=rgb_preview,
-            nn_out=detection_nn.out,
-            dataset=dataset,
-            texts=texts)
-        class_saver.set_datafolder_path(data_folder_path)
-
-        # Start pipeline
-        pipeline.run()
-
-        if KeyboardInterrupt:
-            pass                
-
-        thread = class_saver.get_thread()
-        if thread is not None:
-            thread.join()
+    pipeline.start()
+    visualizer.registerPipeline(pipeline)
+    print("Pipeline created.")
+    while pipeline.isRunning():
+        pipeline.processTasks()
+        key = visualizer.waitKey(1)
+        if key == ord("q"):
+            break
