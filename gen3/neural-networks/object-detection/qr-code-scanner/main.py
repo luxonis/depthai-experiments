197dca33e83b366f5d2a787a25a692ce81fc0cea
Tadeas Pala <tadeas.pala@luxonis.com>
2024-10-23 14:54:51 +0200

    refactored qr-scanner host nodes

--- gen3/neural-networks/object-detection/qr-code-scanner/main.py
+++ gen3/neural-networks/object-detection/qr-code-scanner/main.py
@@ -1,38 +1,55 @@
 import depthai as dai
-from host_qr_scanner import QRScanner
+from host_node.visualize_detections import VisualizeDetections
+from qr_decoder import QRDecoder
+
+device = dai.Device()
 
 # if model changed change README
-model_description = dai.NNModelDescription(modelSlug="qrdet", platform="RVC2")
+model_description = dai.NNModelDescription(
+    modelSlug="qrdet",
+    modelVersionSlug="nano-288x512",
+    platform=device.getPlatform().name,
+)
 archivePath = dai.getModelFromZoo(model_description)
 nn_archive = dai.NNArchive(archivePath)
 
-with dai.Pipeline() as pipeline:
+VIDEO_SIZE = (1920, 1080)
+DETECTION_NN_SIZE = (512, 288)
 
+visualizer = dai.RemoteConnection()
+
+with dai.Pipeline(device) as pipeline:
     print("Creating pipeline...")
-    cam = pipeline.create(dai.node.ColorCamera)
-    cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-    cam.setPreviewSize(1080, 1080)
-    cam.setInterleaved(False)
-    cam.initialControl.setManualFocus(145)
-    cam.setFps(20)
+    cam = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_A)
+    color_out = cam.requestOutput(VIDEO_SIZE, dai.ImgFrame.Type.BGR888p, fps=20)
 
-    manip = pipeline.create(dai.node.ImageManip)
-    manip.initialConfig.setResize(512, 288) 
-    manip.initialConfig.setKeepAspectRatio(False)
-    cam.preview.link(manip.inputImage)
+    manip = pipeline.create(dai.node.ImageManipV2)
+    manip.initialConfig.addResize(*DETECTION_NN_SIZE)
+    color_out.link(manip.inputImage)
 
-    nn = pipeline.create(dai.node.DetectionNetwork).build(nnArchive=nn_archive, input=manip.out, confidenceThreshold=0.3)
+    nn = pipeline.create(dai.node.DetectionNetwork).build(
+        nnArchive=nn_archive, input=manip.out, confidenceThreshold=0.3
+    )
     nn.input.setMaxSize(1)
     nn.input.setBlocking(False)
 
-    scanner = pipeline.create(QRScanner).build(
-        preview=cam.preview,
-        nn=nn.out
+    visualize_detections = pipeline.create(VisualizeDetections).build(
+        nn=nn.out, label_map=nn.getClasses()
     )
-    scanner.inputs["detections"].setBlocking(False)
-    scanner.inputs["detections"].setMaxSize(2)
-    scanner.inputs["preview"].setBlocking(False)
-    scanner.inputs["preview"].setMaxSize(2)
+    visualize_detections.set_draw_labels(False)
+    visualize_detections.set_draw_confidence(False)
+
+    scanner = pipeline.create(QRDecoder).build(frame=color_out, nn=nn.out)
+
+    visualizer.addTopic("Detections", visualize_detections.output)
+    visualizer.addTopic("Decoded text", scanner.output)
+    visualizer.addTopic("Color", color_out)
 
     print("Pipeline created.")
-    pipeline.run()
+    pipeline.start()
+    visualizer.registerPipeline(pipeline)
+    while pipeline.isRunning():
+        pipeline.processTasks()
+        key = visualizer.waitKey(1)
+        if key == ord("q"):
+            break
