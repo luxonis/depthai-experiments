cbdb1cdac2fe3486bbcaaf1c85218f4c2bd06805
alex-luxonis <alex@luxonis.com>
2024-10-23 20:37:53 +0300

    Add 'none' option for encoding, h264 change to baseline, save stream to file as well

--- gen2-play-encoded-stream/main.py
+++ gen2-play-encoded-stream/main.py
@@ -5,6 +5,7 @@ import subprocess as sp
 from os import name as osName
 import argparse
 import sys
+import cv2
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-cam', '--camera', default='rgb',
@@ -14,7 +15,7 @@ parser.add_argument('-res', '--resolution', default='4k',
                     choices={'1080', '1200', '1500', '1520', '1560', '3000', '4k', '12mp', '13mp'},
                     help="Select color camera resolution. Default: %(default)s")
 parser.add_argument('-enc', '--encode', default='h264',
-                    choices={'h264', 'h265', 'jpeg'},
+                    choices={'h264', 'h265', 'jpeg', 'none'},
                     help="Select encoding format. Default: %(default)s")
 parser.add_argument('-fps', '--fps', type=float, default=30.0,
                     help="Camera FPS to configure. Default: %(default)s")
@@ -47,9 +48,15 @@ res_opts = {
 }
 
 enc_opts = {
-    'h264': dai.VideoEncoderProperties.Profile.H264_MAIN,
+    'h264': dai.VideoEncoderProperties.Profile.H264_BASELINE,
     'h265': dai.VideoEncoderProperties.Profile.H265_MAIN,
     'jpeg': dai.VideoEncoderProperties.Profile.MJPEG,
+}
+
+filename_opts = {
+    'h264': 'video.h264',
+    'h265': 'video.h265',
+    'jpeg': 'video.mjpeg',
 }
 
 # Create pipeline
@@ -57,7 +64,7 @@ pipeline = dai.Pipeline()
 
 # Define sources and output
 camRgb = pipeline.create(dai.node.ColorCamera)
-videoEnc = pipeline.create(dai.node.VideoEncoder)
+if args.encode != 'none': videoEnc = pipeline.create(dai.node.VideoEncoder)
 xout = pipeline.create(dai.node.XLinkOut)
 xout.setStreamName("enc")
 
@@ -65,11 +72,17 @@ xout.setStreamName("enc")
 camRgb.setBoardSocket(cam_socket_opts[args.camera])
 camRgb.setResolution(res_opts[args.resolution])
 camRgb.setFps(args.fps)
-videoEnc.setDefaultProfilePreset(camRgb.getFps(), enc_opts[args.encode])
+if args.encode != 'none': 
+    videoEnc.setDefaultProfilePreset(camRgb.getFps() + 1, enc_opts[args.encode])
+    videoEnc.setBitrateKbps(100000)
+    #videoEnc.setMisc("max-bframes", 1)
 
 # Linking
-camRgb.isp.link(videoEnc.input)
-videoEnc.bitstream.link(xout.input)
+if args.encode != 'none':
+    camRgb.isp.link(videoEnc.input)
+    videoEnc.bitstream.link(xout.input)
+else:
+    camRgb.isp.link(xout.input)
 
 width, height = 720, 500
 command = [
@@ -77,6 +90,7 @@ command = [
     "-i", "-",
     "-x", str(width),
     "-y", str(height),
+    "-hide_banner",
     "-framerate", "60",
     "-fflags", "nobuffer",
     "-flags", "low_delay",
@@ -93,6 +107,7 @@ except:
     exit("Error: cannot run ffplay!\nTry running: sudo apt install ffmpeg")
 
 # Connect to device and start pipeline
+f = open(filename_opts[args.encode], 'wb')
 with dai.Device(pipeline) as device:
     # Output queue will be used to get the encoded data from the output defined above
     q = device.getOutputQueue(name="enc", maxSize=30, blocking=True)
@@ -100,14 +115,20 @@ with dai.Device(pipeline) as device:
     try:
         while True:
             pkt = q.get()  # Blocking call, will wait until new data has arrived
-            data = pkt.getData()
             if args.verbose:
                 latms = (dai.Clock.now() - pkt.getTimestamp()).total_seconds() * 1000
                 # Writing to a different channel (stderr)
                 # Also `ffplay` is printing things, adding a separator
                 print(f'Latency: {latms:.3f} ms === ', file=sys.stderr)
-            proc.stdin.write(data)
+            if args.encode == 'none':
+                cv2.imshow('cam', pkt.getCvFrame())
+                cv2.waitKey(1)
+            else:
+                data = pkt.getData()
+                data.tofile(f)
+                proc.stdin.write(data)
     except:
         pass
 
     proc.stdin.close()
+    f.close()
