b77134d4f2d0ab20321c6ad59ca9677976be3dc2
Tadeas Pala <tadeas.pala@luxonis.com>
2024-10-24 15:55:16 +0200

    refactored full-image-classification

--- gen3/neural-networks/classification/full-image-classification/main.py
+++ gen3/neural-networks/classification/full-image-classification/main.py
@@ -2,17 +2,11 @@ import argparse
 from pathlib import Path
 
 import depthai as dai
-from host_image_classification import ImageClassification
+from depthai_nodes import ClassificationParser
+from efficientnet_classes import CLASS_NAMES
+from host_node.visualize_detections import VisualizeDetections
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-nn",
-    "--neural-network",
-    choices=["efficientnet", "flowers"],
-    default="efficientnet",
-    type=str,
-    help="Choose the neural network model used for classification (efficientnet is default)",
-)
 parser.add_argument(
     "-vid",
     "--video",
@@ -21,65 +15,56 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-nn_shape = None
-blob = None
 
 device = dai.Device()
 
-if args.neural_network == "efficientnet":
-    device_platform = device.getPlatform()
-    if device_platform == dai.Platform.RVC2:
-        model_description = dai.NNModelDescription(
-            modelSlug="efficientnet-lite",
-            platform=device_platform.name,
-            modelVersionSlug="lite0-224x224",
-        )
-        nn_shape = (224, 224)
-    else:
-        model_description = dai.NNModelDescription(
-            modelSlug="efficientnet",
-            platform=device_platform.name,
-            modelVersionSlug="300x300",
-        )
-        nn_shape = (300, 300)
-    archive_path = dai.getModelFromZoo(model_description)
-    blob = dai.NNArchive(archive_path).getSuperBlob().getBlobWithNumShaves(6)
-elif args.neural_network == "flowers":
-    nn_shape = (180, 180)
-    blob = dai.OpenVINO.Blob(Path("model/flower.blob").resolve().absolute())
+nn_description = dai.NNModelDescription(
+    modelSlug="efficientnet-lite",
+    platform=device.getPlatform().name,
+    modelVersionSlug="lite0-224x224",
+)
+nn_path = dai.getModelFromZoo(nn_description, useCached=True)
+nn_archive = dai.NNArchive(archivePath=nn_path)
+
+NN_SIZE = (224, 224)
+
+visualizer = dai.RemoteConnection()
 
 with dai.Pipeline(device) as pipeline:
     print("Creating pipeline...")
     if args.video:
         replay = pipeline.create(dai.node.ReplayVideo)
         replay.setReplayVideoFile(Path(args.video).resolve().absolute())
-        replay.setSize(400, 400)
+        replay.setSize(1280, 720)
         replay.setOutFrameType(dai.ImgFrame.Type.BGR888p)
-        preview = replay.out
+        color_out = replay.out
     else:
-        cam = pipeline.create(dai.node.ColorCamera)
-        cam.setPreviewSize(400, 400)
-        cam.setInterleaved(False)
-        cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-        cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
-        preview = cam.preview
+        cam = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_A)
+        color_out = cam.requestOutput(size=(1280, 720), type=dai.ImgFrame.Type.BGR888p)
 
-    manip = pipeline.create(dai.node.ImageManip)
-    manip.initialConfig.setResize(*nn_shape)
+    manip = pipeline.create(dai.node.ImageManipV2)
+    manip.initialConfig.addResize(*NN_SIZE)
     manip.initialConfig.setFrameType(dai.ImgFrame.Type.BGR888p)
-    preview.link(manip.inputImage)
+    color_out.link(manip.inputImage)
 
     nn = pipeline.create(dai.node.NeuralNetwork)
-    nn.setBlob(blob)
+    nn.setNNArchive(nn_archive)
     manip.out.link(nn.input)
 
-    classification = pipeline.create(ImageClassification).build(
-        preview=preview, nn=nn.out, efficientnet=(args.neural_network == "efficientnet")
-    )
-    classification.inputs["preview"].setBlocking(False)
-    classification.inputs["preview"].setMaxSize(4)
-    classification.inputs["nn"].setBlocking(False)
-    classification.inputs["nn"].setMaxSize(4)
+    parser = pipeline.create(ClassificationParser)
+    parser.setClasses(CLASS_NAMES)
+    nn.out.link(parser.input)
+
+    visualize_detections = pipeline.create(VisualizeDetections).build(nn=parser.out)
+
+    visualizer.addTopic("Classification", visualize_detections.output)
+    visualizer.addTopic("Color", color_out)
 
     print("Pipeline created.")
-    pipeline.run()
+    pipeline.start()
+    visualizer.registerPipeline(pipeline)
+    while pipeline.isRunning():
+        pipeline.processTasks()
+        key = visualizer.waitKey(1)
+        if key == ord("q"):
+            break
