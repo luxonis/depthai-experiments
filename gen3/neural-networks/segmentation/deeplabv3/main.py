a16e40a09902e0c6155ccc4de11de11952640854
Tadeas Pala <tadeas.pala@luxonis.com>
2024-10-25 12:55:03 +0200

    refactored deeplabv3

--- gen3/neural-networks/segmentation/deeplabv3/main.py
+++ gen3/neural-networks/segmentation/deeplabv3/main.py
@@ -1,95 +1,94 @@
 import argparse
-from pathlib import Path
 
+import cv2
 import depthai as dai
-from host_deeplab_segmentation import DeeplabSegmentation
+from depthai_nodes import SegmentationParser
+from host_node.host_depth_color_transform import DepthColorTransform
+from host_node.overlay_frames import OverlayFrames
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "-nn",
     "--neural-network",
     choices=["multiclass", "person_small", "person_large"],
-    default="person_large",
+    default="person_small",
     type=str,
     help="Choose the neural network model used for segmentation (multiclass is default)",
 )
-parser.add_argument(
-    "-cam",
-    "--cam_input",
-    choices=["left", "rgb", "right"],
-    default="rgb",
-    type=str,
-    help="Choose camera for inference source (rgb is default)",
-)
+
+version_slugs = {
+    "multiclass": "513x513",
+    "person_small": "person-256x256",
+    "person_large": "person-513x513",
+}
+nn_sizes = {
+    "multiclass": (513, 513),
+    "person_small": (256, 256),
+    "person_large": (513, 513),
+}
+num_classes = {
+    "multiclass": 21,
+    "person_small": 2,
+    "person_large": 2,
+}
+
+
+VIDEO_SIZE = (720, 720)
+
 args = parser.parse_args()
+version_slug = version_slugs[args.neural_network]
+nn_size = nn_sizes[args.neural_network]
+classes = num_classes[args.neural_network]
 
-cam_source = args.cam_input
-nn_shape = None
-nn_path = None
+device = dai.Device()
 
-if args.neural_network == "multiclass":
-    raise NotImplementedError("Multiclass model is not supported yet")
-    nn_shape = (256, 256)
-    nn_path = (
-        Path("model/deeplab_v3_plus_mnv2_decoder_256_openvino_2021.4.blob")
-        .resolve()
-        .absolute()
-    )
-elif args.neural_network == "person_small":
-    nn_shape = (256, 256)
-    model_description = dai.NNModelDescription(
-        modelSlug="deeplabv3", platform="RVC2", modelVersionSlug="256x256"
-    )
-    archive_path = dai.getModelFromZoo(model_description)
-elif args.neural_network == "person_large":
-    nn_shape = (513, 513)
-    model_description = dai.NNModelDescription(
-        modelSlug="deeplabv3", platform="RVC2", modelVersionSlug="513x513"
-    )
-    archive_path = dai.getModelFromZoo(model_description)
+nn_model_description = dai.NNModelDescription(
+    modelSlug="deeplab-v3-plus",
+    platform=device.getPlatform().name,
+    modelVersionSlug=version_slug,
+)
+nn_archive_path = dai.getModelFromZoo(nn_model_description)
+nn_archive = dai.NNArchive(nn_archive_path)
 
-nn_archive = dai.NNArchive(archive_path)
+visualizer = dai.RemoteConnection()
 
-with dai.Pipeline() as pipeline:
+with dai.Pipeline(device) as pipeline:
     print("Creating pipeline...")
+
+    cam = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_A)
+    color_out = cam.requestOutput(VIDEO_SIZE, dai.ImgFrame.Type.BGR888p, fps=10)
+
+    nn_resize = pipeline.create(dai.node.ImageManipV2)
+    nn_resize.initialConfig.addResize(*nn_size)
+    color_out.link(nn_resize.inputImage)
+
     nn = pipeline.create(dai.node.NeuralNetwork)
-    # nn.setBlobPath(nn_path)
     nn.setNNArchive(nn_archive)
-    nn.setNumPoolFrames(4)
-    nn.input.setBlocking(False)
-    nn.setNumInferenceThreads(2)
+    nn_resize.out.link(nn.input)
 
-    if cam_source == "left":
-        cam = pipeline.create(dai.node.MonoCamera)
-        cam.setBoardSocket(dai.CameraBoardSocket.CAM_B)
-    elif cam_source == "right":
-        cam = pipeline.create(dai.node.MonoCamera)
-        cam.setBoardSocket(dai.CameraBoardSocket.CAM_C)
-    else:
-        cam = pipeline.create(dai.node.ColorCamera)
-        cam.setPreviewSize(nn_shape)
-        cam.setInterleaved(False)
-        cam.preview.link(nn.input)
+    parser = pipeline.create(SegmentationParser)
+    parser.setBackgroundClass(True)
+    nn.out.link(parser.input)
 
-    cam.setFps(40)
-    if cam_source != "rgb":
-        manip = pipeline.create(dai.node.ImageManip)
-        manip.initialConfig.setResize(*nn_shape)
-        manip.initialConfig.setFrameType(dai.ImgFrame.Type.BGR888p)
-        manip.setKeepAspectRatio(True)
-        cam.out.link(manip.inputImage)
-        manip.out.link(nn.input)
+    color_transform = pipeline.create(DepthColorTransform).build(parser.out, classes)
+    color_transform.setColormap(cv2.COLORMAP_JET)
 
-    multiclass_segmentation = pipeline.create(DeeplabSegmentation).build(
-        preview=nn.passthrough,
-        nn=nn.out,
-        nn_shape=nn_shape,
-        multiclass=(args.neural_network == "multiclass"),
+    segmentation_map_resize = pipeline.create(dai.node.ImageManipV2)
+    segmentation_map_resize.initialConfig.addResize(*VIDEO_SIZE)
+    segmentation_map_resize.setMaxOutputFrameSize(VIDEO_SIZE[0] * VIDEO_SIZE[1] * 3)
+    color_transform.output.link(segmentation_map_resize.inputImage)
+
+    overlay_frames = pipeline.create(OverlayFrames).build(
+        color_out, segmentation_map_resize.out
     )
-    multiclass_segmentation.inputs["preview"].setBlocking(False)
-    multiclass_segmentation.inputs["preview"].setMaxSize(4)
-    multiclass_segmentation.inputs["nn"].setBlocking(False)
-    multiclass_segmentation.inputs["nn"].setMaxSize(4)
 
     print("Pipeline created.")
-    pipeline.run()
+    visualizer.addTopic("Segmentation", overlay_frames.output)
+    visualizer.addTopic("Color", color_out)
+    pipeline.start()
+    visualizer.registerPipeline(pipeline)
+    while pipeline.isRunning():
+        pipeline.processTasks()
+        key = visualizer.waitKey(1)
+        if key == ord("q"):
+            break
