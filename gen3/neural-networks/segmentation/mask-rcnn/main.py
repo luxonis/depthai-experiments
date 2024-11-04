c54afd37bfaf8699fe9fc440fe7505a0cf185122
Tadeas Pala <tadeas.pala@luxonis.com>
2024-11-04 14:33:26 +0100

    refactored mask-rcnn

--- gen3/neural-networks/segmentation/mask-rcnn/main.py
+++ gen3/neural-networks/segmentation/mask-rcnn/main.py
@@ -1,39 +1,52 @@
-import blobconverter
 import depthai as dai
-import argparse
+from depthai_nodes import ParsingNeuralNetwork
+from host_node.host_depth_color_transform import DepthColorTransform
+from host_node.overlay_frames import OverlayFrames
+from host_node.visualize_detections_v2 import VisualizeDetectionsV2
+from yolo_labels import LABEL_MAP
 
-from host_mask_rcnn import MaskRCNN
+NN_SIZE = (512, 288)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--threshold', default=0.5, type=float
-                    , help="Threshold for filtering out detections with lower probability")
-parser.add_argument('-rt', '--region_threshold', default=0.5, type=float
-                    , help="Threshold for filtering out mask points with low probability")
-args = parser.parse_args()
+device = dai.Device()
 
-nn_shape = (300, 300)
+nn_model_description = dai.NNModelDescription(
+    modelSlug="yolov8-instance-segmentation-nano",
+    platform=device.getPlatform().name,
+    modelVersionSlug="coco-512x288",
+)
 
-with dai.Pipeline() as pipeline:
+nn_archive_path = dai.getModelFromZoo(nn_model_description, useCached=True)
+nn_archive = dai.NNArchive(nn_archive_path)
 
+visualizer = dai.RemoteConnection()
+
+with dai.Pipeline(device) as pipeline:
     print("Creating pipeline...")
-    cam = pipeline.create(dai.node.ColorCamera)
-    cam.setPreviewSize(nn_shape)
-    cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-    cam.setInterleaved(False)
-    cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+    cam = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_A)
+    color_out = cam.requestOutput(NN_SIZE, dai.ImgFrame.Type.BGR888p, fps=10)
 
-    nn = pipeline.create(dai.node.NeuralNetwork)
-    nn.setBlobPath(blobconverter.from_zoo("mask_rcnn_resnet50_coco_300x300", shaves=6, zoo_type="depthai"))
-    cam.preview.link(nn.input)
-
-    maskrcnn = pipeline.create(MaskRCNN).build(
-        preview=cam.preview,
-        nn=nn.out,
-        threshold=args.threshold,
-        region_threshold=args.region_threshold
+    nn = pipeline.create(ParsingNeuralNetwork).build(
+        input=color_out, nn_source=nn_archive
     )
-    maskrcnn.inputs["preview"].setBlocking(False)
-    maskrcnn.inputs["preview"].setMaxSize(4)
+    visualize_detections = pipeline.create(VisualizeDetectionsV2).build(
+        nn.out, label_map=LABEL_MAP
+    )
+
+    color_transform = pipeline.create(DepthColorTransform).build(
+        visualize_detections.output_mask
+    )
+    color_transform.setMaxDisparity(len(LABEL_MAP) + 1)
+
+    overlay = pipeline.create(OverlayFrames).build(color_out, color_transform.output)
 
     print("Pipeline created.")
-    pipeline.run()
+    visualizer.addTopic("Detections", visualize_detections.output)
+    visualizer.addTopic("Color", color_out)
+    visualizer.addTopic("Mask", overlay.output)
+    pipeline.start()
+    visualizer.registerPipeline(pipeline)
+    while pipeline.isRunning():
+        pipeline.processTasks()
+        key = visualizer.waitKey(1)
+        if key == ord("q"):
+            break
