f2b68425c8441b458e4d24556fa837571b213164
aljazkonec1 <konec.aljaz1@gmail.com>
2024-12-02 11:57:23 +0100

    ImgDetectionsExtended segmentation example

--- gen3/neural-networks/segmentation-and-detection/main.py
+++ gen3/neural-networks/segmentation-and-detection/main.py
@@ -1,11 +1,12 @@
 import depthai as dai
 from depthai_nodes import ParsingNeuralNetwork
-import time
+from detection_segmentation_node import DetSegAnntotationNode
 from utils.arguments import initialize_argparser
+import time
 
 arg_parser, args = initialize_argparser()
 
-visualizer = dai.RemoteConnection(httpPort=8082)
+visualizer = dai.RemoteConnection(httpPort=8080)
 device = dai.Device(dai.DeviceInfo(args.device)) if args.device != "" else dai.Device()
 
 with dai.Pipeline(device) as pipeline:
@@ -16,8 +17,12 @@ with dai.Pipeline(device) as pipeline:
         camera_node, dai.NNModelDescription(args.model_slug), fps=args.fps_limit
         )
     
-    visualizer.addTopic("Video", nn_with_parser.passthrough, "images") 
-    visualizer.addTopic("Visualizations", nn_with_parser.out, "images")
+    annotation_node = pipeline.create(DetSegAnntotationNode)
+    nn_with_parser.passthrough.link(annotation_node.input_frame)
+    nn_with_parser.out.link(annotation_node.input_detections)
+
+    visualizer.addTopic("Video", annotation_node.out, "images")
+    visualizer.addTopic("Detections", nn_with_parser.out, "detections")
     
     print("Pipeline created.")
 
