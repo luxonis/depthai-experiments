890c4f61aca385cb77d3781c53464b1754d1e9c5
jkbmrz <jakob.mraz@gmail.com>
2024-12-06 14:42:54 +0100

    feat: run basic-example on video

--- gen3/neural-networks/basic-example/main.py
+++ gen3/neural-networks/basic-example/main.py
@@ -1,28 +1,51 @@
+import time
+from pathlib import Path
 import depthai as dai
 from depthai_nodes import ParsingNeuralNetwork
-import time
 from utils.arguments import initialize_argparser
 
-arg_parser, args = initialize_argparser()
+_, args = initialize_argparser()
 
 visualizer = dai.RemoteConnection(httpPort=8082)
-device = dai.Device(dai.DeviceInfo(args.device)) if args.device != "" else dai.Device()
+device = dai.Device(dai.DeviceInfo(args.device)) if args.device else dai.Device()
 
 with dai.Pipeline(device) as pipeline:
     print("Creating pipeline...")
-    camera_node = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_A)
-    
-    nn_with_parser = pipeline.create(ParsingNeuralNetwork).build(
-        camera_node, dai.NNModelDescription(args.model_slug), fps=args.fps_limit
+
+    model_description = dai.NNModelDescription(args.model_slug)
+    platform = pipeline.getDefaultDevice().getPlatformAsString()
+    model_description.platform = platform
+    nn_archive = dai.NNArchive(dai.getModelFromZoo(model_description))
+
+    if args.media_path:
+        replay = pipeline.create(dai.node.ReplayVideo)
+        replay.setReplayVideoFile(Path(args.media_path))
+        replay.setOutFrameType(dai.ImgFrame.Type.NV12)
+        replay.setLoop(True)
+        imageManip = pipeline.create(dai.node.ImageManipV2)
+        imageManip.initialConfig.addResize(
+            nn_archive.getInputWidth(), nn_archive.getInputHeight()
         )
-    
-    visualizer.addTopic("Video", nn_with_parser.passthrough, "images") 
+        imageManip.initialConfig.setFrameType(dai.ImgFrame.Type.BGR888p)
+        if platform == "RVC4":
+            imageManip.initialConfig.setFrameType(dai.ImgFrame.Type.BGR888i)
+        replay.out.link(imageManip.inputImage)
+
+    input_node = (
+        imageManip.out if args.media_path else pipeline.create(dai.node.Camera).build()
+    )
+
+    nn_with_parser = pipeline.create(ParsingNeuralNetwork).build(
+        input_node, nn_archive, fps=args.fps_limit
+    )
+
+    visualizer.addTopic("Video", nn_with_parser.passthrough, "images")
     visualizer.addTopic("Visualizations", nn_with_parser.out, "images")
-    
+
     print("Pipeline created.")
 
     pipeline.start()
     visualizer.registerPipeline(pipeline)
-    
+
     while pipeline.isRunning():
-        time.sleep(1/30)
+        time.sleep(1 / 30)
