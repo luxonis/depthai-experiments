9115c2a821930d9888dd91029a7b0743c9ff0261
jkbmrz <jakob.mraz@gmail.com>
2024-12-13 14:13:13 +0100

    feat: remove model slug reference from the generic example 2

--- gen3/neural-networks/generic-example/main.py
+++ gen3/neural-networks/generic-example/main.py
@@ -19,7 +19,7 @@ device = dai.Device(dai.DeviceInfo(args.device)) if args.device else dai.Device(
 with dai.Pipeline(device) as pipeline:
     print("Creating pipeline...")
 
-    model_description = dai.NNModelDescription(args.model_slug)
+    model_description = dai.NNModelDescription(args.model)
     platform = pipeline.getDefaultDevice().getPlatformAsString()
     model_description.platform = platform
     nn_archive = dai.NNArchive(dai.getModelFromZoo(model_description))
