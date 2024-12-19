656a1913d23a22354c332141c2917bd93ad0394e
dominik737 <dominik737@gmail.com>
2024-12-19 12:06:19 +0100

    gen2 folder remove

--- gen2/gen2-full-fov-nn/stretching.py
+++
@@ -1,8 +0,0 @@
-from depthai_sdk import OakCamera
-
-with OakCamera() as oak:
-    color = oak.create_camera('color')
-    nn = oak.create_nn('yolov7tiny_coco_416x416', color)
-    nn.config_nn(resize_mode='stretch')
-    oak.visualize([nn, nn.out.passthrough], fps=True)
-    oak.start(blocking=True)
--- gen2/gen2-yolo/host-decoding/models/README.md
+++
@@ -1,2 +0,0 @@
-Download pretrained models from https://drive.google.com/file/d/1kNqH7U07mXWKib8jQ1ULg4Mz03I6EtQk/view?usp=sharing using `python download.py`
-or put your own models here.
