a7ae1424e344658e440ddd256c234292316106e1
dominik737 <dominik737@gmail.com>
2024-12-20 14:33:17 +0100

    default app remove depth

--- apps/default-app/main.py
+++ apps/default-app/main.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-import cv2
 import depthai as dai
-from host_node.host_depth_color_transform import DepthColorTransform
 
 
-STEREO_RESOLUTION = (800, 600)
 NN_DIMENSIONS = (512,288)
 
 remoteConnector = dai.RemoteConnection()
@@ -41,45 +38,6 @@ with dai.Pipeline(device) as pipeline:
     # Add the remote connector topics
     remoteConnector.addTopic("Encoded Video", colorEncoder.out)
     remoteConnector.addTopic("Detections", detectionNetwork.out)
-
-    # Stereo depth - only for stereo devices
-    cameraFeatures = device.getConnectedCameraFeatures()
-
-    cam_mono_1: dai.CameraBoardSocket | None = None
-    cam_mono_2: dai.CameraBoardSocket | None = None
-    for feature in cameraFeatures:
-        if dai.CameraSensorType.MONO in feature.supportedTypes:
-            if cam_mono_1 is None:
-                cam_mono_1 = feature.socket
-            else:
-                cam_mono_2 = feature.socket
-                break
-    if cam_mono_1 and cam_mono_2:
-        left_cam = pipeline.create(dai.node.Camera).build(cam_mono_1)
-        right_cam = pipeline.create(dai.node.Camera).build(cam_mono_2)
-        stereo = pipeline.create(dai.node.StereoDepth).build(
-            left=left_cam.requestFullResolutionOutput(dai.ImgFrame.Type.NV12),
-            right=right_cam.requestFullResolutionOutput(dai.ImgFrame.Type.NV12),
-            presetMode=dai.node.StereoDepth.PresetMode.HIGH_DENSITY
-        )
-        stereo.setDepthAlign(dai.CameraBoardSocket.CAM_A)
-        if platform == dai.Platform.RVC2:
-            stereo.setOutputSize(*STEREO_RESOLUTION)
-        
-        coloredDepth = pipeline.create(DepthColorTransform).build(stereo.disparity)
-        coloredDepth.setColormap(cv2.COLORMAP_JET)
-        
-        coloredDepthManip = pipeline.create(dai.node.ImageManipV2)
-        coloredDepthManip.initialConfig.setFrameType(dai.ImgFrame.Type.NV12)
-        if platform != dai.Platform.RVC2:
-            coloredDepthManip.initialConfig.setOutputSize(480, 640)
-        coloredDepth.output.link(coloredDepthManip.inputImage)
-
-        depthEncoder = pipeline.create(dai.node.VideoEncoder)
-        depthEncoder.setDefaultProfilePreset(30, ENCODING)
-        coloredDepthManip.out.link(depthEncoder.input) 
-
-        remoteConnector.addTopic("Encoded Depth", depthEncoder.out)
 
     pipeline.start()
     remoteConnector.registerPipeline(pipeline)
