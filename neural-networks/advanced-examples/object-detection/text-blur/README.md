06648cc58ee3f67cb2b1de9c1514642e213d1ef1
aljazkonec1 <konec.aljaz1@gmail.com>
2025-01-07 13:12:07 +0100

    initial commit

--- neural-networks/advanced-examples/object-detection/text-blur/README.md
+++ neural-networks/advanced-examples/object-detection/text-blur/README.md
@@ -1,27 +1,41 @@
-## [Gen2] Text blurring on DepthAI
+## Text blurring on DepthAI
 
 This example shows an implementation of [Text Detection](https://hub.luxonis.com/ai/models/131d855c-60b1-4634-a14d-1269bb35dcd2?view=page) on DepthAI in the Gen3 API system with additional text blurring.
 
-![Image example](imgs/example.gif)
+![Image example](imgs/output.gif)
 
-Cat image is taken from [here](https://www.pexels.com/photo/grey-kitten-on-floor-774731/), dog image is taken from [here](https://www.pexels.com/photo/brown-and-white-american-pit-bull-terrier-with-brown-costume-825949/).
-
-## Pre-requisites
+# Instalation
+Running this example requires a **Luxonis OAK4 device** connected to your computer. You can find more information about the supported devices and the set up instructions in our [Documentation](https://rvc4.docs.luxonis.com/hardware).
+Moreover, you need to prepare a **Python 3.10** environment with [DepthAI](https://pypi.org/project/depthai/) and [DepthAI Nodes](https://pypi.org/project/depthai-nodes/) packages installed. You can do this by running:
+```bash
+pip install -r requirements.txt
+```
 
 Install requirements:
 ```
 python3 -m pip install -r requirements.txt
 ```
 
-## Usage
-
+# Usage
+The inference is ran using a simple CLI call:
+```bash
+python3 main.py \
+    --device ... \
+    --media ...
 ```
-python3 main.py [options]
+
+The relevant arguments:
+- **--device** [OPTIONAL]: DeviceID or IP of the camera to connect to.
+By default, the first locally available device is used;
+- **--media** [OPTIONAL]: Path to the media file to be used as input. 
+Currently, only video files are supported but we plan to add support for more formats (e.g. images) in the future.
+By default, camera input is used;
+
+Running the script downloads the model, creates a DepthAI pipeline, infers on camera input or the provided media, and display the results in a web browser using **Depthai Visualizer**.
+
+## Example
+To run the example you can simply run the following command:
+```bash
+python3 main.py \ 
+        -d <<device ip / mxid>>
 ```
-
-Options:
-
-* `-bt, --box_thresh`: Set the box confidence threshold. Default: *0.2*.
-* `-t, --thresh`: Set the bitmap threshold. Default: *0.1*.
-* `-ms, --min_size`: Set the minimum size of box (area). Default: *1*.
-* `-mc, --max_candidates`: Maximum number of returned box candidates. Default: *75*.
--- neural-networks/advanced-examples/object-detection/text-blur/main.py
+++ neural-networks/advanced-examples/object-detection/text-blur/main.py
@@ -1,88 +1,61 @@
-#!/usr/bin/env python3
-import argparse
-
 import depthai as dai
-from host_node.blur_bboxes import BlurBboxes
-from host_node.host_display import Display
-from host_node.host_fps_drawer import FPSDrawer
-from host_node.normalize_bbox import NormalizeBbox
-from mask_to_bbox import MaskToBbox
-from text_detection_parser import TextDetectionParser
+from depthai_nodes import ParsingNeuralNetwork
+from utils.arguments import initialize_argparser
+from pathlib import Path
+from utils.blur_detections import BlurBboxes
 
-device = dai.Device()
+
+_, args = initialize_argparser()
+
+visualizer = dai.RemoteConnection(httpPort=8082)
+device = dai.Device( dai.DeviceInfo(args.device) if args.device else dai.DeviceInfo())
 platform = device.getPlatform()
-rvc2 = platform == dai.Platform.RVC2
 
-NN_WIDTH, NN_HEIGHT = (256, 256) if rvc2 else (960, 544)
-
-modelDescription = dai.NNModelDescription(
-    modelSlug="paddle-text-detection",
-    platform=device.getPlatform().name,
-    modelVersionSlug=f"{NN_HEIGHT}x{NN_WIDTH}",
-)
-archivePath = dai.getModelFromZoo(modelDescription)
-nn_archive = dai.NNArchive(archivePath)
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-bt",
-    "--box_thresh",
-    help="set the confidence threshold of boxes",
-    default=0.2,
-    type=float,
-)
-parser.add_argument(
-    "-t", "--thresh", help="set the bitmap threshold", default=0.01, type=float
-)
-parser.add_argument(
-    "-ms", "--min_size", default=1, type=int, help="set min size of box"
-)
-parser.add_argument(
-    "-mc",
-    "--max_candidates",
-    default=75,
-    type=int,
-    help="maximum number of candidate boxes",
-)
-
-
-args = parser.parse_args()
+FPS = 28
+if "RVC4" in str(platform):
+    frame_type = dai.ImgFrame.Type.BGR888i
+    FPS = 28
+else:
+    frame_type = dai.ImgFrame.Type.BGR888p
 
 with dai.Pipeline(device) as pipeline:
-    # Define camera
-    cam = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_A)
-    output = cam.requestOutput(
-        (NN_WIDTH, NN_HEIGHT),
-        dai.ImgFrame.Type.BGR888p if rvc2 else dai.ImgFrame.Type.BGR888i,
-    )
-
-    # Define a neural network that will detect text
-    detection_nn = pipeline.create(dai.node.NeuralNetwork).build(output, nn_archive)
-    detection_nn.setNumPoolFrames(4)
-    detection_nn.input.setBlocking(False)
-    detection_nn.setNumInferenceThreads(2)
-    parser = pipeline.create(TextDetectionParser).build(
-        nn=detection_nn.out, nn_size=(NN_HEIGHT, NN_WIDTH)
-    )
-    parser.setConfidenceThreshold(args.thresh)
-
-    mask_to_bbox = pipeline.create(MaskToBbox).build(nn=parser.output)
-    mask_to_bbox.setBboxThreshold(args.box_thresh)
-    mask_to_bbox.setBitmapThreshold(args.thresh)
-    mask_to_bbox.setMinSize(args.min_size)
-    mask_to_bbox.setMaxCandidates(args.max_candidates)
-    mask_to_bbox.setPadding(5)
-    normalize_bbox = pipeline.create(NormalizeBbox).build(
-        nn=mask_to_bbox.output, frame=output, manip_mode=dai.ImgResizeMode.CROP
-    )
-    blur_bboxes = pipeline.create(BlurBboxes).build(
-        nn=normalize_bbox.output, frame=output
-    )
-
-    output_fps = pipeline.create(FPSDrawer).build(blur_bboxes.output)
-    display_preds = pipeline.create(Display).build(output_fps.output)
-    display_preds.setName("Text Blur")
-
-    print("Pipeline created")
-    pipeline.run()
-    print("Pipeline ended")
+    print("Creating pipeline...")
+    
+    if args.media_path:
+        replay_node = pipeline.create(dai.node.ReplayVideo)
+        replay_node.setReplayVideoFile(Path(args.media_path))
+        replay_node.setOutFrameType(dai.ImgFrame.Type.NV12)
+        replay_node.setLoop(True)
+        
+        video_resize_node = pipeline.create(dai.node.ImageManipV2)
+        video_resize_node.initialConfig.setOutputSize(576, 320)
+        video_resize_node.initialConfig.setFrameType(frame_type)
+        
+        replay_node.out.link(video_resize_node.inputImage)
+        
+        input_node = video_resize_node.out
+    else:
+        camera_node = pipeline.create(dai.node.Camera).build()
+        input_node = camera_node.requestOutput((576, 320), frame_type, fps= FPS)   
+    
+    
+    detection_node: ParsingNeuralNetwork = pipeline.create(ParsingNeuralNetwork).build(
+        input_node, "luxonis/paddle-text-detection:320x576"
+        )
+    
+    blur_node = pipeline.create(BlurBboxes)
+    detection_node.out.link(blur_node.input_detections)
+    detection_node.passthrough.link(blur_node.input_frame)
+    
+    visualizer.addTopic("Video",detection_node.passthrough )
+    visualizer.addTopic("Blur Text", blur_node.out)
+    
+    print("Pipeline created.")
+    pipeline.start()
+    visualizer.registerPipeline(pipeline)
+    
+    while pipeline.isRunning():
+        key = visualizer.waitKey(1)
+        if key == ord('q'):
+            print("Got q key. Exiting...")
+            break
