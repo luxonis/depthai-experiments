8cc24d684cc4b29e74f5fd46fa83b1c11f048f35
jkbmrz <jakob.mraz@gmail.com>
2025-01-20 15:49:15 +0100

    feat: fix pipeline stopping when no detections

--- neural-networks/advanced-examples/reidentification/utils/sync.py
+++ neural-networks/advanced-examples/reidentification/utils/sync.py
@@ -4,62 +4,77 @@ from depthai_nodes.ml.messages import ImgDetectionsExtended
 
 
 class AnnotationSyncNode(dai.node.ThreadedHostNode):
-    def __init__(self, csim: float = 0.7) -> None:
+    """A host node for syncing annotations in a two-stage pipeline.
+    The node receives detections and recognitions from two different stages of the pipeline,
+    merges them, and sends the merged annotations to the next stage of the pipeline.
+
+    Attributes
+    ----------
+    input_detections : dai.Input
+        The input message for the detections.
+    input_recognitions : dai.Input
+        The input message for the recognitions.
+    output_detections : dai.Output
+        The output message for the merged detections.
+    csim : float
+        The cosine similarity threshold used for merging.
+    label_basename : str
+        The basename of the labels (e.g., "person"). The labels will be in the format "basename_0", "basename_1", etc.
+    """
+
+    def __init__(self, csim: float = 0.7, label_basename: str = "person") -> None:
         super().__init__()
         self.input_detections = self.createInput()
         self.input_recognitions = self.createInput()
         self.output_detections = self.createOutput()
 
-        self._cos_sim_threshold = csim  # cosine similarity threshold
-        self._labels_dict = {}  # label: embedding of the last seen entry
+        self._cos_sim_threshold = csim
+        self._label_basename = label_basename
+        self._embeddings_dict = {}
 
     def run(self) -> None:
-
-        seq = 0  # sequence number
+        seqs = set()
         labels = []
 
         while self.isRunning():
-            rec = self.input_recognitions.get()
-            rec_seq = rec.getSequenceNum()
-            print("REC", rec_seq)
+            if len(seqs) <= 1:  # at most one sequence stored
+                rec = self.input_recognitions.get()
+                seqs.add(rec.getSequenceNum())
+                print("rec", seqs)
 
-            if rec_seq > seq: # current rec is from a new sequence
+            if len(seqs) > 1:  # multiple sequences stored
                 dets = self.input_detections.get()
-                dets_seq = dets.getSequenceNum()
-                print("DETS", dets_seq)
-
-                if dets_seq < seq: # catch-up with detections if there is no rec
+                det_seq = dets.getSequenceNum()
+                print("det_seq", det_seq)
+                if det_seq < min(seqs):
                     self._send_output(dets, [])
-                    dets = self.input_detections.get()
-                    dets_seq = dets.getSequenceNum()
-                    print("DETS", dets_seq)
-
-                if dets_seq == seq:
+                elif det_seq == min(seqs):
                     self._send_output(dets, labels)
-                    labels = []
-                    seq = rec_seq
+                    seqs.remove(min(seqs))  # remove sequence
+                    labels = []  # remove sequence labels
                 else:
-                    raise Exception(f"SequenceNum mismatch.")
+                    raise ValueError("Detections ahead of recognitions.")
 
-            labels.append(self._get_label(rec))
+            if len(seqs) <= 1:  # store labels of at most one sequence
+                labels.append(self._get_label(rec, self._label_basename))
 
-    def _get_label(self, rec, base="person") -> str:
+    def _get_label(self, rec, basename="person") -> str:
         embedding = rec.getTensor("output", dequantize=True)
         sim = [
-            self._cos_sim(embedding, self._labels_dict[key])
-            for key in self._labels_dict
+            self._cos_sim(embedding, self._embeddings_dict[key])
+            for key in self._embeddings_dict
         ]
 
         if sim:
             if max(sim) >= self._cos_sim_threshold:
                 idx = sim.index(max(sim))
-                label = list(self._labels_dict.keys())[idx]
+                label = list(self._embeddings_dict.keys())[idx]
             else:
-                label = f"{base}_{len(self._labels_dict)}"
+                label = f"{basename}_{len(self._embeddings_dict)}"
         else:
-            label = f"{base}_0"
+            label = f"{basename}_0"
 
-        self._labels_dict[label] = embedding
+        self._embeddings_dict[label] = embedding
         return label
 
     def _cos_sim(self, X: np.ndarray, Y: np.ndarray) -> float:
@@ -69,7 +84,7 @@ class AnnotationSyncNode(dai.node.ThreadedHostNode):
 
     def _send_output(self, dets: ImgDetectionsExtended, labels: list[str]) -> None:
         if len(labels) != len(dets.detections):
-            raise ValueError("Labels and detections must have the same length")
+            raise ValueError("Detections and labels do not match.")
 
         for i, detection in enumerate(dets.detections):
             detection.label_name = labels[i]
