2ece586d408b88ea24cd25fde3fa914c42e9b3d6
aljazkonec1 <konec.aljaz1@gmail.com>
2025-02-24 16:10:42 +0100

    more pre-commit errors

--- neural-networks/facial-detection/fatigue-detection/utils/host_process_detections.py
+++ neural-networks/facial-detection/fatigue-detection/utils/host_process_detections.py
@@ -1,11 +1,12 @@
 import depthai as dai
 from depthai_nodes.ml.messages import ImgDetectionExtended
 
+
 class ProcessDetections(dai.node.ThreadedHostNode):
-    """ A host node for processing a list of detections in a two stage pipeline.
+    """A host node for processing a list of detections in a two stage pipeline.
     The node iterates over a list of detections and sends a dai.MessageGroup with
     a list of ImageManipConfigV2 objects that can be executed by the ImageManipV2 node.
-    
+
     Before use, the source and target sizes need to be set with the set_source_size and set_target_size functions.
     Attributes
     ----------
@@ -21,16 +22,17 @@ class ProcessDetections(dai.node.ThreadedHostNode):
         The width of the output image.
     target_h : int
         The height of the output image.
-    
+
     """
+
     def __init__(self):
         super().__init__()
         self.detections_input = self.createInput()
         self.config_output = self.createOutput()
-        
+
         self._w = 1
         self._h = 1
-        
+
         self._target_w = 100
         self._target_h = 100
 
@@ -51,51 +53,51 @@ class ProcessDetections(dai.node.ThreadedHostNode):
                 cfg.setReusePreviousImage(False)
                 cfg.setTimestamp(img_detections.getTimestamp())
                 cfg.setSequenceNum(img_detections.getSequenceNum())
-                
-                configs_message[str(i+100)] = cfg
-                
-            configs_message.setSequenceNum(img_detections.getSequenceNum()) 
+
+                configs_message[str(i + 100)] = cfg
+
+            configs_message.setSequenceNum(img_detections.getSequenceNum())
             configs_message.setTimestamp(img_detections.getTimestamp())
             self.config_output.send(configs_message)
-    
+
     def set_target_size(self, w: int, h: int):
-        """ Set the target size for the output image. """
+        """Set the target size for the output image."""
         self._target_w = w
         self._target_h = h
-    
-    def set_source_size(self, w: int , h: int):
-        """ Set the source size for the input image. """
+
+    def set_source_size(self, w: int, h: int):
+        """Set the source size for the input image."""
         self._w = w
         self._h = h
 
     @property
     def w(self):
         return self._w
-    
+
     @property
     def h(self):
         return self._h
-    
+
     @property
     def target_w(self):
         return self._target_w
-    
+
     @property
     def target_h(self):
         return self._target_h
-    
+
     @w.setter
     def w(self, w):
         self._w = w
-        
+
     @h.setter
     def h(self, h):
         self._h = h
-    
+
     @target_w.setter
     def target_w(self, target_w):
         self._target_w = target_w
-        
+
     @target_h.setter
     def target_h(self, target_h):
         self._target_h = target_h
