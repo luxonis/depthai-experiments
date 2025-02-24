461058bfd7cbc18b5cf350424400d0874646470d
jkbmrz <74824974+jkbmrz@users.noreply.github.com>
2025-02-24 15:26:09 +0100

    Gen3: Crowdcounting (#604)
    * update example code
    
    * update example requirements
    
    * update example media
    
    * update example readme
    
    * add standalone app file
    
    * fix: pre-commit
    
    * fix example demo
    
    * update example in the main table
    
    * fix pre-commit
    
    * fix non-looping gif
    
    * fix division by zero
    
    * fix MaxOutputFrameSize setting
    
    * fix pre-commit
    
    * change density map color palette
    
    * update example media
    
    * fix pre-commit
    
    * feat: remove ImageManip after replay node
    
    * feat: remove test videos download to match the format of other experiments
    
    * feat: update requirements.txt

---
+++ neural-networks/counting/crowdcounting/utils/density_map_transform.py
@@ -0,0 +1,46 @@
+import cv2
+import depthai as dai
+import numpy as np
+
+
+class DensityMapToFrame(dai.node.HostNode):
+    """A host node that receives density map and transforms it into an image frame.
+
+    Attributes
+    ----------
+    output : dai.ImgFrame
+        The output message for the density map overlayed frame.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.output = self.createOutput(
+            possibleDatatypes=[
+                dai.Node.DatatypeHierarchy(dai.DatatypeEnum.ImgFrame, True)
+            ]
+        )
+        self.setColormap(cv2.COLORMAP_WINTER)
+
+    def setColormap(self, colormap_value: int) -> None:
+        color_map = cv2.applyColorMap(np.arange(256, dtype=np.uint8), colormap_value)
+        color_map[0] = [0, 0, 0]
+        self._colormap = color_map
+
+    def build(self, map_msg: dai.Node.Output) -> "DensityMapToFrame":
+        self.link_args(map_msg)
+        return self
+
+    def process(self, map_msg: dai.Buffer) -> None:
+        density_map = map_msg.map
+        density_map_normalized = (
+            density_map / density_map.max() if density_map.max() > 0 else density_map
+        )
+        density_map_image = cv2.applyColorMap(
+            (density_map_normalized * 255).astype(np.uint8), self._colormap
+        )
+
+        density_map_frame_msg = dai.ImgFrame()
+        density_map_frame_msg.setCvFrame(density_map_image, dai.ImgFrame.Type.BGR888i)
+        density_map_frame_msg.setTimestamp(map_msg.getTimestamp())
+        density_map_frame_msg.setSequenceNum(map_msg.getSequenceNum())
+        self.output.send(density_map_frame_msg)
---
+++ neural-networks/counting/crowdcounting/utils/text_annotation_builder.py
@@ -0,0 +1,47 @@
+from datetime import timedelta
+
+import depthai as dai
+
+Point = tuple[float, float]
+ColorRGBA = tuple[float, float, float, float]
+
+
+class TextAnnotationBuilder:
+    def __init__(self):
+        self.annotation: dai.ImgAnnotation = dai.ImgAnnotation()
+
+    def draw_text(
+        self,
+        text: str,
+        position: Point,
+        color: ColorRGBA,
+        background_color: ColorRGBA | None = None,
+        size: float = 32,
+    ):
+        text_annot = dai.TextAnnotation()
+        text_annot.position = dai.Point2f(position[0], position[1])
+        text_annot.text = text
+        text_annot.textColor = self._create_color(color)
+        text_annot.fontSize = size
+        if background_color is not None:
+            text_annot.backgroundColor = self._create_color(background_color)
+        self.annotation.texts.append(text_annot)
+        return self
+
+    def build(self, timestamp: timedelta, sequence_num: int) -> dai.ImgAnnotations:
+        annotations_msg = dai.ImgAnnotations()
+        annotations_msg.annotations = dai.VectorImgAnnotation([self.annotation])
+        annotations_msg.setTimestamp(timestamp)
+        annotations_msg.setSequenceNum(sequence_num)
+        return annotations_msg
+
+    def _create_color(self, color: ColorRGBA) -> dai.Color:
+        c = dai.Color()
+        c.a = color[3]
+        c.r = color[0]
+        c.g = color[1]
+        c.b = color[2]
+        return c
+
+    def _create_points_vector(self, points: list[Point]) -> dai.VectorPoint2f:
+        return dai.VectorPoint2f([dai.Point2f(pt[0], pt[1]) for pt in points])
