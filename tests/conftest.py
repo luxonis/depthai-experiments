d093f58497b02503d824250400bbe78373112b76
klemen1999 <klemenskrlj8@gmail.com>
2025-03-04 19:40:07 +0100

    updated testing scripts

---
+++ tests/conftest.py
@@ -0,0 +1,108 @@
+import pytest
+from pathlib import Path
+import os
+import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--root-dir",
+        type=Path,
+        required=True,
+        help="Path to the directory with projects or a single project.",
+    )
+    parser.addoption(
+        "--timeout",
+        type=int,
+        default=30,
+        help="Timeout for script execution (default: 30s).",
+    )
+    parser.addoption(
+        "--depthai-version",
+        type=str,
+        help="Specify a depthai version to override requirements.txt.",
+    )
+    parser.addoption(
+        "--environment-variables",
+        type=str,
+        help="List of additional environment variables (format: VAR1=VAL1 VAR2=VAL2).",
+    )
+    parser.addoption(
+        "--virtual-display",
+        action="store_true",
+        help="Enable virtual display (sets DISPLAY=':99').",
+    )
+    parser.addoption(
+        "--platform",
+        default="RVC2",
+        type=str,
+        choices=["RVC2", "RVC4"],
+        help="Specify a platform this is tested on (RVC2 or RVC4). Only used for filtering test examples.",
+    )
+    parser.addoption(
+        "--python-version",
+        default="3.10",
+        type=str,
+        choices=["3.8", "3.10", "3.12"],
+        help="Specify a python version this is tested with (3.8, 3.10 or 3.12). Only used for filtering test examples.",
+    )
+
+
+@pytest.fixture(scope="session")
+def test_args(request):
+    args = {
+        "root_dir": request.config.getoption("--root-dir"),
+        "timeout": request.config.getoption("--timeout"),
+        "depthai_version": request.config.getoption("--depthai-version"),
+        "environment_variables": request.config.getoption("--environment-variables")
+        or {},
+        "virtual_display": request.config.getoption("--virtual-display"),
+        "platform": request.config.getoption("--platform"),
+        "python_version": request.config.getoption("--python-version"),
+    }
+    return args
+
+
+def pytest_generate_tests(metafunc):
+    if "experiment_dir" in metafunc.fixturenames:
+        root_dir = metafunc.config.getoption("--root-dir")
+        platform = metafunc.config.getoption("--platform")
+        python_version = metafunc.config.getoption("--python-version")
+        exp_dirs = []
+
+        for dirpath, dirnames, filenames in os.walk(root_dir):
+            if "main.py" in filenames and "requirements.txt" in filenames:
+                exp_dirs.append(Path(dirpath))
+            elif "main.py" in filenames and "requirements.txt" not in filenames:
+                logger.error(f"Skipping {dirpath} because it has no requirements.txt")
+            elif "main.py" not in filenames and "requirements.txt" in filenames:
+                logger.error(f"Skipping {dirpath} because it has no main.py")
+
+        exp_dirs = filter_experiments(exp_dirs, platform, python_version)
+        metafunc.parametrize("experiment_dir", exp_dirs, ids=[str(p) for p in exp_dirs])
+
+
+def filter_experiments(all_exp_dirs, curr_platform, curr_python_version):
+    filtered_exp_dirs = []
+
+    script_dir = Path(__file__).parent
+    file_path = script_dir / "known_failing_tests.json"
+
+    with open(file_path) as f:
+        data = json.load(f)
+    excluded_dirs = (
+        data[curr_platform][curr_python_version] + data[curr_platform]["all"]
+    )
+    for exp_dir in all_exp_dirs:
+        logger.info(exp_dir, excluded_dirs)
+        if any([str(i) in str(exp_dir) for i in excluded_dirs]):
+            logger.error(
+                f"Skipping {exp_dir} because not supported on platform={curr_platform} with python={curr_python_version}"
+            )
+        else:
+            filtered_exp_dirs.append(exp_dir)
+    return filtered_exp_dirs
---
+++ tests/test_gen3_experiments.py
@@ -0,0 +1,154 @@
+import os
+import subprocess
+import shutil
+import sys
+import pytest
+from pathlib import Path
+from venv import EnvBuilder
+import logging
+
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO)
+
+
+def test_experiment_runs(experiment_dir, test_args):
+    """Tests if a experiment runs for at least N seconds without errors."""
+    if test_args["virtual_display"]:
+        setup_virtual_display()
+
+    experiment_dir = experiment_dir.resolve()
+
+    main_script = experiment_dir / "main.py"
+    requirements_file = experiment_dir / "requirements.txt"
+    venv_dir = experiment_dir / ".test-venv"
+    env_exe = venv_dir / "bin" / "python3"
+
+    if not main_script.exists():
+        pytest.skip(f"Skipping {experiment_dir}, no main.py found.")
+    if not requirements_file.exists():
+        pytest.skip(f"Skipping {experiment_dir}, no requirements.txt found.")
+
+    setup_virtual_env(venv_dir, requirements_file, test_args["depthai_version"])
+    success = run_experiment(
+        env_exe,
+        experiment_dir,
+        test_args,
+    )
+    shutil.rmtree(venv_dir, ignore_errors=True)
+    assert success, f"Test failed for {experiment_dir}"
+
+
+def setup_virtual_display():
+    logger.debug("Ensuring virtual display is set up...")
+    result = subprocess.run(
+        ["pgrep", "-f", "Xvfb :99"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    if result.returncode != 0:
+        logger.debug("Starting virtual display...")
+        result = subprocess.run(
+            ["which", "Xvfb"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        if result.returncode != 0:
+            logger.debug(
+                "Xvfb is not installed on this machine. Please install it and try again."
+            )
+            sys.exit(1)
+        subprocess.Popen(
+            ["Xvfb", ":99", "-screen", "0", "1920x1080x24"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+
+def setup_virtual_env(venv_dir, requirements_file, depthai_version):
+    """Creates and sets up a virtual environment with the required dependencies."""
+    logger.debug(f"Setting up virtual environment for {venv_dir.parent}...")
+    EnvBuilder(clear=True, with_pip=True).create(venv_dir)
+    env_exe = venv_dir / "bin" / "python3"
+
+    # Modify requirements.txt if depthai version is specified
+    with open(requirements_file, "r") as f:
+        requirements = f.readlines()
+    if depthai_version:
+        requirements = [
+            f"depthai=={depthai_version}\n" if "depthai==" in line else line
+            for line in requirements
+        ]
+
+    with open(venv_dir / "requirements.txt", "w") as f:
+        f.writelines(requirements)
+
+    # Install dependencies
+    try:
+        subprocess.run(
+            [
+                env_exe,
+                "-m",
+                "pip",
+                "install",
+                "-r",
+                str(venv_dir / "requirements.txt"),
+                "--timeout=60",
+            ],
+            check=True,
+            text=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        logger.debug(f"Installed packages:\n{get_installed_packages(env_exe)}")
+    except subprocess.CalledProcessError as e:
+        shutil.rmtree(venv_dir)
+        pytest.fail(f"Failed to install dependencies for {venv_dir.parent}: {e.stderr}")
+
+
+def run_experiment(env_exe, experiment_dir, args):
+    """Runs the main.py script for the given timeout duration."""
+    timeout = args["timeout"]
+    env_vars = args["environment_variables"]
+    virtual_env = args["virtual_display"]
+    logger.debug(f"Running {experiment_dir} with timeout {timeout}s...")
+
+    main_script = "main.py"
+
+    original_dir = Path.cwd()
+    os.chdir(experiment_dir)
+
+    env = os.environ.copy()
+    env.update(env_vars)
+    if virtual_env:
+        env["DISPLAY"] = ":99"
+
+    try:
+        # Run the experiment script (main.py)
+        result = subprocess.run(
+            [env_exe, str(main_script)],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            env=env,
+            timeout=timeout,
+        )
+
+        if result.returncode == 0:
+            logger.error(
+                f"{experiment_dir} ran for {timeout} seconds before terminating (exit code 0)."
+            )
+            return False
+        else:
+            logger.error(f"Error in {experiment_dir}:\n{result.stderr}")
+            return False
+    except subprocess.TimeoutExpired:
+        logger.info(f"{experiment_dir} ran for {timeout} seconds before timeout.")
+        return True
+    finally:
+        # Restore the original working directory after running the script
+        os.chdir(original_dir)
+
+
+def get_installed_packages(env_exe):
+    """Returns the list of installed packages in the virtual environment."""
+    return subprocess.check_output([env_exe, "-m", "pip", "freeze"], text=True)
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
