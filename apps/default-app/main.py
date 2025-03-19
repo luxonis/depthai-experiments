8f3db5712f9a6f2fa3d420853e32c4e6a0571ab5
Han Bui <ha@sparkenv.com>
2025-03-19 10:05:44 +0100

    Gen3/default_app: info message if laser is not available

--- apps/default-app/main.py
+++ apps/default-app/main.py
@@ -13,7 +13,8 @@ device = dai.Device(dai.DeviceInfo(args.device)) if args.device else dai.Device(
 STEREO_RESOLUTION = (800, 600)
 NN_DIMENSIONS = (512, 288)
 
-# device.setIrLaserDotProjectorIntensity(1)
+if not device.setIrLaserDotProjectorIntensity(1):
+    print("Failed to set IR laser projector intensity. Maybe your device does not support this feature.")
 with dai.Pipeline(device) as pipeline:
     print("Creating pipeline...")
     model_description = dai.NNModelDescription(f"yolov6-nano:r2-coco-{NN_DIMENSIONS[0]}x{NN_DIMENSIONS[1]}")
