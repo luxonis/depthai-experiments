5dde45be9df5ff6ebb61f93ddc49488e3a795e41
klemen1999 <klemenskrlj8@gmail.com>
2025-03-20 23:05:12 +0100

    fix crestereo

--- neural-networks/depth-estimation/crestereo-stereo-matching/main.py
+++ neural-networks/depth-estimation/crestereo-stereo-matching/main.py
@@ -71,7 +71,6 @@ with dai.Pipeline(device) as pipeline:
     lr_sync.out.link(demux.input)
 
     nn = pipeline.create(ParsingNeuralNetwork)
-    nn.setNNArchive(model, numShaves=7)
     if platform == dai.Platform.RVC4:
         nn.setBackend("snpe")
         nn.setBackendProperties(
@@ -80,6 +79,8 @@ with dai.Pipeline(device) as pipeline:
                 "performance_profile": "default",
             }
         )
+    elif platform == dai.Platform.RVC2:
+        nn.setNNArchive(model, numShaves=7)
 
     demux.outputs["left"].link(nn.inputs["left"])
     demux.outputs["right"].link(nn.inputs["right"])
