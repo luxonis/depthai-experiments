dc0af31bec78fde685208c776b84c414569e4993
Tadeas Pala <tadeas.pala@luxonis.com>
2025-04-02 12:24:52 +0200

    add support for cameras without stereo depth to camera-demo

--- tutorials/camera-demo/main.py
+++ tutorials/camera-demo/main.py
@@ -13,28 +13,38 @@ print(device.getDeviceInfo())
 
 with dai.Pipeline(device) as pipeline:
     print("Creating pipeline...")
-    color = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_A)
-    left = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_B)
-    right = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_C)
+    connected_cameras = device.getConnectedCameras()
 
-    color_output = color.requestOutput(color_resolution, dai.ImgFrame.Type.NV12)
-    left_output = left.requestFullResolutionOutput(dai.ImgFrame.Type.NV12)
-    right_output = right.requestFullResolutionOutput(dai.ImgFrame.Type.NV12)
+    if dai.CameraBoardSocket.CAM_A in connected_cameras:
+        color = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_A)
+        color_output = color.requestOutput(color_resolution, dai.ImgFrame.Type.NV12)
 
-    stereo = pipeline.create(dai.node.StereoDepth).build(
-        left=left_output,
-        right=right_output,
-        presetMode=dai.node.StereoDepth.PresetMode.DEFAULT,
-    )
+        visualizer.addTopic("Color", color_output, "images")
 
-    depth_parser = pipeline.create(DepthColorTransform).build(stereo.disparity)
-    depth_parser.setMaxDisparity(stereo.initialConfig.getMaxDisparity())
-    depth_parser.setColormap(cv2.COLORMAP_JET)
+    if (
+        dai.CameraBoardSocket.CAM_B in connected_cameras
+        and dai.CameraBoardSocket.CAM_C in connected_cameras
+    ):
+        left = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_B)
+        right = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_C)
+        left_output = left.requestFullResolutionOutput(dai.ImgFrame.Type.NV12)
+        right_output = right.requestFullResolutionOutput(dai.ImgFrame.Type.NV12)
 
-    visualizer.addTopic("Color", color_output, "images")
-    visualizer.addTopic("Left", left_output, "images")
-    visualizer.addTopic("Right", right_output, "images")
-    visualizer.addTopic("Depth", depth_parser.output, "images")
+        stereo = pipeline.create(dai.node.StereoDepth).build(
+            left=left_output,
+            right=right_output,
+            presetMode=dai.node.StereoDepth.PresetMode.DEFAULT,
+        )
+
+        depth_parser = pipeline.create(DepthColorTransform).build(stereo.disparity)
+        depth_parser.setMaxDisparity(stereo.initialConfig.getMaxDisparity())
+        depth_parser.setColormap(cv2.COLORMAP_JET)
+
+        visualizer.addTopic("Left", left_output, "images")
+        visualizer.addTopic("Right", right_output, "images")
+        visualizer.addTopic("Depth", depth_parser.output, "images")
+    else:
+        print("Stereo depth is not available on this device")
 
     print("Pipeline created.")
 
