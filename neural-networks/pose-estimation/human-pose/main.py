8e371a26d79d0f02d362da8cc0a133aae531c007
jkbmrz <jakob.mraz@gmail.com>
2025-04-17 18:31:41 +0200

    feat: human pose improve readability

--- neural-networks/pose-estimation/human-pose/main.py
+++ neural-networks/pose-estimation/human-pose/main.py
@@ -6,9 +6,10 @@ from depthai_nodes.node import (
     GatherData,
     ImgDetectionsFilter,
 )
+
+from depthai_nodes.node.utils import generate_script_content
 from utils.arguments import initialize_argparser
 from utils.annotation_node import AnnotationNode
-from utils.script import generate_script_content
 
 _, args = initialize_argparser()
 
@@ -16,7 +17,7 @@ detection_model_slug: str = "luxonis/yolov6-nano:r2-coco-512x288"
 pose_model_slug: str = args.model
 
 padding = 0.1
-valid_labels = [0]  # people
+fps = args.fps_limit
 
 visualizer = dai.RemoteConnection(httpPort=8082)
 device = dai.Device(dai.DeviceInfo(args.device)) if args.device else dai.Device()
@@ -26,23 +27,19 @@ with dai.Pipeline(device) as pipeline:
     print("Creating pipeline...")
 
     detection_model_description = dai.NNModelDescription(detection_model_slug)
-    platform = device.getPlatform().name
-    print(f"Platform: {platform}")
     detection_model_description.platform = platform
     detection_nn_archive = dai.NNArchive(
         dai.getModelFromZoo(detection_model_description, useCached=False)
     )
-    classes = detection_nn_archive.getConfig().model.heads[0].metadata.classes
+
+    valid_labels = [
+        detection_nn_archive.getConfig().model.heads[0].metadata.classes.index("person")
+    ]
 
     pose_model_description = dai.NNModelDescription(pose_model_slug)
     pose_model_description.platform = platform
     pose_nn_archive = dai.NNArchive(
         dai.getModelFromZoo(pose_model_description, useCached=False)
-    )
-    connection_pairs = (
-        pose_nn_archive.getConfig()
-        .model.heads[0]
-        .metadata.extraParams["skeleton_edges"]
     )
 
     if args.media_path:
@@ -57,6 +54,9 @@ with dai.Pipeline(device) as pipeline:
         if args.fps_limit:
             replay.setFps(args.fps_limit)
             args.fps_limit = None  # only want to set it once
+        replay.setSize(
+            detection_nn_archive.getInputWidth(), detection_nn_archive.getInputHeight()
+        )
     input_node = (
         replay.out if args.media_path else pipeline.create(dai.node.Camera).build()
     )
@@ -65,16 +65,16 @@ with dai.Pipeline(device) as pipeline:
         input_node, detection_nn_archive, fps=args.fps_limit
     )
 
-    script = pipeline.create(dai.node.Script)
-    detection_nn.out.link(script.inputs["det_in"])
-    detection_nn.passthrough.link(script.inputs["preview"])
+    script_node = pipeline.create(dai.node.Script)
+    detection_nn.out.link(script_node.inputs["det_in"])
+    detection_nn.passthrough.link(script_node.inputs["preview"])
     script_content = generate_script_content(
         resize_width=pose_nn_archive.getInputWidth(),
         resize_height=pose_nn_archive.getInputHeight(),
         padding=padding,
         valid_labels=valid_labels,
     )
-    script.setScript(script_content)
+    script_node.setScript(script_content)
 
     pose_manip = pipeline.create(dai.node.ImageManipV2)
     pose_manip.initialConfig.setOutputSize(
@@ -82,8 +82,8 @@ with dai.Pipeline(device) as pipeline:
     )
     pose_manip.inputConfig.setWaitForMessage(True)
 
-    script.outputs["manip_cfg"].link(pose_manip.inputConfig)
-    script.outputs["manip_img"].link(pose_manip.inputImage)
+    script_node.outputs["manip_cfg"].link(pose_manip.inputConfig)
+    script_node.outputs["manip_img"].link(pose_manip.inputImage)
 
     pose_nn: ParsingNeuralNetwork = pipeline.create(ParsingNeuralNetwork).build(
         pose_manip.out, pose_nn_archive
@@ -97,13 +97,18 @@ with dai.Pipeline(device) as pipeline:
         detection_nn.out, labels_to_keep=valid_labels
     )
 
-    gather_data_node = pipeline.create(GatherData).build(camera_fps=args.fps_limit)
+    gather_data_node = pipeline.create(GatherData).build(fps)
     pose_nn.out.link(gather_data_node.input_data)
     detections_filter.out.link(gather_data_node.input_reference)
 
+    skeleton_edges = (
+        pose_nn_archive.getConfig()
+        .model.heads[0]
+        .metadata.extraParams["skeleton_edges"]
+    )
     annotation_node = pipeline.create(AnnotationNode).build(
         gather_data_node.out,
-        connection_pairs=connection_pairs,
+        connection_pairs=skeleton_edges,
         valid_labels=valid_labels,
     )
 
