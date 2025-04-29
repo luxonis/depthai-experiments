d987042901084f8a2d9e21c26de402e472542b62
dominik737 <dominik737@gmail.com>
2025-04-29 18:53:46 +0200

    stereo on host rvc4 fix

--- depth-measurement/stereo-on-host/utils/host_stereo_sgbm.py
+++ depth-measurement/stereo-on-host/utils/host_stereo_sgbm.py
@@ -61,7 +61,9 @@ class StereoSGBM(dai.node.HostNode):
         self.link_args(monoLeftOut, monoRightOut)
 
         self.baseline = calibObj.getBaselineDistance() * 10  # mm
-        self.focal_length = self.count_focal_length(calibObj, resolution)
+        self.focal_length = self.count_focal_length(
+            calibObj, device, resolution
+        )
         self.H1, self.H2 = self.count_h(
             calibObj, device, resolution
         )  # for left, right camera
@@ -107,13 +109,13 @@ class StereoSGBM(dai.node.HostNode):
         return H_left, H_right
 
     def count_focal_length(
-        self, calibObj: dai.CalibrationHandler, resolution: Tuple[int, int]
+        self, calibObj: dai.CalibrationHandler, device: dai.Device, resolution: Tuple[int, int]
     ):
         width, height = resolution
 
         M_right = np.array(
             calibObj.getCameraIntrinsics(
-                calibObj.getStereoRightCameraId(), width, height
+                device.getStereoPairs()[0].right, width, height
             )
         )
         focalLength = M_right[0][0]
@@ -127,7 +129,7 @@ class StereoSGBM(dai.node.HostNode):
             left_img.shape[:2][::-1],
             cv2.INTER_CUBIC + cv2.WARP_FILL_OUTLIERS + cv2.WARP_INVERSE_MAP,
         )
-
+    
         img_r = cv2.warpPerspective(
             right_img,
             self.H2,
@@ -151,15 +153,15 @@ class StereoSGBM(dai.node.HostNode):
         pad_img = np.zeros(shape=pad_img_shape, dtype=np.uint8)
         left_img_rect_pad = cv2.hconcat([pad_img, left_img_rect])
         right_img_rect_pad = cv2.hconcat([pad_img, right_img_rect])
-        self.disparity = self.stereoProcessor.compute(
+        disparity = self.stereoProcessor.compute(
             left_img_rect_pad, right_img_rect_pad
         )
-        self.disparity = self.disparity[
-            0 : self.disparity.shape[0], self.max_disparity : self.disparity.shape[1]
+        disparity = disparity[
+            0 : disparity.shape[0], self.max_disparity : disparity.shape[1]
         ]
 
         # scale back to integer disparities, opencv has 4 subpixel bits
-        disparity_scaled = (self.disparity / 16.0).astype(np.uint8)
+        disparity_scaled = (disparity / 16.0).astype(np.uint8)
 
         disparity_colour_mapped = cv2.applyColorMap(
             (disparity_scaled * (256.0 / self.max_disparity)).astype(np.uint8),
@@ -170,11 +172,11 @@ class StereoSGBM(dai.node.HostNode):
             self._create_img_frame(disparity_colour_mapped, dai.ImgFrame.Type.BGR888i)
         )
 
-        self.disparity = np.clip(self.disparity / 16, 0, self.max_disparity).astype(
+        disparity = np.clip(disparity / 16, 0, self.max_disparity).astype(
             np.uint16
         )
         self.raw_disparity_out.send(
-            self._create_img_frame(self.disparity, dai.ImgFrame.Type.RAW16)
+            self._create_img_frame(disparity, dai.ImgFrame.Type.RAW16)
         )
         self.rectified_left.send(
             self._create_img_frame(left_img_rect, dai.ImgFrame.Type.NV12)
