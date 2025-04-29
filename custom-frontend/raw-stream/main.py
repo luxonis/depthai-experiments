e0ef239f2814418babc276a9054321c65ec13e75
dominik737 <dominik737@gmail.com>
2025-04-29 15:16:51 +0200

    use arguments

--- custom-frontend/raw-stream/main.py
+++ custom-frontend/raw-stream/main.py
@@ -1,11 +1,14 @@
 import depthai as dai
 from frontend_server import FrontendServer
 from pathlib import Path
+from utils.arguments import initialize_argparser
 
+
+_, args = initialize_argparser()
 
 FRONTEND_DIRECTORY = Path(__file__).parent / "frontend" / "dist"
-IP = "localhost"
-PORT = 8080
+IP = args.ip or "localhost"
+PORT = args.port or 8080
 
 frontend_server = FrontendServer(IP, PORT, FRONTEND_DIRECTORY)
 print(f"Serving frontend at http://{IP}:{PORT}")
@@ -14,7 +17,7 @@ frontend_server.start()
 visualizer = dai.RemoteConnection(serveFrontend=False)
 with dai.Pipeline() as pipeline:
     cam = pipeline.create(dai.node.Camera).build()
-    raw_stream = cam.requestOutput((640, 480), dai.ImgFrame.Type.NV12)
+    raw_stream = cam.requestOutput((640, 480), dai.ImgFrame.Type.NV12, fps=30 or args.fps_limit)
     visualizer.addTopic("Raw Stream", raw_stream)
 
     def custom_service(message):
