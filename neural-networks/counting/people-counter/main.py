1ee5e4f4f1deea6c3d85702124467cf52eabeb31
jkbmrz <jakob.mraz@gmail.com>
2025-04-29 11:27:56 +0200

    refactor: counting/people-counter

--- neural-networks/counting/people-counter/main.py
+++ neural-networks/counting/people-counter/main.py
@@ -2,29 +2,27 @@ from pathlib import Path
 
 import depthai as dai
 from depthai_nodes.node import ParsingNeuralNetwork
+
 from utils.arguments import initialize_argparser
 from utils.object_counter import ObjectCounter
 
 _, args = initialize_argparser()
 
-if args.fps_limit and args.media_path:
-    args.fps_limit = None
-    print(
-        "WARNING: FPS limit is set but media path is provided. FPS limit will be ignored."
-    )
-
 visualizer = dai.RemoteConnection(httpPort=8082)
 device = dai.Device(dai.DeviceInfo(args.device)) if args.device else dai.Device()
+platform = device.getPlatform().name
+
+MODEL = "luxonis/scrfd-person-detection:25g-640x640"
 
 with dai.Pipeline(device) as pipeline:
     print("Creating pipeline...")
 
-    platform = device.getPlatformAsString()
-    model_description = dai.NNModelDescription(
-        "luxonis/scrfd-person-detection:25g-640x640", platform=platform
-    )
-    nn_archive = dai.NNArchive(dai.getModelFromZoo(model_description))
+    # people detection model
+    det_model_description = dai.NNModelDescription(MODEL)
+    det_model_description.platform = platform
+    det_model_nn_archive = dai.NNArchive(dai.getModelFromZoo(det_model_description))
 
+    # media/camera input
     if args.media_path:
         replay = pipeline.create(dai.node.ReplayVideo)
         replay.setReplayVideoFile(Path(args.media_path))
@@ -34,24 +32,22 @@ with dai.Pipeline(device) as pipeline:
             else dai.ImgFrame.Type.BGR888p
         )
         replay.setLoop(True)
-        if args.fps_limit:
-            replay.setFps(args.fps_limit)
-            args.fps_limit = None  # only want to set it once
-        replay.setSize(nn_archive.getInputWidth(), nn_archive.getInputHeight())
+        replay.setSize(
+            det_model_nn_archive.getInputWidth(), det_model_nn_archive.getInputHeight()
+        )
+    input_node = replay if args.media_path else pipeline.create(dai.node.Camera).build()
 
-    input_node = (
-        replay.out if args.media_path else pipeline.create(dai.node.Camera).build()
+    det_nn: ParsingNeuralNetwork = pipeline.create(ParsingNeuralNetwork).build(
+        input_node, det_model_nn_archive, fps=args.fps_limit
     )
 
-    nn_with_parser: ParsingNeuralNetwork = pipeline.create(ParsingNeuralNetwork).build(
-        input_node, nn_archive, fps=args.fps_limit
-    )
     object_counter = pipeline.create(ObjectCounter).build(
-        nn=nn_with_parser.out, label_map=["People"]
+        nn=det_nn.out, label_map=["People"]
     )
 
-    visualizer.addTopic("Video", nn_with_parser.passthrough)
-    visualizer.addTopic("Visualizations", nn_with_parser.out)
+    # visualization
+    visualizer.addTopic("Video", det_nn.passthrough)
+    visualizer.addTopic("Visualizations", det_nn.out)
     visualizer.addTopic("Object count", object_counter.out)
 
     print("Pipeline created.")
