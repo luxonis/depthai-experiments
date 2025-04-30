6a99f44ad828aa6454a40a4ea337908ccfd069be
jkbmrz <jakob.mraz@gmail.com>
2025-04-30 12:29:08 +0200

    feat: facial-detection/fatigue-detection utilize AnnotationHelper

--- neural-networks/facial-detection/fatigue-detection/utils/host_fatigue_detection.py
+++ neural-networks/facial-detection/fatigue-detection/utils/host_fatigue_detection.py
@@ -2,6 +2,8 @@ import depthai as dai
 from collections import deque
 from utils.face_landmarks import determine_fatigue
 
+from depthai_nodes.utils import AnnotationHelper
+
 
 class FatigueDetection(dai.node.ThreadedHostNode):
     def __init__(self) -> None:
@@ -18,16 +20,6 @@ class FatigueDetection(dai.node.ThreadedHostNode):
         self.out = self.createOutput()
         self.output_frame = self.createOutput()
 
-    def create_output_text(self, text: str, x_position: float, y_position: float):
-        text_annotation = dai.TextAnnotation()
-        text_annotation.position = dai.Point2f(x_position, y_position)
-        text_annotation.text = text
-        text_annotation.textColor = dai.Color(0.5, 0.5, 1.0, 1.0)
-        text_annotation.fontSize = 45
-        text_annotation.backgroundColor = dai.Color(1.0, 1.0, 0.5, 1.0)
-
-        return text_annotation
-
     def run(self) -> None:
         while self.isRunning():
             frame = self.preview.get().getCvFrame()
@@ -35,8 +27,7 @@ class FatigueDetection(dai.node.ThreadedHostNode):
             dets = face_dets.detections
             n_detections = len(dets)
 
-            img_annotations = dai.ImgAnnotations()
-            annotation = dai.ImgAnnotation()
+            annotations = AnnotationHelper()
 
             if n_detections >= 1:
                 crop_face = self.crop_face.get().getCvFrame()
@@ -57,16 +48,24 @@ class FatigueDetection(dai.node.ThreadedHostNode):
                     self.head_tilted_duration
                 )
 
+                print(percent_tilted, percent_closed_eyes)
                 if percent_tilted >= 0.75:
-                    annotation.texts.append(
-                        self.create_output_text("Head Tilted!", 0.1, 0.1)
+                    print("tilted")
+                    annotations.draw_text(
+                        text="Head Tilted!",
+                        position=(0.1, 0.1),
                     )
 
                 if percent_closed_eyes >= 0.75:
-                    annotation.texts.append(
-                        self.create_output_text("Eyes Closed!", 0.1, 0.2)
+                    print("closed")
+                    annotations.draw_text(
+                        text="Eyes Closed!",
+                        position=(0.1, 0.2),
                     )
 
-            img_annotations.annotations.append(annotation)
-            img_annotations.setTimestamp(face_dets.getTimestamp())
-            self.out.send(img_annotations)
+            annotations_msg = annotations.build(
+                timestamp=face_dets.getTimestamp(),
+                sequence_num=face_dets.getSequenceNum(),
+            )
+
+            self.out.send(annotations_msg)
