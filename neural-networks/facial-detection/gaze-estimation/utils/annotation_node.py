f9296b58712dc225155e23a2b8a5bf9d40d3ab42
jkbmrz <jakob.mraz@gmail.com>
2025-04-30 15:57:52 +0200

    feat: facial-detection/gaze-estimation utilize AnnotationHelper

--- neural-networks/facial-detection/gaze-estimation/utils/annotation_node.py
+++ neural-networks/facial-detection/gaze-estimation/utils/annotation_node.py
@@ -1,5 +1,6 @@
 import depthai as dai
-from depthai_nodes import OUTLINE_COLOR
+
+from depthai_nodes.utils import AnnotationHelper
 
 
 class AnnotationNode(dai.node.ThreadedHostNode):
@@ -14,56 +15,50 @@ class AnnotationNode(dai.node.ThreadedHostNode):
             detections_msg = landmarks["detections"]
             gazes_msg = landmarks["gazes"]
 
-            annotation = dai.ImgAnnotation()
-            img_annotations = dai.ImgAnnotations()
             src_w, src_h = detections_msg.transformation.getSourceSize()
+
+            annotations = AnnotationHelper()
 
             for detection, gaze in zip(detections_msg.detections, gazes_msg.landmarks):
                 face_bbox = detection.rotated_rect.getPoints()
                 keypoints = detection.keypoints
 
-                points_annotation = dai.PointsAnnotation()
-                points_annotation.type = dai.PointsAnnotationType.LINE_LOOP
-                points_annotation.points = dai.VectorPoint2f(face_bbox)
-                points_annotation.outlineColor = OUTLINE_COLOR
-                points_annotation.thickness = 2
-                annotation.points.append(points_annotation)
+                # Draw bbox
+                annotations.draw_rectangle(
+                    [face_bbox[0].x, face_bbox[0].y], [face_bbox[2].x, face_bbox[2].y]
+                )
 
                 # Draw gaze
                 gaze_tensor = gaze.getTensor("Identity", dequantize=True)
                 gaze_tensor = gaze_tensor.flatten()
 
                 left_eye = keypoints[0]
-                le_line_ann = self._draw_line(
-                    dai.Point2f(left_eye.x, left_eye.y), gaze_tensor, src_w, src_h
+                annotations.draw_line(
+                    [left_eye.x, left_eye.y],
+                    self._get_end_point(left_eye, gaze_tensor, src_w, src_h),
                 )
-                annotation.points.append(le_line_ann)
 
                 right_eye = keypoints[1]
-                re_line_ann = self._draw_line(
-                    dai.Point2f(right_eye.x, right_eye.y), gaze_tensor, src_w, src_h
+                annotations.draw_line(
+                    [right_eye.x, right_eye.y],
+                    self._get_end_point(right_eye, gaze_tensor, src_w, src_h),
                 )
-                annotation.points.append(re_line_ann)
 
-            img_annotations.annotations.append(annotation)
-            img_annotations.setTimestamp(landmarks.getTimestamp())
-            img_annotations.setSequenceNum(landmarks.getSequenceNum())
+            annotations_msg = annotations.build(
+                timestamp=detections_msg.getTimestamp(),
+                sequence_num=detections_msg.getSequenceNum(),
+            )
 
-            self.output.send(img_annotations)
+            self.output.send(annotations_msg)
 
-    def _draw_line(
+    def _get_end_point(
         self, start_point: dai.Point2f, vector: list, src_w: int, src_h: int
     ) -> dai.PointsAnnotation:
         gaze_vector = (vector * 640)[:2]
         gaze_vector_x = gaze_vector[0] / src_w
         gaze_vector_y = gaze_vector[1] / src_h
-        end_point = dai.Point2f(
-            start_point.x + gaze_vector_x, start_point.y - gaze_vector_y
-        )
-
-        line_ann = dai.PointsAnnotation()
-        line_ann.type = dai.PointsAnnotationType.LINE_STRIP
-        line_ann.points = dai.VectorPoint2f([start_point, end_point])
-        line_ann.outlineColor = dai.Color(0.0, 0.0, 1.0, 1.0)
-        line_ann.thickness = 2
-        return line_ann
+        end_point = [
+            start_point.x + gaze_vector_x.item(),
+            start_point.y - gaze_vector_y.item(),
+        ]
+        return end_point
