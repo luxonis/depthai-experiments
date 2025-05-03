fix(box-measurement): correct metric, used dist coefficients instead of zeros array for precise calculation

depth-measurement/3d-measurement/box-measurement/main.py
@@ -1,4 +1,5 @@
 import depthai as dai
+import numpy as np
 
 from utils.host_box_measurement import BoxMeasurement
 from utils.arguments import initialize_argparser
@@ -20,9 +21,6 @@ with dai.Pipeline(device) as pipeline:
     cam_out = cam.requestOutput(
         IMG_SHAPE,
         dai.ImgFrame.Type.RGB888i,
-        # dai.ImgResizeMode.CROP, 30, True
-        # if platform == dai.Platform.RVC4
-        # else dai.ImgFrame.Type.BGR888p,
     )
 
     left = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_B)
@@ -36,35 +34,35 @@ with dai.Pipeline(device) as pipeline:
         right=right_output,
         presetMode=dai.node.StereoDepth.PresetMode.DEFAULT,
     )
-    # stereo.initialConfig.setMedianFilter(dai.MedianFilter.KERNEL_7x7)
-    # stereo.setLeftRightCheck(True)
-    # stereo.setExtendedDisparity(False)
-    # stereo.setSubpixel(True)
-    # stereo.setSubpixelFractionalBits(3)
-    # stereo.setDepthAlign(dai.CameraBoardSocket.CAM_A)
+    stereo.initialConfig.setMedianFilter(dai.MedianFilter.KERNEL_7x7)
+    stereo.setLeftRightCheck(True)
+    stereo.setExtendedDisparity(False)
+    stereo.setSubpixel(True)
+    stereo.setSubpixelFractionalBits(3)
+    stereo.setDepthAlign(dai.CameraBoardSocket.CAM_A)
     stereo.setOutputSize(IMG_SHAPE[0], IMG_SHAPE[1])
 
     """ In-place post-processing configuration for a stereo depth node
     The best combo of filters is application specific. Hard to say there is a one size fits all.
     They also are not free. Even though they happen on device, you pay a penalty in fps. """
-    # stereo.initialConfig.postProcessing.speckleFilter.enable = False
-    # stereo.initialConfig.postProcessing.speckleFilter.speckleRange = 50
-    # stereo.initialConfig.postProcessing.temporalFilter.enable = True
-    # stereo.initialConfig.postProcessing.spatialFilter.enable = True
-    # stereo.initialConfig.postProcessing.spatialFilter.holeFillingRadius = 2
-    # stereo.initialConfig.postProcessing.spatialFilter.numIterations = 1
-    # stereo.initialConfig.postProcessing.thresholdFilter.minRange = 400
-    # stereo.initialConfig.postProcessing.thresholdFilter.maxRange = 15000
-    # stereo.initialConfig.postProcessing.decimationFilter.decimationFactor = 1
+    stereo.initialConfig.postProcessing.speckleFilter.enable = False
+    stereo.initialConfig.postProcessing.speckleFilter.speckleRange = 50
+    stereo.initialConfig.postProcessing.temporalFilter.enable = True
+    stereo.initialConfig.postProcessing.spatialFilter.enable = True
+    stereo.initialConfig.postProcessing.spatialFilter.holeFillingRadius = 2
+    stereo.initialConfig.postProcessing.spatialFilter.numIterations = 1
+    stereo.initialConfig.postProcessing.thresholdFilter.minRange = 400
+    stereo.initialConfig.postProcessing.thresholdFilter.maxRange = 15000
+    stereo.initialConfig.postProcessing.decimationFilter.decimationFactor = 1
 
     width, height = IMG_SHAPE 
     intrinsics = calib_data.getCameraIntrinsics(
         dai.CameraBoardSocket.CAM_A, dai.Size2f(width, height)
     )
+    dist_coeffs = np.array(calib_data.getDistortionCoefficients(dai.CameraBoardSocket.CAM_A))
 
     cam_out.link(stereo.inputAlignTo)
     rgbd = pipeline.create(dai.node.RGBD).build()
-    rgbd.setDepthUnits(dai.StereoDepthConfig.AlgorithmControl.DepthUnit.METER)
     stereo.depth.link(rgbd.inDepth)
     cam_out.link(rgbd.inColor)
 
@@ -72,7 +70,7 @@ with dai.Pipeline(device) as pipeline:
         color=cam_out,
         pcl=rgbd.pcl,
         cam_intrinsics=intrinsics,
-        shape=IMG_SHAPE,
+        dist_coeffs=dist_coeffs,
         max_dist=args.max_dist,
         min_box_size=args.min_box_size,
     )

depth-measurement/3d-measurement/box-measurement/utils/box_estimator.py
@@ -90,7 +90,7 @@ class BoxEstimator:
             self.vis.update_renderer()
             self.vis.remove_geometry(line_set, reset_bounding_box=False)
 
-    def add_visualization_2d(self, intrinsic_mat, annotation_helper: AnnotationHelper, width, height):
+    def add_visualization_2d(self, intrinsic_mat, dist_coeffs, annotation_helper: AnnotationHelper, width, height):
         bounding_box = self.bounding_box
         points_floor = np.c_[bounding_box, np.zeros(4)]
         points_top = np.c_[bounding_box, self.height * np.ones(4)]
@@ -131,7 +131,7 @@ class BoxEstimator:
             (0, 0, 0),
             (0, 0, 0),
             intrinsic_mat,
-            np.zeros(4, dtype="float32"),
+            dist_coeffs
         )
 
         for line in lines:
@@ -141,6 +141,7 @@ class BoxEstimator:
 
     def process_pcl(self, raw_pcl: dai.PointCloudData):
         pts, cols = raw_pcl.getPointsRGB()
+        pts = pts * 0.001
         self._pcd.points = o3d.utility.Vector3dVector(pts.astype(np.float64))
         rgb = (cols[:, :3] / 255.0).astype(np.float64)
         self._pcd.colors = o3d.utility.Vector3dVector(rgb)

depth-measurement/3d-measurement/box-measurement/utils/host_box_measurement.py
@@ -1,6 +1,6 @@
+import numpy as np
 import depthai as dai
 from .box_estimator import BoxEstimator
-from .projector_3d import PointCloudFromRGBD
 from .img_annotation_helper import AnnotationHelper
 
 
@@ -28,6 +28,7 @@ class BoxMeasurement(dai.node.ThreadedHostNode):
         )
         self.box_estimator = None
         self.intrinsics = None
+        self.dist_coeffs = None
         self.min_box_size = None
 
     def build(
@@ -35,11 +36,12 @@ class BoxMeasurement(dai.node.ThreadedHostNode):
         color: dai.Node.Output,
         pcl: dai.Node.Output,
         cam_intrinsics: list,
-        shape: tuple[int, int],
+        dist_coeffs: np.ndarray,
         max_dist: float,
         min_box_size: float,
     ) -> "BoxMeasurement":
         self.intrinsics = cam_intrinsics
+        self.dist_coeffs = dist_coeffs
         self.min_box_size = min_box_size
 
         self.box_estimator = BoxEstimator(max_dist)
@@ -70,7 +72,7 @@ class BoxMeasurement(dai.node.ThreadedHostNode):
             if l * w * h > self.min_box_size:
                 # Create ImgAnnotations and draw lines
                 height, width, _ = color_frame.shape
-                self.box_estimator.add_visualization_2d(self.intrinsics, bbox_annot_builder, width, height)
+                self.box_estimator.add_visualization_2d(self.intrinsics, self.dist_coeffs, bbox_annot_builder, width, height)
                 bbox_annot = bbox_annot_builder.build(
                     color_msg.getTimestamp(), color_msg.getSequenceNum()
                 )