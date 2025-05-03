56ac2c2009411c1484669fd1fa0e48e791863ab8
dominik737 <dominik737@gmail.com>
2025-05-03 21:53:40 +0200

    ruff formating fix

--- custom-frontend/raw-stream/main.py
+++ custom-frontend/raw-stream/main.py
@@ -17,7 +17,9 @@ frontend_server.start()
 visualizer = dai.RemoteConnection(serveFrontend=False)
 with dai.Pipeline() as pipeline:
     cam = pipeline.create(dai.node.Camera).build()
-    raw_stream = cam.requestOutput((640, 480), dai.ImgFrame.Type.NV12, fps=30 or args.fps_limit)
+    raw_stream = cam.requestOutput(
+        (640, 480), dai.ImgFrame.Type.NV12, fps=30 or args.fps_limit
+    )
     visualizer.addTopic("Raw Stream", raw_stream)
 
     def custom_service(message):
