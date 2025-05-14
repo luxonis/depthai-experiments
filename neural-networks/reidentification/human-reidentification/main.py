400996fd4e9b8342394491182ef3f147db8038c0
jkbmrz <jakob.mraz@gmail.com>
2025-05-14 11:21:59 +0200

    refactor: reidentification/human-reidentification simplify argument setting

--- neural-networks/reidentification/human-reidentification/main.py
+++ neural-networks/reidentification/human-reidentification/main.py
@@ -2,38 +2,51 @@ from pathlib import Path
 import depthai as dai
 from depthai_nodes.node import ParsingNeuralNetwork, GatherData, ImgDetectionsBridge
 from depthai_nodes.node.utils import generate_script_content
-
 from utils.arguments import initialize_argparser
 from utils.identification import IdentificationNode
 
 _, args = initialize_argparser()
-
 visualizer = dai.RemoteConnection(httpPort=8082)
 device = dai.Device(dai.DeviceInfo(args.device)) if args.device else dai.Device()
 platform = device.getPlatform().name
+
+frame_type = (
+    dai.ImgFrame.Type.BGR888i if platform == "RVC4" else dai.ImgFrame.Type.BGR888p
+)
+
+if args.identify == "pose":
+    DET_MODEL = "luxonis/scrfd-person-detection:25g-640x640"
+    REC_MODEL = "luxonis/osnet:imagenet-128x256"
+    CSIM = 0.8
+elif args.identify == "face":
+    DET_MODEL = "luxonis/scrfd-face-detection:10g-640x640"  # "luxonis/yunet:640x480" is also an option
+    REC_MODEL = "luxonis/arcface:lfw-112x112"
+    CSIM = 0.1
+else:
+    raise ValueError("Unknown identify option provided.")
+
+if args.cos_similarity_threshold:
+    CSIM = args.cos_similarity_threshold  # override default
+
 
 with dai.Pipeline(device) as pipeline:
     print("Creating pipeline...")
 
     # detection model
-    det_model_description = dai.NNModelDescription(args.det_model)
+    det_model_description = dai.NNModelDescription(DET_MODEL)
     det_model_description.platform = platform
-    det_nn_archive = dai.NNArchive(dai.getModelFromZoo(det_model_description))
+    det_model_nn_archive = dai.NNArchive(dai.getModelFromZoo(det_model_description))
 
     # recognition model
-    rec_model_description = dai.NNModelDescription(args.rec_model)
+    rec_model_description = dai.NNModelDescription(REC_MODEL)
     rec_model_description.platform = platform
     rec_nn_archive = dai.NNArchive(dai.getModelFromZoo(rec_model_description))
 
-    # media/camera source
+    # media/camera input
     if args.media_path:
         replay = pipeline.create(dai.node.ReplayVideo)
         replay.setReplayVideoFile(Path(args.media_path))
-        replay.setOutFrameType(
-            dai.ImgFrame.Type.BGR888i
-            if platform == "RVC4"
-            else dai.ImgFrame.Type.BGR888p
-        )
+        replay.setOutFrameType(frame_type)
         replay.setLoop(True)
         replay.setSize(det_nn_archive.getInputWidth(), det_nn_archive.getInputHeight())
     else:
@@ -76,9 +89,7 @@ with dai.Pipeline(device) as pipeline:
     det_nn.out.link(gather_data_node.input_reference)
 
     # idenfication
-    id_node = pipeline.create(IdentificationNode).build(
-        gather_data_node.out, csim=args.cos_similarity_threshold
-    )
+    id_node = pipeline.create(IdentificationNode).build(gather_data_node.out, csim=CSIM)
 
     # Visualizer
     visualizer.addTopic("Video", det_nn.passthrough, "images")
