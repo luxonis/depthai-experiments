312728ae2fe70ad985dfca26aff666ae4c5ac908
Han Bui <ha@sparkenv.com>
2025-05-26 23:07:34 +0200

    WIP: multicam calibration initial refactoring attempt

---
+++ tutorials/multiple-devices/multi-cam-calibration/utils/arguments.py
@@ -0,0 +1,26 @@
+import argparse
+
+
+def initialize_argparser():
+    """Initialize the argument parser for the script."""
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.description = "Example with multiple devices connected."
+
+    parser.add_argument(
+        "--include-ip",
+        action="store_true",
+        help="Also include IP-only cameras (e.g. OAK-4) in the device list",
+    )
+
+    parser.add_argument(
+        "--max-devices",
+        type=int,
+        default=None,
+        help="Limit the total number of devices to this count",
+    )
+
+    args = parser.parse_args()
+
+    return parser, args
